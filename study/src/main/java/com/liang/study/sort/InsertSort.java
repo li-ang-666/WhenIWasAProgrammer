dd213183976b185e88bd7655f1a254c85f73f8fd
liang <li_ang_666@163.com>
2023-02-26 22:32:29 +0800

    1

---
+++ study/src/main/java/com/liang/study/sort/InsertSort.java
@@ -0,0 +1,18 @@
+package com.liang.study.sort;
+
+import org.junit.Test;
+
+public class InsertSort {
+    @Test
+    public void test() {
+        ArrayUtils.test(this::sort);
+    }
+
+    public void sort(int[] arr) {
+        for (int i = 1; i <= arr.length - 1; i++) {
+            for (int j = i; j - 1 >= 0 && arr[j - 1] > arr[j]; j--) {
+                ArrayUtils.swap(arr, j, j - 1);
+            }
+        }
+    }
+}
