baf751e13540dee47168285358d9a27867a0ffc7
liang <li_ang_666@163.com>
2023-03-04 20:56:58 +0800

    update .gitignore

--- spark/src/main/java/com/liang/spark/InterviewTest.java
+++ spark/src/main/java/com/liang/spark/InterviewTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class InterviewTest {
     @Test
-    public void test() {
+    public void test() throws Throwable {
         SparkSession spark = SparkSession
                 .builder()
                 .config("spark.sql.autoBroadcastJoinThreshold", "-1")
@@ -14,7 +14,11 @@ public class InterviewTest {
         spark.read()
                 .option("header", "true")
                 .option("inferSchema", "true")
-                .csv("/Users/liang/Desktop/WhenIWasAProgrammer/spark/src/main/resources/tb1.csv")
+                .csv("/Users/liang/Desktop/WhenIWasAProgrammer/spark/src/main/resources/t1.csv")
                 .createTempView("t1");
+
+        spark.sql("select * from t1").show();
+
+        spark.stop();
     }
 }
