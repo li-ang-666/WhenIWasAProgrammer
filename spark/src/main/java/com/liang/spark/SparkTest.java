bbf9355a030c0e48e3934dac4d717081b7f834fb
liang <li_ang_666@163.com>
2023-04-24 22:30:18 +0800

    1

--- spark/src/main/java/com/liang/spark/SparkTest.java
+++ spark/src/main/java/com/liang/spark/SparkTest.java
@@ -1,7 +1,6 @@
 package com.liang.spark;
 
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.storage.StorageLevel;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
@@ -23,17 +22,9 @@ public class SparkTest {
                 .createTempView("t");
         spark.udf().register("countd", new CountDistinct());
 
-        spark.sql("explain select channel_id,count(distinct candidate_id),collect_list(distinct application_id),sum(candidate_id) from t group by channel_id order by channel_id")
+        spark.sql("explain select col_dim,count(distinct col_a),count(distinct col_b),max(col_c),min(col_d) from t group by col_dim")
                 .show(false);
 
-
-        spark.sql("explain select t1.* from t t1 join t t2 on t1.channel_id=t2.channel_id")
-                .show(false);
-
-        spark.sql("explain select channel_id,countd(candidate_id),countd(application_id),sum(candidate_id) from t group by channel_id")
-                .show(false);
-
-       // Thread.sleep(1000*3600);
         spark.stop();
     }
 }
--- study/src/main/java/com/liang/study/test/Test.java
+++ study/src/main/java/com/liang/study/test/Test.java
@@ -6,10 +6,12 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        Map<Integer, String> dataMap = new HashMap<>();
+        Map<Integer, Integer> dataMap = new HashMap<>();
         Random r = new Random();
         while (true) {
-            dataMap.put(r.nextInt(), String.valueOf(r.nextInt()));
+            int i = r.nextInt();
+            dataMap.put(i, i);
+            System.out.println(dataMap.size());
         }
     }
 }
