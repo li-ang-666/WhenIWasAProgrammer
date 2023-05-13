2b606a320e2ee14d191dc300755ca89db4816d54
liang <li_ang_666@163.com>
2023-05-13 17:23:01 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/LocalTest.java
+++ repair/src/main/java/com/liang/repair/impl/LocalTest.java
@@ -3,21 +3,23 @@ package com.liang.repair.impl;
 import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.common.service.database.template.RedisEnhancer;
 import com.liang.repair.trait.Runner;
+import org.apache.flink.api.java.tuple.Tuple2;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public class LocalTest implements Runner {
     @Override
     public void run(String[] args) throws Exception {
         RedisEnhancer redis = new RedisEnhancer("localhost");
-        Set<String> res = redis.exec(j -> j.keys("*"));
-        for (String re : res) {
-            System.out.println(re + " -> " + redis.exec(j -> j.get(re)));
-        }
+        Map<String, String> firstHash = redis.hScan("first_hash");
+        System.out.println(firstHash);
+
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate("localhost");
-        List<String> showTables = jdbcTemplate.queryForList("show tables", resultSet -> resultSet.getString(1));
-        System.out.println(showTables);
+        List<Tuple2<String, String>> query = jdbcTemplate.queryForList("select * from liang_test", rs -> Tuple2.of(rs.getString(1), rs.getString(2)));
+        for (Tuple2<String, String> tuple2 : query) {
+            System.out.println(tuple2.f0 + " -> " + tuple2.f1);
+        }
     }
 }
