747398dda67d862fae61dbe020ce3fdb1fa699a2
liang <li_ang_666@163.com>
2023-05-13 14:16:04 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/LocalTest.java
+++ repair/src/main/java/com/liang/repair/impl/LocalTest.java
@@ -1,18 +1,23 @@
 package com.liang.repair.impl;
 
+import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.common.service.database.template.RedisEnhancer;
 import com.liang.repair.trait.Runner;
 
+import java.util.List;
 import java.util.Set;
 
 public class LocalTest implements Runner {
     @Override
     public void run(String[] args) throws Exception {
-        RedisEnhancer localhost = new RedisEnhancer("localhost");
-        Set<String> res = localhost.exec(j -> j.keys("*"));
+        RedisEnhancer redis = new RedisEnhancer("localhost");
+        Set<String> res = redis.exec(j -> j.keys("*"));
         for (String re : res) {
-            System.out.println(re + " -> " + localhost.exec(j -> j.get(re)));
+            System.out.println(re + " -> " + redis.exec(j -> j.get(re)));
         }
 
+        JdbcTemplate jdbcTemplate = new JdbcTemplate("localhost");
+        List<String> showTables = jdbcTemplate.queryForList("show tables", resultSet -> resultSet.getString(1));
+        System.out.println(showTables);
     }
 }
