e2e4514da51e6b5bb4bdd69ccd6eb0626f403f15
liang <li_ang_666@163.com>
2023-05-19 17:44:28 +0800

    1

--- repair/src/main/java/com/liang/repair/annotation/Prop.java
+++ repair/src/main/java/com/liang/repair/annotation/Prop.java
@@ -5,4 +5,7 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Prop {
+    String key1() default "value1";
+
+    String key2() default "value2";
 }
