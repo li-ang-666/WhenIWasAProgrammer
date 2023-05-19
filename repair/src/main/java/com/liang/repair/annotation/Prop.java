62d5992d43674bbb29b6686f842acb5a7d2ffe01
liang <li_ang_666@163.com>
2023-05-19 18:16:06 +0800

    1

--- repair/src/main/java/com/liang/repair/annotation/Prop.java
+++ repair/src/main/java/com/liang/repair/annotation/Prop.java
@@ -1,9 +1,14 @@
 package com.liang.repair.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+//一般来说自己写的注解都是RUNTIME
 @Retention(RetentionPolicy.RUNTIME)
+//类注解
+@Target(ElementType.TYPE)
 public @interface Prop {
     String key1() default "value1";
 
