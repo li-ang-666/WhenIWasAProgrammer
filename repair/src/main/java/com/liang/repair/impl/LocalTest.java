caba2d8303af9806d86271af886b4d2c3079ee74
liang <li_ang_666@163.com>
2023-05-19 17:54:42 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/LocalTest.java
+++ repair/src/main/java/com/liang/repair/impl/LocalTest.java
@@ -9,14 +9,13 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import java.util.List;
 import java.util.Map;
 
-
+@Prop(key1 = "aaa", key2 = "bbb")
 public class LocalTest implements Runner {
     @Override
     public void run(String[] args) throws Exception {
         RedisEnhancer redis = new RedisEnhancer("localhost");
         Map<String, String> firstHash = redis.hScan("first_hash");
         System.out.println(firstHash);
-
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate("localhost");
         List<Tuple2<String, String>> query = jdbcTemplate.queryForList("select * from liang_test", rs -> Tuple2.of(rs.getString(1), rs.getString(2)));
--- repair/src/main/java/com/liang/repair/launch/LocalLauncher.java
+++ repair/src/main/java/com/liang/repair/launch/LocalLauncher.java
@@ -5,9 +5,11 @@ import com.liang.common.util.ConfigUtils;
 import com.liang.common.util.YamlUtils;
 import com.liang.repair.annotation.Prop;
 import com.liang.repair.trait.Runner;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.InputStream;
 
+@Slf4j
 public class LocalLauncher {
     public static void main(String[] args) {
         init();
@@ -25,10 +27,11 @@ public class LocalLauncher {
     private static void run(String className, String[] args) {
         try {
             Class<?> aClass = Class.forName("com.liang.repair.impl." + className);
+
             Prop prop = aClass.getAnnotation(Prop.class);
             System.out.println(prop);
-            Object instance = aClass.newInstance();
-            ((Runner) instance).run(args);
+
+            ((Runner) aClass.newInstance()).run(args);
         } catch (Exception e) {
             e.printStackTrace();
         }
