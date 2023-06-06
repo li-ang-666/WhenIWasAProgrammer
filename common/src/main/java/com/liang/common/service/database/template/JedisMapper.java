cb491cada7cf7dcf08fdd20c42481dc44e4039d7
liang <li_ang_666@163.com>
2023-06-06 11:15:21 +0800

    1

--- common/src/main/java/com/liang/common/service/database/template/JedisMapper.java
+++
@@ -1,8 +0,0 @@
-package com.liang.common.service.database.template;
-
-import redis.clients.jedis.Jedis;
-
-@FunctionalInterface
-public interface JedisMapper<T> {
-    T map(Jedis jedis) throws Exception;
-}
