f44491b5365ed9504255a52f7e778f018dfa06e4
liang <li_ang_666@163.com>
2023-06-08 19:29:35 +0800

    1

--- spark/src/main/java/com/liang/spark/basic/SparkSessionFactory.java
+++ spark/src/main/java/com/liang/spark/basic/SparkSessionFactory.java
@@ -2,14 +2,8 @@ package com.liang.spark.basic;
 
 import com.liang.common.dto.Config;
 import com.liang.common.util.ConfigUtils;
-import com.liang.common.util.YamlUtils;
-import com.liang.spark.job.DataConcatJob;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.SparkSession;
-
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @Slf4j
 public class SparkSessionFactory {
@@ -22,15 +16,7 @@ public class SparkSessionFactory {
     }
 
     private static void initConfig(String[] args) throws Exception {
-        InputStream resourceStream;
-        if (args.length == 0) {
-            log.warn("参数没有传递外部config文件, 从内部 resource 寻找 ...");
-            resourceStream = DataConcatJob.class.getClassLoader().getResourceAsStream("config.yml");
-        } else {
-            log.info("外部参数传递 config 文件: {}", args[0]);
-            resourceStream = Files.newInputStream(Paths.get(args[0]));
-        }
-        Config config = YamlUtils.parse(resourceStream, Config.class);
+        Config config = ConfigUtils.initConfig(args);
         ConfigUtils.setConfig(config);
     }
 
