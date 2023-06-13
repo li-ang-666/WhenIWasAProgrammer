dcd34d4bec1c6a8b217ac9367435635166e226c0
liang <li_ang_666@163.com>
2023-06-13 21:29:02 +0800

    1

--- flink/src/main/java/com/liang/flink/service/RepairDataHandler.java
+++ flink/src/main/java/com/liang/flink/service/RepairDataHandler.java
@@ -18,7 +18,7 @@ import static com.liang.common.dto.config.RepairTask.ScanMode.TumblingWindow;
 
 @Slf4j
 public class RepairDataHandler implements Runnable {
-    private final static int BATCH_SIZE = 10000;
+    private final static int BATCH_SIZE = 1000;
     private final static int MAX_QUEUE_SIZE = 10000 * 100;
     private final static int DIRECT_TASK_FINISH_ID = 404;
 
