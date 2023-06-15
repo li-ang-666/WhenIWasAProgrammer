ea7cacad3dabe243ebf8a699de270c6ab5acb3ec
liang <li_ang_666@163.com>
2023-06-15 20:05:33 +0800

    1

--- flink/src/main/resources/flink-run.sh
+++ flink/src/main/resources/flink-run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-export FLINK_HOME=/data/liang/flink-1.17.1/
-export FLINK_CONF_DIR=/data/liang/flink-conf/
+export FLINK_HOME=/data/liang/flink-1.17.1
+export FLINK_CONF_DIR=/data/liang/flink-conf
 
+export jobName=FlinkJobName
+export configName=config.yml
+
+export folderName=$(echo ${jobName} | sed -E 's/([A-Z])/-\1/g' | sed -E 's/^-//g' | tr 'A-Z' 'a-z')
 /data/liang/flink-1.17.1/bin/flink run-application -t yarn-application \
   -D jobmanager.memory.process.size=1024mb \
   -D taskmanager.memory.process.size=2048mb \
   -D taskmanager.numberOfTaskSlots=1 \
-  -D parallelism.default=1 \
-  -D yarn.application.queue=default \
-  -D yarn.application.name=${JobName} \
-  -D state.checkpoints.dir="hdfs:///liang/flink-checkpoints/${job-name}/" \
-  -D yarn.ship-files="config.yml" \
-  -c com.liang.flink.job.DataConcatJob flink-1.0-jar-with-dependencies.jar config.yml
+  -D yarn.application.name=${jobName} \
+  -D state.checkpoints.dir=hdfs:///liang/flink-checkpoints/${folderName} \
+  -D yarn.ship-files=${configName} \
+  -c com.liang.flink.job.DataConcatJob flink-1.0-jar-with-dependencies.jar ${configName}
