71ac96fd4e545f029291e79a3239c13e144a57fd
liang <li_ang_666@163.com>
2023-06-24 21:53:27 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/ObsWriterTest.java
+++ repair/src/main/java/com/liang/repair/impl/ObsWriterTest.java
@@ -9,8 +9,8 @@ import java.util.UUID;
 public class ObsWriterTest implements Runner {
     @Override
     public void run(String[] args) throws Exception {
-        ObsWriter writer = new ObsWriter("obs://hadoop-obs/flink/tb1");
-        writer.enableCache(1000*2, (int) (50000 * 50 * 1.2) - 1);
+        ObsWriter writer = new ObsWriter("obs://hadoop-obs/flink/tb1/");
+        writer.enableCache(1500, (int) (50000 * 50 * 1.2) - 1);
         ArrayList<String> list = new ArrayList<>();
         for (int i = 1; i <= 50000 * 50 * 1.2; i++) {
             writer.update(UUID.randomUUID().toString());
