d5193188437c9f947bf604f011e4e3a6dc2ded66
liang <li_ang_666@163.com>
2023-06-26 16:53:57 +0800

    1

--- spark/src/main/resources/spark-submit.sh
+++ spark/src/main/resources/spark-submit.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-export jobName=MysqlToHiveJob
-
 nohup spark-submit \
-  --class com.liang.spark.job.${jobName} \
-  --name ${jobName} \
+  --class com.liang.spark.job.AdsJob \
+  --name ads \
   --proxy-user liang \
   --master yarn \
   --deploy-mode cluster \
   --driver-memory 2g --driver-cores 2 \
   --conf spark.driver.memoryOverhead=512m \
-  --executor-memory 5g --num-executors 10 --executor-cores 3 \
+  --executor-memory 6g --num-executors 10 --executor-cores 2 \
   --conf spark.executor.memoryOverhead=512m \
-  --conf spark.memory.offHeap.enabled=true --conf spark.memory.offHeap.size=512m \
+  --conf spark.memory.offHeap.enabled=true --conf spark.memory.offHeap.size=2g \
   --queue default \
   --conf spark.memory.fraction=0.8 \
-  --conf spark.driver.extraJavaOptions="-Dlog4j.configuration=log4j-all.properties" \
-  --conf spark.executor.extraJavaOptions="-Dlog4j.configuration=log4j-all.properties" \
-  --conf spark.yarn.am.extraJavaOptions="-Dlog4j.configuration=log4j-all.properties" \
-  --conf spark.yarn.cluster.driver.extraJavaOptions="-Dlog4j.configuration=log4j-all.properties" \
+  --conf spark.driver.extraJavaOptions="-Dlog4j.configuration=log4j-all.properties -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m" \
+  --conf spark.executor.extraJavaOptions="-Dlog4j.configuration=log4j-all.properties -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m" \
+  --conf spark.yarn.am.extraJavaOptions="-Dlog4j.configuration=log4j-all.properties -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m" \
+  --conf spark.yarn.cluster.driver.extraJavaOptions="-Dlog4j.configuration=log4j-all.properties -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m" \
   --files log4j-all.properties,config.yml \
   --conf spark.yarn.maxAppAttempts=1 \
-  --conf spark.sql.shuffle.partitions=600 \
+  --conf spark.sql.shuffle.partitions=1200 \
   --conf spark.shuffle.io.maxRetries=10 \
-  ./spark-1.0-jar-with-dependencies.jar config.yml >log 2>&1 &
+  ./spark-1.0-jar-with-dependencies.jar config.yml bid_obs_tmp >log 2>&1 &
