ef27634ce33fbec7c64696e042fa628fdb21981a
liang <li_ang_666@163.com>
2023-06-27 20:29:14 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Nfs.java
+++ repair/src/main/java/com/liang/repair/test/Nfs.java
@@ -1,18 +1,30 @@
 package com.liang.repair.test;
 
+import lombok.AllArgsConstructor;
+
 public class Nfs {
     public static void main(String[] args) {
-        String pt = "20230618";
-        String orgId = "wangzhitianyuanT7";
-        String tableName = "company_tm";
+        printShell(new Target("20230619", "wangzhitianyuanT7", "company_tm"));
+    }
 
+    private static void printShell(Target target) {
+        String pt = target.pt;
+        String orgId = target.orgId;
+        String tableName = target.tableName;
         String fromDir = String.format("/nfs/open_data/dblog/%s/all_table/%s/_c1=%s", pt, pt, tableName);
         String toDir = String.format("/nfs/ftp/databases3/%s/%s/_c1=%s", orgId, pt, tableName);
 
-        System.out.println(String.format("ls %s # fromDir", fromDir));
-        System.out.println(String.format("ls %s # toDir", toDir));
+        System.out.println(String.format("# ls %s # fromDir", fromDir));
+        System.out.println(String.format("# ls %s # toDir", toDir));
 
         System.out.println(String.format("rm -rf %s/*", toDir));
         System.out.println(String.format("cp -r %s/* %s/", fromDir, toDir));
+    }
+
+    @AllArgsConstructor
+    private static final class Target {
+        private String pt;
+        private String orgId;
+        private String tableName;
     }
 }
