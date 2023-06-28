f250d9b881a04f2522766f792bf8f56904d3a296
liang <li_ang_666@163.com>
2023-06-28 21:28:29 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/DorisBitmapTest.java
+++ repair/src/main/java/com/liang/repair/impl/DorisBitmapTest.java
@@ -1,36 +1,98 @@
 package com.liang.repair.impl;
 
-import com.liang.common.dto.DorisOneRow;
-import com.liang.common.dto.DorisSchema;
-import com.liang.common.service.database.template.DorisTemplate;
 import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.repair.trait.Runner;
+import org.roaringbitmap.RoaringBitmap;
+import org.roaringbitmap.longlong.Roaring64NavigableMap;
 
-import java.util.Collections;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.sql.ResultSet;
+import java.util.List;
 
 public class DorisBitmapTest implements Runner {
 
     @Override
     public void run(String[] args) throws Exception {
         JdbcTemplate doris = new JdbcTemplate("doris");
-        DorisTemplate dorisTemplate = new DorisTemplate("dorisSink");
-        DorisSchema dorisSchema = DorisSchema.builder()
-                .database("test_db")
-                .tableName("bitmap_test")
-                .derivedColumns(Collections.singletonList("btmp = to_bitmap(btmp)"))
-                .build();
-        DorisOneRow row1 = new DorisOneRow(dorisSchema)
-                .put("id", 1)
-                .put("btmp", null);
-        DorisOneRow row2 = new DorisOneRow(dorisSchema)
-                .put("id", 1)
-                .put("btmp", 243);
-        DorisOneRow row3 = new DorisOneRow(dorisSchema)
-                .put("id", 1)
-                .put("btmp", 287667876573L);
-        DorisOneRow row4 = new DorisOneRow(dorisSchema)
-                .put("id", 1)
-                .put("btmp", 2222222222L);
-        dorisTemplate.update(row1, row2, row3,row4);
+        doris.update("INSERT INTO bitmap_test VALUES\n" +
+                " (0, bitmap_empty()),\n" +
+                " (1, to_bitmap(243)),\n" +
+                " (2, bitmap_from_array([1,2,3,4,5,434543])),\n" +
+                " (3, to_bitmap(287667876573)),\n" +
+                " (4, bitmap_from_array([487667876573, 387627876573, 987567876573, 187667876573]));");
+
+        doris.update("set global return_object_data_as_binary=true");
+        List<Object> bitmaps = doris.queryForList("select btmp from bitmap_test", this::parseBitmap);
+        System.out.println(bitmaps);
+    }
+
+    private Object[] decodeVarint64(byte[] bt) { // nolint
+        long result = 0;
+        int shift = 0;
+        short B = 128;
+        int idx = 1;
+        for (; ; ) {
+            short readByte = bt[idx];
+            idx++;
+            boolean isEnd = (readByte & B) == 0;
+            result |= (long) (readByte & (B - 1)) << (shift * 7);
+            if (isEnd) {
+                break;
+            }
+            shift++;
+        }
+        byte[] bytes = new byte[8];
+        for (int i = 0; i < bytes.length; i++) {
+            // LITTLE_ENDIAN
+            bytes[i] = (byte) (result >> 8 * i);
+        }
+        return new Object[]{bytes, idx};
+    }
+
+    private Object parseBitmap(ResultSet resultSet) throws Exception {
+        byte[] bytes = resultSet.getBytes(1);
+
+        RoaringBitmap bitmap32 = new RoaringBitmap();
+        // Only Roaring64NavigableMap can work, Roaring64Bitmap can't work!!!
+        Roaring64NavigableMap bitmap64 = new Roaring64NavigableMap();
+        switch (bytes[0]) {
+            case 0: // for empty bitmap
+                break;
+            case 1: // for only 1 element in bitmap32
+                bitmap32.add(ByteBuffer.wrap(bytes, 1, bytes.length - 1)
+                        .order(ByteOrder.LITTLE_ENDIAN)
+                        .getInt());
+                break;
+            case 2: // for more than 1 element in bitmap32
+                bitmap32.deserialize(ByteBuffer.wrap(bytes, 1, bytes.length - 1));
+                break;
+            case 3: // for only 1 element in bitmap64
+                bitmap64.add(ByteBuffer.wrap(bytes, 1, bytes.length - 1)
+                        .order(ByteOrder.LITTLE_ENDIAN)
+                        .getLong());
+                break;
+            case 4: // for more than 1 element in bitmap64
+                Object[] tuple2 = decodeVarint64(bytes);
+                int offset = (int) tuple2[1];
+                int newLen = 8 + bytes.length - offset;
+
+                try (ByteArrayOutputStream baos = new ByteArrayOutputStream(newLen);
+                     DataOutputStream dos = new DataOutputStream(baos)) {
+                    dos.write((byte[]) tuple2[0]);
+                    dos.write(bytes, offset, bytes.length - offset);
+                    dos.flush();
+                    try (DataInputStream dis = new DataInputStream(
+                            new ByteArrayInputStream(baos.toByteArray()))) {
+                        bitmap64.deserializePortable(dis);
+                    }
+                }
+                break;
+        }
+        return bytes[0] <= 2 ? bitmap32 : bitmap64;
     }
 }
---
+++ spark/src/main/java/com/liang/spark/job/DirtyCompanyJob.java
@@ -0,0 +1,24 @@
+package com.liang.spark.job;
+
+import com.liang.spark.basic.SparkSessionFactory;
+import org.apache.spark.sql.SparkSession;
+
+public class DirtyCompanyJob {
+    public static void main(String[] args) throws Exception {
+        SparkSession spark = SparkSessionFactory.createSpark(args);
+
+        spark.sql("use test");
+        spark.sql("select name from ods.ods_prism1_company_df where pt='20230627' group by name")
+                .createOrReplaceTempView("t1");
+        spark.sql("select dirty_company from test.test_dirty_company_df")
+                .createOrReplaceTempView("t2");
+        spark.sql("insert overwrite table test.test_dirty_company_df_result_copy_new partition(pt='20230627') select /*+ REPARTITION(600) */ name from t1 left semi join t2 on name like CONCAT('%', dirty_company))");
+//        spark.sql("insert overwrite table test.test_dirty_company_df_result_copy_new partition(pt='20230627')\n" +
+//                "select /*+ REPARTITION(600) */ name from\n" +
+//                "(select name from ods.ods_prism1_company_df where pt='20230627' group by name)t1\n" +
+//                "join\n" +
+//                "(select dirty_company from test.test_dirty_company_df)t2\n" +
+//                "on t1.name  LIKE CONCAT('%', t2.dirty_company)");
+        spark.stop();
+    }
+}
