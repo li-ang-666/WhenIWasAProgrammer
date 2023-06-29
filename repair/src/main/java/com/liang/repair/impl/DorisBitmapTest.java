12b0bb4fb2782a6d1fa0b1479b7ac4c07eb7ecc6
liang <li_ang_666@163.com>
2023-06-29 17:31:18 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/DorisBitmapTest.java
+++ repair/src/main/java/com/liang/repair/impl/DorisBitmapTest.java
@@ -13,6 +13,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.sql.ResultSet;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class DorisBitmapTest implements Runner {
 
@@ -24,45 +25,20 @@ public class DorisBitmapTest implements Runner {
                 " (1, to_bitmap(243)),\n" +
                 " (2, bitmap_from_array([1,2,3,4,5,434543])),\n" +
                 " (3, to_bitmap(287667876573)),\n" +
-                " (4, bitmap_from_array([487667876573, 387627876573, 987567876573, 187667876573]));");
+                " (4, bitmap_from_array([487667876573, 387627876573, 987567876573, 187667876573]))");
 
         doris.update("set global return_object_data_as_binary=true");
         List<Object> bitmaps = doris.queryForList("select btmp from bitmap_test", this::parseBitmap);
         System.out.println(bitmaps);
-
+        doris.queryForColumnMaps("select btmp from bitmap_test");
 //        List<Object> lst = doris.queryForList("select btmp from bitmap_test", rs -> rs.getObject(1));
 //        for (Object o : lst) {
 //            System.out.println(o.getClass());
 //        }
     }
 
-    private Object[] decodeVarInt64(byte[] bt) { // nolint
-        long result = 0;
-        int shift = 0;
-        short B = 128;
-        int idx = 1;
-        for (; ; ) {
-            short readByte = bt[idx];
-            idx++;
-            boolean isEnd = (readByte & B) == 0;
-            result |= (long) (readByte & (B - 1)) << (shift * 7);
-            if (isEnd) {
-                break;
-            }
-            shift++;
-        }
-        byte[] bytes = new byte[8];
-        for (int i = 0; i < bytes.length; i++) {
-            // LITTLE_ENDIAN
-            bytes[i] = (byte) (result >> 8 * i);
-        }
-        return new Object[]{bytes, idx};
-    }
-
     private Object parseBitmap(ResultSet resultSet) throws Exception {
-        //byte[] bytes = resultSet.getBytes(1);
-        byte[] bytes = resultSet.getString(1).getBytes();
-
+        byte[] bytes = resultSet.getBytes(1);
         RoaringBitmap bitmap32 = new RoaringBitmap();
         // Only Roaring64NavigableMap can work, Roaring64Bitmap can't work!!!
         Roaring64NavigableMap bitmap64 = new Roaring64NavigableMap();
@@ -99,6 +75,32 @@ public class DorisBitmapTest implements Runner {
                 }
                 break;
         }
-        return bytes[0] <= 2 ? bitmap32 : bitmap64;
+        if (bytes[0] <= 2) {
+            bitmap32.forEach((Consumer<Integer>) bitmap64::addInt);
+        }
+        return bitmap64;
+    }
+
+    private Object[] decodeVarInt64(byte[] bt) { // nolint
+        long result = 0;
+        int shift = 0;
+        short B = 128;
+        int idx = 1;
+        for (; ; ) {
+            short readByte = bt[idx];
+            idx++;
+            boolean isEnd = (readByte & B) == 0;
+            result |= (long) (readByte & (B - 1)) << (shift * 7);
+            if (isEnd) {
+                break;
+            }
+            shift++;
+        }
+        byte[] bytes = new byte[8];
+        for (int i = 0; i < bytes.length; i++) {
+            // LITTLE_ENDIAN
+            bytes[i] = (byte) (result >> 8 * i);
+        }
+        return new Object[]{bytes, idx};
     }
 }
