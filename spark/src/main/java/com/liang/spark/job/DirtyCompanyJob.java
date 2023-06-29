00ec1b9e1955b7b29e9c9624847c7bc17897d941
liang <li_ang_666@163.com>
2023-06-29 09:49:16 +0800

    1

--- spark/src/main/java/com/liang/spark/job/DirtyCompanyJob.java
+++ spark/src/main/java/com/liang/spark/job/DirtyCompanyJob.java
@@ -12,7 +12,9 @@ public class DirtyCompanyJob {
                 .createOrReplaceTempView("t1");
         spark.sql("select distinct dirty_company from test.test_dirty_company_df")
                 .createOrReplaceTempView("t2");
-        spark.sql("insert overwrite table test.test_dirty_company_df_result_copy_new partition(pt='20230628') select /*+ REPARTITION(600) */ name from t1 left semi join t2 on substr(reverse(name),0,1) = substr(reverse(dirty_company),0,1) and substr(reverse(name),0,length(dirty_company)) = reverse(dirty_company)");
+        spark.sql("insert overwrite table test.test_dirty_company_df_result_copy_new partition(pt='20230628')" +
+                " select /*+ REPARTITION(600) */ name from t1 left semi join t2" +
+                " on substr(reverse(name),0,1) = substr(reverse(dirty_company),0,1) and substr(reverse(name),0,length(dirty_company)) = reverse(dirty_company)");
 //        spark.sql("insert overwrite table test.test_dirty_company_df_result_copy_new partition(pt='20230627')\n" +
 //                "select /*+ REPARTITION(600) */ name from\n" +
 //                "(select name from ods.ods_prism1_company_df where pt='20230627' group by name)t1\n" +
