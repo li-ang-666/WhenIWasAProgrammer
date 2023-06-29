8d4b1f7009527f2074cfd4fcf3a27423478fcb59
liang <li_ang_666@163.com>
2023-06-29 18:02:31 +0800

    1

--- spark/src/main/java/com/liang/spark/udf/GetHoliday.java
+++ spark/src/main/java/com/liang/spark/udf/GetHoliday.java
@@ -1,14 +1,12 @@
 package com.liang.spark.udf;
 
-import org.apache.hadoop.hive.ql.exec.UDF;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class GetHoliday extends UDF {
+public class GetHoliday /*extends UDF*/ {
     public String evaluate(String dt) {
         if (dt == null || dt.trim().isEmpty())
             return null;
--- spark/src/main/java/com/liang/spark/udf/Holiday.java
+++
@@ -1,9 +0,0 @@
-package com.liang.spark.udf;
-
-import org.apache.hadoop.hive.ql.exec.UDF;
-
-public class Holiday extends UDF {
-    public String evaluate(String dt) {
-        return "aaaa";
-    }
-}
