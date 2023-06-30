80e29804287740f40e30a9bee09598363f735d96
liang <li_ang_666@163.com>
2023-06-30 17:36:37 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -1,7 +1,5 @@
 package com.liang.repair.test;
 
-import com.ctrip.framework.apollo.core.dto.ApolloConfig;
-
 public class Test {
     public static void main(String[] args) throws Exception {
     }
