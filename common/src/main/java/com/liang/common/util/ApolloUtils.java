f040e6cebf65d3eb5f8fd7857297eec1cf0fa529
liang <li_ang_666@163.com>
2023-06-30 16:15:14 +0800

    1

--- common/src/main/java/com/liang/common/util/ApolloUtils.java
+++ common/src/main/java/com/liang/common/util/ApolloUtils.java
@@ -1,17 +1,34 @@
 package com.liang.common.util;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
 import com.ctrip.framework.apollo.ConfigService;
+import com.ctrip.framework.apollo.model.ConfigChange;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
 @UtilityClass
+@Slf4j
 public class ApolloUtils {
-    public static void main(String[] args) {
-        f();
+    private static final Config apollo;
+
+    static {
+        // http://apollo.jindidata.com
+        System.setProperty("apollo.meta", "http://apollo.middleware.huawei:8080");
+        System.setProperty("app.id", "ApolloUtils");
+        apollo = ConfigService.getAppConfig();
+        apollo.addChangeListener(new ConfigChangeListener() {
+            @Override
+            public void onChange(ConfigChangeEvent configChangeEvent) {
+                for (String key : configChangeEvent.changedKeys()) {
+                    ConfigChange change = configChangeEvent.getChange(key);
+                    log.info("{} {}, {} -> {}", change.getPropertyName(), change.getChangeType(), change.getOldValue(), change.getNewValue());
+                }
+            }
+        });
     }
 
-    public static void f() {
-        System.setProperty("apollo.meta", "http://apollo.middleware.huawei:8080");
-        System.setProperty("app.id", "config-apollo");
-        System.out.println(ConfigService.getAppConfig().getPropertyNames());
+    public static void main(String[] args) {
     }
 }
