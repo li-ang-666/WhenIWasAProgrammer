822cdac6b35c210dd79cc5b1403fecd11ea56726
liang <li_ang_666@163.com>
2023-06-30 15:10:56 +0800

    1

---
+++ spark/src/main/java/com/liang/spark/basic/SparkTester.java
@@ -0,0 +1,29 @@
+package com.liang.spark.basic;
+
+import lombok.experimental.UtilityClass;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+@UtilityClass
+public class SparkTester {
+    public static void test(SparkConsumer consumer) {
+        SparkSession spark = SparkSession.builder()
+                .config("spark.debug.maxToStringFields", "200")
+                .master("local[*]")
+                .getOrCreate();
+        consumer.consume(spark);
+    }
+
+    public static Dataset<Row> getCSVTable(SparkSession spark, String fileName) {
+        return spark.read()
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .csv("/Users/liang/Desktop/WhenIWasAProgrammer/spark/src/main/resources/" + fileName);
+    }
+
+    @FunctionalInterface
+    public interface SparkConsumer {
+        void consume(SparkSession sparkSession);
+    }
+}
--- spark/src/main/java/com/liang/spark/test/SparkTest.java
+++ spark/src/main/java/com/liang/spark/test/SparkTest.java
@@ -1,29 +1,15 @@
 package com.liang.spark.test;
 
-import com.liang.spark.udf.CountDistinct;
+import com.liang.spark.basic.SparkTester;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.spark.sql.SparkSession;
 
 
 @Slf4j
 public class SparkTest {
     public static void main(String[] args) throws Exception {
-        SparkSession spark = SparkSession
-                .builder()
-                .config("spark.sql.autoBroadcastJoinThreshold", "-1")
-                .config("sspark.sql.shuffle.partitions", "20")
-                .master("local[*]")
-                .getOrCreate();
-        spark.read()
-                .option("header", "true")
-                .option("inferSchema", "true")
-                .csv("/Users/liang/Desktop/WhenIWasAProgrammer/spark/src/main/resources/tb.csv")
-                .createTempView("t");
-        spark.udf().register("countd", new CountDistinct());
-
-        spark.sql("explain select col_dim,count(distinct col_a),count(distinct col_b),max(col_c),min(col_d) from t group by col_dim")
-                .show(false);
-
-        spark.stop();
+        SparkTester.test(spark -> {
+            SparkTester.getCSVTable(spark, "tb.csv")
+                    .show(false);
+        });
     }
 }
