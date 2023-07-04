8854fb830b0c494fe429c4d101a733ec2deea1a1
liang <li_ang_666@163.com>
2023-07-04 09:53:39 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -1,5 +1,17 @@
 package com.liang.repair.test;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.apache.calcite.adapter.jdbc.JdbcSchema;
+import org.apache.calcite.jdbc.CalciteConnection;
+import org.apache.calcite.schema.Schema;
+import org.apache.calcite.schema.SchemaPlus;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
 public class Test {
     public static void main(String[] args) throws Exception {
 
