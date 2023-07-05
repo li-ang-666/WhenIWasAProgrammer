a9a32ef130c19ea116b1a9ca18648d6cc38808a7
liang <li_ang_666@163.com>
2023-07-05 14:21:23 +0800

    1

--- flink/src/main/java/com/liang/flink/job/DimCountJob.java
+++ flink/src/main/java/com/liang/flink/job/DimCountJob.java
@@ -2,6 +2,7 @@ package com.liang.flink.job;
 
 import com.liang.common.dto.Config;
 import com.liang.common.dto.HbaseOneRow;
+import com.liang.common.service.database.template.HbaseTemplate;
 import com.liang.common.util.ConfigUtils;
 import com.liang.flink.basic.StreamEnvironmentFactory;
 import com.liang.flink.dto.SingleCanalBinlog;
@@ -14,19 +15,25 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 
+import java.util.List;
+
 @Slf4j
 public class DimCountJob {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamEnvironmentFactory.create(args);
         DataStream<SingleCanalBinlog> sourceStream = StreamFactory.create(env);
         Config config = ConfigUtils.getConfig();
-        sourceStream.addSink()
+        sourceStream.addSink(new DimCountSink(config))
+                .setParallelism(config.getFlinkConfig().getOtherParallel())
+                .name("HbaseSink");
+        env.execute("DimCountJob");
     }
 
     @Slf4j
     private final static class DimCountSink extends RichSinkFunction<SingleCanalBinlog> {
         private final Config config;
         private DataUpdateService<HbaseOneRow> service;
+        private HbaseTemplate hbaseTemplate;
 
         public DimCountSink(Config config) {
             this.config = config;
@@ -40,11 +47,13 @@ public class DimCountJob {
                     .addClass("EntityBeneficiaryDetails")
                     .addClass("EntityControllerDetails");
             service = new DataUpdateService<>(context);
+            hbaseTemplate = new HbaseTemplate("hbaseSink");
         }
 
         @Override
         public void invoke(SingleCanalBinlog singleCanalBinlog, Context context) throws Exception {
-            service.invoke(singleCanalBinlog)
+            List<HbaseOneRow> hbaseRows = service.invoke(singleCanalBinlog);
+            hbaseTemplate.update(hbaseRows);
         }
     }
 }
