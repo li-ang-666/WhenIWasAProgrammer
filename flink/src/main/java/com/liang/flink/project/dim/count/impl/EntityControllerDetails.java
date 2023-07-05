621017a3a97410fcfaab6af4719671f9cd13f1ae
liang <li_ang_666@163.com>
2023-07-05 15:14:06 +0800

    1

--- flink/src/main/java/com/liang/flink/project/dim/count/impl/EntityControllerDetails.java
+++ flink/src/main/java/com/liang/flink/project/dim/count/impl/EntityControllerDetails.java
@@ -1,18 +1,32 @@
 package com.liang.flink.project.dim.count.impl;
 
 import com.liang.common.dto.HbaseOneRow;
+import com.liang.common.dto.HbaseSchema;
 import com.liang.flink.dto.SingleCanalBinlog;
 import com.liang.flink.project.dim.count.dao.EntityControllerDetailsDao;
 import com.liang.flink.service.data.update.AbstractDataUpdate;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class EntityControllerDetails extends AbstractDataUpdate<HbaseOneRow> {
     private final EntityControllerDetailsDao dao = new EntityControllerDetailsDao();
 
     @Override
     public List<HbaseOneRow> updateWithReturn(SingleCanalBinlog singleCanalBinlog) {
-        return super.updateWithReturn(singleCanalBinlog);
+        Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+        String shareholderId = String.valueOf(columnMap.get("tyc_unique_entity_id"));
+        String count = dao.queryCount(shareholderId);
+        HbaseSchema hbaseSchema = HbaseSchema.builder()
+                .namespace("prism_c")
+                .tableName("company_all_count")
+                .columnFamily("count")
+                .rowKeyReverse(true)
+                .build();
+        HbaseOneRow row = new HbaseOneRow(hbaseSchema, shareholderId)
+                .put("entity_controller_details_count", count);
+        return Collections.singletonList(row);
     }
 
     @Override
