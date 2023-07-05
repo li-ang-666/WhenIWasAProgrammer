e3c769ac0070cb74f00432507467c65e57037697
liang <li_ang_666@163.com>
2023-07-05 14:23:35 +0800

    1

--- flink/src/main/java/com/liang/flink/project/dim/count/impl/EntityBeneficiaryDetails.java
+++ flink/src/main/java/com/liang/flink/project/dim/count/impl/EntityBeneficiaryDetails.java
@@ -1,4 +1,19 @@
 package com.liang.flink.project.dim.count.impl;
 
-public class EntityBeneficiaryDetails {
+import com.liang.common.dto.HbaseOneRow;
+import com.liang.flink.dto.SingleCanalBinlog;
+import com.liang.flink.service.data.update.AbstractDataUpdate;
+
+import java.util.List;
+
+public class EntityBeneficiaryDetails extends AbstractDataUpdate<HbaseOneRow> {
+    @Override
+    public List<HbaseOneRow> updateWithReturn(SingleCanalBinlog singleCanalBinlog) {
+        return super.updateWithReturn(singleCanalBinlog);
+    }
+
+    @Override
+    public List<HbaseOneRow> deleteWithReturn(SingleCanalBinlog singleCanalBinlog) {
+        return updateWithReturn(singleCanalBinlog);
+    }
 }
--- flink/src/main/java/com/liang/flink/project/dim/count/impl/EntityControllerDetails.java
+++ flink/src/main/java/com/liang/flink/project/dim/count/impl/EntityControllerDetails.java
@@ -1,4 +1,19 @@
 package com.liang.flink.project.dim.count.impl;
 
-public class EntityControllerDetails {
+import com.liang.common.dto.HbaseOneRow;
+import com.liang.flink.dto.SingleCanalBinlog;
+import com.liang.flink.service.data.update.AbstractDataUpdate;
+
+import java.util.List;
+
+public class EntityControllerDetails extends AbstractDataUpdate<HbaseOneRow> {
+    @Override
+    public List<HbaseOneRow> updateWithReturn(SingleCanalBinlog singleCanalBinlog) {
+        return super.updateWithReturn(singleCanalBinlog);
+    }
+
+    @Override
+    public List<HbaseOneRow> deleteWithReturn(SingleCanalBinlog singleCanalBinlog) {
+        return updateWithReturn(singleCanalBinlog);
+    }
 }
