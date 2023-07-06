96bbf1be91ba2c4791af6a7ec26114907a79727f
liang <li_ang_666@163.com>
2023-07-06 12:44:45 +0800

    1

--- flink/src/main/java/com/liang/flink/test/FlinkTest.java
+++ flink/src/main/java/com/liang/flink/test/FlinkTest.java
@@ -4,11 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FlinkTest {
-    private final static String AK = "NT5EWZ4FRH54R2R2CB8G";
-    private final static String SK = "BJok3jQFTmFYUS68lFWegazYggw5anKsOFUb65bS";
-    private final static String EP = "obs.cn-north-4.myhuaweicloud.com";
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
     }
 }
