503b6cf5bfb693b2e28077c486cc03f6ac854d31
liang <li_ang_666@163.com>
2023-07-10 16:41:11 +0800

    1

--- flink/src/main/java/com/liang/flink/project/no/thareholder/company/info/impl/CompanyBondPlates.java
+++ flink/src/main/java/com/liang/flink/project/no/thareholder/company/info/impl/CompanyBondPlates.java
@@ -3,7 +3,7 @@ package com.liang.flink.project.no.thareholder.company.info.impl;
 import com.liang.flink.dto.SingleCanalBinlog;
 import com.liang.flink.project.no.thareholder.company.info.dao.NoShareholderDao;
 import com.liang.flink.service.data.update.AbstractDataUpdate;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
