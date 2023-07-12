86a3f16b01bf0c81edbbffa4acfb48ae7b5bd70d
liang <li_ang_666@163.com>
2023-07-12 16:20:48 +0800

    1

--- flink/src/main/java/com/liang/flink/project/bdp/equity/dao/BdpEquityDao.java
+++ flink/src/main/java/com/liang/flink/project/bdp/equity/dao/BdpEquityDao.java
@@ -51,4 +51,32 @@ public class BdpEquityDao {
                 .toString();
         jdbcTemplate.update(sql);
     }
+
+    public void updateShareholderName(String shareholderId, String name) {
+        String update1 = new SQL()
+                .UPDATE("entity_beneficiary_details")
+                .SET("entity_name_beneficiary = " + formatValue(name))
+                .WHERE("tyc_unique_entity_id_beneficiary = " + formatValue(shareholderId))
+                .toString();
+        String update2 = new SQL()
+                .UPDATE("entity_controller_details")
+                .SET("entity_name_valid = " + formatValue(name))
+                .WHERE("tyc_unique_entity_id = " + formatValue(shareholderId))
+                .toString();
+        jdbcTemplate.update(update1, update2);
+    }
+
+    public void updateCompanyName(String companyId, String companyName) {
+        String update1 = new SQL()
+                .UPDATE("entity_beneficiary_details")
+                .SET("entity_name_valid = " + formatValue(companyName))
+                .WHERE("tyc_unique_entity_id = " + formatValue(companyId))
+                .toString();
+        String update2 = new SQL()
+                .UPDATE("entity_controller_details")
+                .SET("company_name_controlled = " + formatValue(companyName))
+                .WHERE("company_id_controlled = " + formatValue(companyId))
+                .toString();
+        jdbcTemplate.update(update1, update2);
+    }
 }
