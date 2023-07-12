da9f7e4215f326f208855959a9ab4269a172b441
liang <li_ang_666@163.com>
2023-07-12 15:56:05 +0800

    1

---
+++ flink/src/main/java/com/liang/flink/project/bdp/equity/dao/BdpEquityDao.java
@@ -0,0 +1,52 @@
+package com.liang.flink.project.bdp.equity.dao;
+
+import com.liang.common.service.database.template.JdbcTemplate;
+import com.liang.common.util.SqlUtils;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.ibatis.jdbc.SQL;
+
+import java.util.Map;
+
+import static com.liang.common.util.SqlUtils.formatValue;
+
+public class BdpEquityDao {
+    private final JdbcTemplate jdbcTemplate = new JdbcTemplate("bdpEquity");
+
+    public void deleteAll(String id, String companyId, String shareholderId) {
+        String delete1 = new SQL()
+                .DELETE_FROM("entity_beneficiary_details")
+                .WHERE("id = " + formatValue(id))
+                .toString();
+        String delete2 = new SQL()
+                .DELETE_FROM("entity_beneficiary_details")
+                .WHERE("tyc_unique_entity_id = " + formatValue(companyId))
+                .WHERE("tyc_unique_entity_id_beneficiary = " + formatValue(shareholderId))
+                .toString();
+        String delete3 = new SQL()
+                .DELETE_FROM("entity_controller_details")
+                .WHERE("id = " + formatValue(id))
+                .toString();
+        String delete4 = new SQL()
+                .DELETE_FROM("entity_controller_details")
+                .WHERE("company_id_controlled = " + formatValue(companyId))
+                .WHERE("tyc_unique_entity_id = " + formatValue(shareholderId))
+                .toString();
+        jdbcTemplate.update(delete1, delete2, delete3, delete4);
+    }
+
+    public String getEntityName(String entityId) {
+        String sql = new SQL()
+                .SELECT("entity_name_valid")
+                .FROM("tyc_entity_main_reference")
+                .WHERE("tyc_unique_entity_id = " + formatValue(entityId))
+                .toString();
+        return jdbcTemplate.queryForObject(sql, rs -> rs.getString(1));
+    }
+
+    public void replaceInto(String tableName, Map<String, Object> columnMap) {
+        Tuple2<String, String> insert = SqlUtils.columnMap2Insert(columnMap);
+        String sql = new SQL()
+        String replaceSql = String.format("replace into entity_beneficiary_details(%s)values(%s)", insert.f0, insert.f1);
+        jdbcTemplate.update(replaceSql);
+    }
+}
---
+++ flink/src/main/java/com/liang/flink/project/bdp/equity/impl/RatioPathCompany.java
@@ -0,0 +1,125 @@
+package com.liang.flink.project.bdp.equity.impl;
+
+import com.liang.common.util.SqlUtils;
+import com.liang.flink.dto.SingleCanalBinlog;
+import com.liang.flink.project.bdp.equity.dao.BdpEquityDao;
+import com.liang.flink.service.data.update.AbstractDataUpdate;
+import com.liang.flink.utils.BuildTab3Path;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.ibatis.jdbc.SQL;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.alibaba.otter.canal.protocol.CanalEntry.EventType.DELETE;
+import static com.liang.common.util.SqlUtils.formatValue;
+
+public class RatioPathCompany extends AbstractDataUpdate<SQL> {
+    private final BdpEquityDao dao = new BdpEquityDao();
+
+    @Override
+    public List<SQL> updateWithReturn(SingleCanalBinlog singleCanalBinlog) {
+        Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+        String id = String.valueOf(columnMap.get("id"));
+        String companyId = String.valueOf(columnMap.get("company_id"));
+        String shareholderId = String.valueOf(columnMap.get("shareholder_id"));
+        dao.deleteAll(id, companyId, shareholderId);
+        String isDeleted = String.valueOf(columnMap.get("is_deleted"));
+        //删除
+        if (singleCanalBinlog.getEventType() == DELETE || "1".equals(isDeleted)) {
+            return new ArrayList<>();
+        }
+        //写入受益所有人
+        String isUltimate = String.valueOf(columnMap.get("is_ultimate"));
+        if ("1".equals(isUltimate)) {
+            parseIntoEntityBeneficiaryDetails(singleCanalBinlog);
+        }
+        //写入实际控制人
+        String isController = String.valueOf(columnMap.get("is_controller"));
+        if ("1".equals(isController)) {
+            parseIntoEntityControllerDetails(singleCanalBinlog);
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<SQL> deleteWithReturn(SingleCanalBinlog singleCanalBinlog) {
+        return updateWithReturn(singleCanalBinlog);
+    }
+
+    // entity_beneficiary_details
+    // unique (tyc_unique_entity_id_beneficiary, tyc_unique_entity_id)
+    private void parseIntoEntityBeneficiaryDetails(SingleCanalBinlog singleCanalBinlog) {
+        Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+        String companyId = String.valueOf(columnMap.get("company_id"));
+        String shareholderId = String.valueOf(columnMap.get("shareholder_id"));
+        String id = String.valueOf(columnMap.get("id"));
+        String investmentRatioTotal = String.valueOf(columnMap.get("investment_ratio_total"));
+        String equityHoldingPath = String.valueOf(columnMap.get("equity_holding_path"));
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("id", id);
+        resultMap.put("tyc_unique_entity_id", companyId);
+        resultMap.put("tyc_unique_entity_id_beneficiary", shareholderId);
+        //公司名字
+        String companyName = dao.getEntityName(companyId);
+        if (companyName == null) {
+            companyName = "";
+        }
+        resultMap.put("entity_name_valid", companyName);
+        //股东名字
+        String shareholderName = dao.getEntityName(shareholderId);
+        if (shareholderName == null) {
+            shareholderName = "";
+        }
+        resultMap.put("entity_name_beneficiary", shareholderName);
+        //其它信息
+        BuildTab3Path.PathNode pathNode = BuildTab3Path.buildTab3PathSafe(shareholderId, equityHoldingPath);
+        resultMap.put("equity_relation_path_cnt", pathNode.getCount());
+        resultMap.put("beneficiary_equity_relation_path_detail", pathNode.getPathStr());
+        resultMap.put("estimated_equity_ratio_total", investmentRatioTotal);
+        resultMap.put("beneficiary_validation_time_year", 2023);
+        resultMap.put("entity_type_id", 1);
+        Tuple2<String, String> insert = SqlUtils.columnMap2Insert(resultMap);
+        String replaceSql = String.format("replace into entity_beneficiary_details(%s)values(%s)", insert.f0, insert.f1);
+        jdbcTemplate.update(replaceSql);
+    }
+
+    // entity_controller_details
+    // unique (tyc_unique_entity_id, company_id_controlled)
+    private void parseIntoEntityControllerDetails(SingleCanalBinlog singleCanalBinlog) {
+        Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+        String companyId = String.valueOf(columnMap.get("company_id"));
+        String shareholderId = String.valueOf(columnMap.get("shareholder_id"));
+        String id = String.valueOf(columnMap.get("id"));
+        String investmentRatioTotal = String.valueOf(columnMap.get("investment_ratio_total"));
+        String equityHoldingPath = String.valueOf(columnMap.get("equity_holding_path"));
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("id", id);
+        resultMap.put("company_id_controlled", companyId);
+        resultMap.put("tyc_unique_entity_id", shareholderId);
+        //公司名字
+        String companyName = jdbcTemplate.queryForObject(String.format("select entity_name_valid from tyc_entity_main_reference where tyc_unique_entity_id = %s", formatValue(companyId)), rs -> rs.getString(1));
+        if (companyName == null) {
+            companyName = "";
+        }
+        resultMap.put("company_name_controlled", companyName);
+        //股东名字
+        String shareholderName = jdbcTemplate.queryForObject(String.format("select entity_name_valid from tyc_entity_main_reference where tyc_unique_entity_id = %s", formatValue(shareholderId)), rs -> rs.getString(1));
+        if (shareholderName == null) {
+            shareholderName = "";
+        }
+        resultMap.put("entity_name_valid", shareholderName);
+        //其他信息
+        BuildTab3Path.PathNode pathNode = BuildTab3Path.buildTab3PathSafe(shareholderId, equityHoldingPath);
+        resultMap.put("equity_relation_path_cnt", pathNode.getCount());
+        resultMap.put("controlling_equity_relation_path_detail", pathNode.getPathStr());
+        resultMap.put("estimated_equity_ratio_total", investmentRatioTotal);
+        resultMap.put("control_validation_time_year", 2023);
+        resultMap.put("entity_type_id", String.valueOf(columnMap.get("shareholder_entity_type")));
+        Tuple2<String, String> insert = SqlUtils.columnMap2Insert(resultMap);
+        String replaceSql = String.format("replace into entity_controller_details(%s)values(%s)", insert.f0, insert.f1);
+        jdbcTemplate.update(replaceSql);
+    }
+}
