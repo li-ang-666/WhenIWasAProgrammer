5a3d7e8447b40644c6356644090dd3c6b8f3225a
liang <li_ang_666@163.com>
2023-07-13 09:40:14 +0800

    1

--- flink/src/main/java/com/liang/flink/project/bdp/equity/dao/BdpEquityDao.java
+++ flink/src/main/java/com/liang/flink/project/bdp/equity/dao/BdpEquityDao.java
@@ -63,18 +63,23 @@ public class BdpEquityDao {
         jdbcTemplate.update(sql);
     }
 
-    public void updateShareholderName(String shareholderId, String name) {
+    public void updateShareholderName(String shareholderId, String shareholderName) {
         String update1 = new SQL()
                 .UPDATE("entity_beneficiary_details")
-                .SET("entity_name_beneficiary = " + formatValue(name))
+                .SET("entity_name_beneficiary = " + formatValue(shareholderName))
                 .WHERE("tyc_unique_entity_id_beneficiary = " + formatValue(shareholderId))
                 .toString();
         String update2 = new SQL()
                 .UPDATE("entity_controller_details")
-                .SET("entity_name_valid = " + formatValue(name))
+                .SET("entity_name_valid = " + formatValue(shareholderName))
                 .WHERE("tyc_unique_entity_id = " + formatValue(shareholderId))
                 .toString();
-        jdbcTemplate.update(update1, update2);
+        String update3 = new SQL()
+                .UPDATE("tyc_entity_main_reference")
+                .SET("entity_name_valid_with_shareholder_identity_type = " + formatValue(shareholderName))
+                .WHERE("tyc_unique_entity_id_with_shareholder_identity_type = " + formatValue(shareholderId))
+                .toString();
+        jdbcTemplate.update(update1, update2, update3);
     }
 
     public void updateCompanyName(String companyId, String companyName) {
@@ -88,7 +93,12 @@ public class BdpEquityDao {
                 .SET("company_name_controlled = " + formatValue(companyName))
                 .WHERE("company_id_controlled = " + formatValue(companyId))
                 .toString();
-        jdbcTemplate.update(update1, update2);
+        String update3 = new SQL()
+                .UPDATE("tyc_entity_main_reference")
+                .SET("entity_name_valid = " + formatValue(companyName))
+                .WHERE("tyc_unique_entity_id = " + formatValue(companyId))
+                .toString();
+        jdbcTemplate.update(update1, update2, update3);
     }
 
     public void replaceIntoRelation(List<Map<String, Object>> columnMaps) {
@@ -101,7 +111,7 @@ public class BdpEquityDao {
                     .INTO_VALUES(insert.f1)
                     .toString();
             sqls.add(sql);
-            jdbcTemplate.update(sqls);
         }
+        jdbcTemplate.update(sqls);
     }
 }
