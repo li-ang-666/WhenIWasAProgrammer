121be197e7a51c31b6022401944ce759a4427ca8
liang <li_ang_666@163.com>
2023-07-13 18:15:35 +0800

    1

---
+++ repair/src/main/java/com/liang/repair/impl/MemJdbcTemplateTest.java
@@ -0,0 +1,12 @@
+package com.liang.repair.impl;
+
+import com.liang.common.service.database.template.MemJdbcTemplate;
+import com.liang.repair.service.ConfigHolder;
+
+public class MemJdbcTemplateTest extends ConfigHolder {
+    public static void main(String[] args) {
+        MemJdbcTemplate jdbcTemplate = new MemJdbcTemplate("aaa");
+        String s = jdbcTemplate.queryForObject("select now()", rs -> rs.getString(1));
+        log.info("s: {}",s);
+    }
+}
