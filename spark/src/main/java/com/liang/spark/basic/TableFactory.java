69b33c544ca8baaa80f0113dbb917de82e3cc7d3
liang <li_ang_666@163.com>
2023-07-15 17:51:41 +0800

    1

---
+++ spark/src/main/java/com/liang/spark/basic/TableFactory.java
@@ -0,0 +1,46 @@
+package com.liang.spark.basic;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.liang.common.service.database.holder.DruidHolder;
+import com.liang.common.service.database.template.JdbcTemplate;
+import lombok.experimental.UtilityClass;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Properties;
+
+@UtilityClass
+public class TableFactory {
+    public static Dataset<Row> csv(SparkSession spark, String fileName) {
+        return spark.read()
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .csv("/Users/liang/Desktop/WhenIWasAProgrammer/spark/src/main/resources/" + fileName);
+    }
+
+    public static Dataset<Row> jdbc(SparkSession spark, String sourceName, String tableName) {
+        Tuple2<Long, Long> minAndMax = new JdbcTemplate(sourceName).queryForObject(
+                String.format("select min(id),max(id) from %s", tableName),
+                rs -> Tuple2.of(rs.getLong(1), rs.getLong(2)));
+        long minId = minAndMax.f0;
+        long maxId = minAndMax.f1;
+        DruidDataSource dataSource = new DruidHolder().getPool(tableName);
+        String url = dataSource.getUrl();
+        String user = dataSource.getUsername();
+        String password = dataSource.getPassword();
+        return spark.read().jdbc(
+                url,
+                tableName,
+                "id",
+                minId,
+                maxId,
+                (int) ((maxId - minId) / 10240),
+                new Properties() {{
+                    put("user", user);
+                    put("password", password);
+                }}
+        );
+    }
+}
