78ed8d9f3e7650157d3297f4f73c1f0fab0bb520
liang <li_ang_666@163.com>
2023-07-15 10:50:41 +0800

    1

--- common/src/main/java/com/liang/common/util/JsonUtils.java
+++ common/src/main/java/com/liang/common/util/JsonUtils.java
@@ -1,13 +1,14 @@
 package com.liang.common.util;
 
 import com.fasterxml.jackson.core.json.JsonReadFeature;
-import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -15,18 +16,13 @@ import java.util.Map;
 @SuppressWarnings("unchecked")
 @UtilityClass
 public class JsonUtils {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    static {
-        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
-        // 生成对象的时候, json多了字段, 不报错
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        DeserializationConfig deserializationConfig = objectMapper
-                .getDeserializationConfig()
-                .withFeatures(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS)
-                .withFeatures(JsonReadFeature.ALLOW_JAVA_COMMENTS);
-        objectMapper.setConfig(deserializationConfig);
-    }
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false).setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
+            .configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(), true)
+            .configure(JsonReadFeature.ALLOW_JAVA_COMMENTS.mappedFeature(), true);
 
     /*----------------------------------解析{XX=XX, XX=XX, XX=XX}----------------------------------*/
     public static Map<String, Object> parseJsonObj(String json) {
--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -1,28 +1,18 @@
 package com.liang.repair.test;
 
+import com.liang.common.dto.Config;
+import com.liang.common.dto.config.FlinkConfig;
+import com.liang.common.util.JsonUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class Test {
     public static void main(String[] args) throws Exception {
-        Student stu = new Student(1, "tom");
-        ff(stu.id);
-        System.out.println(stu);
-    }
+        Date date = new Date(System.currentTimeMillis());
 
-    public static void f(Student arg) {
-        //arg.name=null;
-        //arg=null;
-    }
-
-    public static void ff(int i) {
-        i = 5;
-    }
-
-    @Data
-    @AllArgsConstructor
-    private static class Student {
-        public int id;
-        public String name;
+        System.out.println(JsonUtils.toString(date));
     }
 }
