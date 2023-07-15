4d74a0786bb24477fba1e03b99142b7705bf9716
liang <li_ang_666@163.com>
2023-07-15 11:37:29 +0800

    1

--- common/src/main/java/com/liang/common/util/JsonUtils.java
+++ common/src/main/java/com/liang/common/util/JsonUtils.java
@@ -1,5 +1,6 @@
 package com.liang.common.util;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
@@ -11,17 +12,24 @@ import lombok.extern.slf4j.Slf4j;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
 @Slf4j
 @SuppressWarnings("unchecked")
 @UtilityClass
 public class JsonUtils {
     private static final ObjectMapper objectMapper = new ObjectMapper()
+            .setTimeZone(TimeZone.getTimeZone("GTM+8"))
+            .configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true)
+            .configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
             .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
             .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false).setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
+            //识别控制字符
             .configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature(), true)
+            //识别不认识的控制字符
+            .configure(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER.mappedFeature(), true)
             .configure(JsonReadFeature.ALLOW_JAVA_COMMENTS.mappedFeature(), true);
 
     /*----------------------------------解析{XX=XX, XX=XX, XX=XX}----------------------------------*/
@@ -57,7 +65,7 @@ public class JsonUtils {
         return result;
     }
 
-    /*----------------------------------反解析------------------------------------------------------*/
+    /*----------------------------------序列化------------------------------------------------------*/
     public static String toString(Object o) {
         String result;
         try {
--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -6,13 +6,20 @@ import com.liang.common.util.JsonUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        Date date = new Date(System.currentTimeMillis());
+        Map<String, Object> map = JsonUtils.parseJsonObj("{\"id\":1.1000000000000000000000,\"name\":\"tom\"}");
+        map.forEach((k,v)->{
+            System.out.println(k+" -> "+v);
+            System.out.println(k.getClass());
+            System.out.println(v.getClass());
+        });
 
-        System.out.println(JsonUtils.toString(date));
+        System.out.println(JsonUtils.toString(new BigDecimal("3E10")));
     }
 }
