d4883be4226973dadfc366c47640c5036582e61a
liang <li_ang_666@163.com>
2023-07-15 17:53:00 +0800

    1

--- spark/src/main/java/com/liang/spark/service/SparkHolder.java
+++ spark/src/main/java/com/liang/spark/service/SparkHolder.java
@@ -1,19 +1,10 @@
 package com.liang.spark.service;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-public class SparkHolder {
+public abstract class SparkHolder {
     private final static SparkSession sparkSession = SparkSession.builder()
             .config("spark.debug.maxToStringFields", "200")
             .master("local[*]")
             .getOrCreate();
-
-    private static Dataset<Row> csv(SparkSession spark, String fileName) {
-        return spark.read()
-                .option("header", "true")
-                .option("inferSchema", "true")
-                .csv("/Users/liang/Desktop/WhenIWasAProgrammer/spark/src/main/resources/" + fileName);
-    }
 }
--- spark/src/main/java/com/liang/spark/test/SparkTest.java
+++ spark/src/main/java/com/liang/spark/test/SparkTest.java
@@ -1,9 +1,10 @@
 package com.liang.spark.test;
 
+import com.liang.spark.service.SparkHolder;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class SparkTest {
+public class SparkTest extends SparkHolder {
     public static void main(String[] args) throws Exception {
 
     }
