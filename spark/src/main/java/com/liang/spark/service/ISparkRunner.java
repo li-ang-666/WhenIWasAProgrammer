aa67ec87d5bc8f2025f78e20e9c5c2737a804b00
liang <li_ang_666@163.com>
2023-07-15 17:48:00 +0800

    1

--- spark/src/main/java/com/liang/spark/service/ISparkRunner.java
+++
@@ -1,7 +0,0 @@
-package com.liang.spark.service;
-
-import org.apache.spark.sql.SparkSession;
-
-public interface ISparkRunner {
-    void run(SparkSession spark);
-}
--- spark/src/main/java/com/liang/spark/test/SparkTest.java
+++ spark/src/main/java/com/liang/spark/test/SparkTest.java
@@ -1,14 +1,10 @@
 package com.liang.spark.test;
 
-import com.liang.spark.basic.SparkTester;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SparkTest {
     public static void main(String[] args) throws Exception {
-        SparkTester.test(spark -> {
-            SparkTester.csv(spark, "tb.csv").createOrReplaceTempView("t");
-            spark.sql("explain select distinct c1 from t").show(false);
-        });
+
     }
 }
