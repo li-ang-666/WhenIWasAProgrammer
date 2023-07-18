f22d4d6f81fc3b12e562cd94512217f7d05ac4bb
liang <li_ang_666@163.com>
2023-07-18 18:21:47 +0800

    1

--- flink/src/main/java/com/liang/flink/high/level/api/StreamFactory.java
+++ flink/src/main/java/com/liang/flink/high/level/api/StreamFactory.java
@@ -1,9 +1,7 @@
 package com.liang.flink.high.level.api;
 
 import com.liang.common.dto.Config;
-import com.liang.common.service.database.template.RedisTemplate;
 import com.liang.common.util.ConfigUtils;
-import com.liang.common.util.JsonUtils;
 import com.liang.common.util.StackUtils;
 import com.liang.flink.basic.KafkaMonitor;
 import com.liang.flink.basic.KafkaSourceFactory;
@@ -11,7 +9,7 @@ import com.liang.flink.basic.RepairSource;
 import com.liang.flink.dto.BatchCanalBinlog;
 import com.liang.flink.dto.KafkaRecord;
 import com.liang.flink.dto.SingleCanalBinlog;
-import lombok.SneakyThrows;
+import com.liang.flink.service.RepairDataReporter;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
@@ -19,17 +17,13 @@ import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 import static com.liang.common.dto.config.FlinkConfig.SourceType.Kafka;
 
 @UtilityClass
 @Slf4j
 public class StreamFactory {
-    private final static int REPORT_INTERVAL = 1000 * 60 * 3;
-
     public static DataStream<SingleCanalBinlog> create(StreamExecutionEnvironment streamEnvironment) {
         Config config = ConfigUtils.getConfig();
         return config.getFlinkConfig().getSourceType() == Kafka ?
@@ -52,20 +46,7 @@ public class StreamFactory {
         String[] split = jobClassName.split("\\.");
         String simpleName = split[split.length - 1];
         String repairId = simpleName + "-" + UUID.randomUUID();
-        new Thread(new Runnable() {
-            private final RedisTemplate redisTemplate = new RedisTemplate("metadata");
-
-            @Override
-            @SneakyThrows(InterruptedException.class)
-            public void run() {
-                while (true) {
-                    TimeUnit.MILLISECONDS.sleep(REPORT_INTERVAL);
-                    Map<String, String> reportMap = redisTemplate.hScan(repairId);
-                    String reportContent = JsonUtils.toString(reportMap);
-                    log.info("repair report: {}", reportContent);
-                }
-            }
-        }).start();
+        new Thread(new RepairDataReporter(repairId)).start();
         return streamEnvironment
                 .addSource(new RepairSource(config, repairId))
                 .name("RepairSource")
---
+++ flink/src/main/java/com/liang/flink/service/RepairDataReporter.java
@@ -0,0 +1,29 @@
+package com.liang.flink.service;
+
+import com.liang.common.service.database.template.RedisTemplate;
+import com.liang.common.util.JsonUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@RequiredArgsConstructor
+public class RepairDataReporter implements Runnable {
+    private final static int REPORT_INTERVAL = 1000 * 60 * 3;
+    private final RedisTemplate redisTemplate = new RedisTemplate("metadata");
+    private final String repairId;
+
+    @Override
+    @SneakyThrows(InterruptedException.class)
+    public void run() {
+        while (true) {
+            TimeUnit.MILLISECONDS.sleep(REPORT_INTERVAL);
+            Map<String, String> reportMap = redisTemplate.hScan(repairId);
+            String reportContent = JsonUtils.toString(reportMap);
+            log.info("repair report: {}", reportContent);
+        }
+    }
+}
