14ceb916069a1e07b3687297858b5476d35b4c3b
liang <li_ang_666@163.com>
2023-07-19 13:56:58 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -1,12 +1,16 @@
 package com.liang.repair.test;
 
+import com.liang.common.util.JsonUtils;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        //{"id":1}
-        byte[] bytes = "{\"id\":1}".getBytes(StandardCharsets.UTF_8);
+        // {"id":"1","name":"tom"}
+        byte[] bytes = "{\"id\":\"1\",\"name\":\"tom\"}".getBytes(StandardCharsets.UTF_8);
         System.out.println(Arrays.toString(bytes));
+        System.out.println(JsonUtils.parseJsonObj("{\"id\":\"1\",\"name\":\"tom\"}"));
+
     }
 }
