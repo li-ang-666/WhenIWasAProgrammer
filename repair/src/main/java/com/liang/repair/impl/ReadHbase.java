e961ab9510a6d5a88b8daba68d51a3b9df965616
liang <li_ang_666@163.com>
2023-07-20 16:03:35 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/ReadHbase.java
+++ repair/src/main/java/com/liang/repair/impl/ReadHbase.java
@@ -3,14 +3,14 @@ package com.liang.repair.impl;
 import com.liang.common.dto.HbaseOneRow;
 import com.liang.common.dto.HbaseSchema;
 import com.liang.common.service.database.template.HbaseTemplate;
+import com.liang.common.util.JsonUtils;
 import com.liang.repair.service.ConfigHolder;
 
 public class ReadHbase extends ConfigHolder {
     public static void main(String[] args) {
         HbaseTemplate hbaseTemplate = new HbaseTemplate("hbaseSink");
         HbaseOneRow queryRow = new HbaseOneRow(HbaseSchema.HUMAN_ALL_COUNT, "J0FL2MH02Q9Z6BCV6");
-        /*HbaseOneRow row = hbaseTemplate.getRow(queryRow);
-        log.info("row: {}", JsonUtils.toString(row));*/
-        hbaseTemplate.scan(queryRow, e -> log.info("row: {}", e));
+        HbaseOneRow row = hbaseTemplate.getRow(queryRow);
+        log.info("row: {}", JsonUtils.toString(row));
     }
 }
