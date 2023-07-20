50b305d5e0159015696daec86207540525a6d3e5
liang <li_ang_666@163.com>
2023-07-20 14:25:31 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -1,15 +1,9 @@
 package com.liang.repair.test;
 
-import com.liang.common.util.JsonUtils;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.math.BigDecimal;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        // {"id":"1","name":"tom"}
-        byte[] bytes = "{\"id\":\"1\",\"name\":\"tom\"}".getBytes(StandardCharsets.UTF_8);
-        System.out.println(Arrays.toString(bytes));
-        System.out.println(JsonUtils.parseJsonObj("{\"id\":\"1\",\"name\":\"tom\"}"));
+        System.out.println(new BigDecimal("99.99%").toPlainString());
     }
 }
