da5be927d8dbdae61b7494ca8f9d6a55af352445
liang <li_ang_666@163.com>
2023-07-20 16:40:38 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/ScanHbase.java
+++ repair/src/main/java/com/liang/repair/impl/ScanHbase.java
@@ -10,7 +10,7 @@ import com.liang.repair.service.ConfigHolder;
 public class ScanHbase extends ConfigHolder {
     public static void main(String[] args) {
         HbaseTemplate hbaseTemplate = new HbaseTemplate("hbaseSink");
-        ObsWriter obsWriter = new ObsWriter("obs://hadoop-obs/flink");
+        ObsWriter obsWriter = new ObsWriter("obs://hadoop-obs/flink/hbase/");
         obsWriter.enableCache();
         HbaseOneRow queryRow = new HbaseOneRow(HbaseSchema.HUMAN_ALL_COUNT, "J0FL2MH02Q9Z6BCV6");
         hbaseTemplate.scan(queryRow, row -> obsWriter.update(JsonUtils.toString(row)));
