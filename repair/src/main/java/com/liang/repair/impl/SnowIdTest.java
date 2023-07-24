7ffb2a36ef39fc0138a24cbf91fbe88b429f0ac2
liang <li_ang_666@163.com>
2023-07-25 01:30:56 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/SnowIdTest.java
+++ repair/src/main/java/com/liang/repair/impl/SnowIdTest.java
@@ -1,64 +1,13 @@
 package com.liang.repair.impl;
 
-import cn.hutool.core.lang.Snowflake;
 import com.liang.repair.service.ConfigHolder;
-
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class SnowIdTest extends ConfigHolder {
 
     public static void main(String[] args) {
-        Set<Long> set = ConcurrentHashMap.newKeySet();
-        Snowflake snowflake1 = new Snowflake(1);
-        Snowflake snowflake2 = new Snowflake(2);
-        Snowflake snowflake3 = new Snowflake(3);
-        Snowflake snowflake4 = new Snowflake(4);
-        Snowflake snowflake5 = new Snowflake(5);
-        new Thread(() -> {
-            while (true) {
-                long l = snowflake1.nextId();
-                boolean add = set.add(l);
-                if (!add) {
-                    log.info("l: {}", l);
-                }
-            }
-        }).start();
-        new Thread(() -> {
-            while (true) {
-                long l = snowflake2.nextId();
-                boolean add = set.add(l);
-                if (!add) {
-                    log.info("l: {}", l);
-                }
-            }
-        }).start();
-        new Thread(() -> {
-            while (true) {
-                long l = snowflake3.nextId();
-                boolean add = set.add(l);
-                if (!add) {
-                    log.info("l: {}", l);
-                }
-            }
-        }).start();
-        new Thread(() -> {
-            while (true) {
-                long l = snowflake4.nextId();
-                boolean add = set.add(l);
-                if (!add) {
-                    log.info("l: {}", l);
-                }
-            }
-        }).start();
-        new Thread(() -> {
-            while (true) {
-                long l = snowflake5.nextId();
-                boolean add = set.add(l);
-                if (!add) {
-                    log.info("l: {}", l);
-                }
-            }
-        }).start();
+        for (int i = 1; i <= 2048; i++) {
+            int i1 = (i-1) % (32 * 32);
+            log.info("dataCenterId: {}, workerId: {}",i1/32,i1%32);
+        }
     }
 }
