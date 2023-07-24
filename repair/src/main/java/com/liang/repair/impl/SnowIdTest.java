0d7b0c68cef3fc4bd19424171d5ed3acf6a07e3a
liang <li_ang_666@163.com>
2023-07-25 01:38:34 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/SnowIdTest.java
+++ repair/src/main/java/com/liang/repair/impl/SnowIdTest.java
@@ -1,13 +1,66 @@
 package com.liang.repair.impl;
 
+import cn.hutool.core.lang.Snowflake;
 import com.liang.repair.service.ConfigHolder;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SnowIdTest extends ConfigHolder {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 2048; i++) {
-            int i1 = (i-1) % (32 * 32);
-            log.info("dataCenterId: {}, workerId: {}",i1/32,i1%32);
-        }
+        Set<Long> set = ConcurrentHashMap.newKeySet();
+        Date date = new Date();
+        Snowflake snowflake1 = new Snowflake(date, 1, 0, true);
+        Snowflake snowflake2 = new Snowflake(date, 2, 0, true);
+        Snowflake snowflake3 = new Snowflake(date, 3, 0, true);
+        Snowflake snowflake4 = new Snowflake(date, 4, 0, true);
+        Snowflake snowflake5 = new Snowflake(date, 5, 0, true);
+        new Thread(() -> {
+            while (true) {
+                long l = snowflake1.nextId();
+                boolean add = set.add(l);
+                if (!add) {
+                    log.info("l1: {}", l);
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                long l = snowflake2.nextId();
+                boolean add = set.add(l);
+                if (!add) {
+                    log.info("l2: {}", l);
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                long l = snowflake3.nextId();
+                boolean add = set.add(l);
+                if (!add) {
+                    log.info("l3: {}", l);
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                long l = snowflake4.nextId();
+                boolean add = set.add(l);
+                if (!add) {
+                    log.info("l4: {}", l);
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                long l = snowflake5.nextId();
+                boolean add = set.add(l);
+                if (!add) {
+                    log.info("l5: {}", l);
+                }
+            }
+        }).start();
     }
 }
