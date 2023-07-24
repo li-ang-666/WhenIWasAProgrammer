5e3e4541ba1c599a9217c611f855fe3317a183eb
liang <li_ang_666@163.com>
2023-07-24 16:00:05 +0800

    1

--- spark/src/main/java/com/liang/spark/job/RatioPathCompanyJob.java
+++ spark/src/main/java/com/liang/spark/job/RatioPathCompanyJob.java
@@ -1,9 +1,12 @@
 package com.liang.spark.job;
 
+import com.liang.common.dto.Config;
+import com.liang.common.util.ConfigUtils;
 import com.liang.common.util.JsonUtils;
 import com.liang.flink.project.ratio.path.company.RatioPathCompanyService;
 import com.liang.spark.basic.SparkSessionFactory;
 import com.liang.spark.basic.TableFactory;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.api.java.function.ForeachPartitionFunction;
@@ -22,14 +25,17 @@ public class RatioPathCompanyJob {
                 .createOrReplaceTempView("t");
         spark.sql("select distinct company_id_invested from t")
                 .repartition(1200)
-                .foreachPartition(new RatioPathCompanyForeach());
+                .foreachPartition(new RatioPathCompanyForeach(ConfigUtils.getConfig()));
     }
 
     @Slf4j
+    @RequiredArgsConstructor
     private final static class RatioPathCompanyForeach implements ForeachPartitionFunction<Row> {
+        private final Config config;
 
         @Override
         public void call(Iterator<Row> iterator) throws Exception {
+            ConfigUtils.setConfig(config);
             RatioPathCompanyService service = new RatioPathCompanyService();
             Set<Long> set = new HashSet<>();
             while (iterator.hasNext()) {
