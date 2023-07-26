27c6b8107a41550189b597b28b21252d5bbc2cbc
liang <li_ang_666@163.com>
2023-07-26 10:49:59 +0800

    1

--- common/src/main/java/com/liang/common/service/database/factory/DruidFactory.java
+++ common/src/main/java/com/liang/common/service/database/factory/DruidFactory.java
@@ -75,7 +75,7 @@ public class DruidFactory implements IFactory<DruidDataSource> {
         druidDataSource.setTestOnBorrow(false);
         druidDataSource.setTestOnReturn(false);
         druidDataSource.setTestWhileIdle(true);
-        // 清理minIdle以外
+        // 保持minIdle
         druidDataSource.setTimeBetweenEvictionRunsMillis(1000 * 60);
         druidDataSource.setMinEvictableIdleTimeMillis(1000 * 60 * 5);
         druidDataSource.setMaxEvictableIdleTimeMillis(1000 * 60 * 10);
