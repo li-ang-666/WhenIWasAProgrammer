8d795c12501a184c31b9bfb75647bbef2f419e45
liang <li_ang_666@163.com>
2023-07-29 03:52:53 +0800

    1

--- spark/src/main/java/com/liang/spark/job/RatioPathCompanyDeleteJob.java
+++ spark/src/main/java/com/liang/spark/job/RatioPathCompanyDeleteJob.java
@@ -1,19 +1,9 @@
 package com.liang.spark.job;
 
-import com.liang.common.dto.Config;
-import com.liang.common.util.ConfigUtils;
-import com.liang.common.util.JsonUtils;
-import com.liang.flink.project.ratio.path.company.RatioPathCompanyDao;
 import com.liang.spark.basic.SparkSessionFactory;
 import com.liang.spark.basic.TableFactory;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.spark.api.java.function.ForeachPartitionFunction;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-
-import java.util.Iterator;
-import java.util.Map;
 
 @Slf4j
 public class RatioPathCompanyDeleteJob {
@@ -21,26 +11,8 @@ public class RatioPathCompanyDeleteJob {
         SparkSession spark = SparkSessionFactory.createSpark(args);
         TableFactory.jdbc(spark, "prismShareholderPath", "investment_relation")
                 .createOrReplaceTempView("t1");
-        TableFactory.jdbc(spark, "prismShareholderPath", "investment_relation")
+        TableFactory.jdbc(spark, "prismShareholderPath", "ratio_path_company")
                 .createOrReplaceTempView("t2");
-        spark.sql("select * from t1 union all select * from t2").limit(1).show();
-    }
-
-    @Slf4j
-    @RequiredArgsConstructor
-    private final static class RatioPathCompanyDeleteSink implements ForeachPartitionFunction<Row> {
-        private final Config config;
-
-        @Override
-        public void call(Iterator<Row> t) throws Exception {
-            ConfigUtils.setConfig(config);
-            RatioPathCompanyDao dao = new RatioPathCompanyDao();
-            while (t.hasNext()) {
-                Row row = t.next();
-                Map<String, Object> columnMap = JsonUtils.parseJsonObj(row.json());
-                long companyId = Long.parseLong(String.valueOf(columnMap.get("company_id")));
-                dao.deleteAll(companyId);
-            }
-        }
+        spark.sql("insert overwrite table test.company_id_test select /*+ REPARTITION(1200) */ * from (select company_id_invested from t1 union all select company_id from t2) t");
     }
 }
