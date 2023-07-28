0de04c8d7d533efdd770ce7c01688d1f9bc50c34
liang <li_ang_666@163.com>
2023-07-28 19:44:59 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/ReadHbase.java
+++ repair/src/main/java/com/liang/repair/impl/ReadHbase.java
@@ -3,14 +3,26 @@ package com.liang.repair.impl;
 import com.liang.common.dto.HbaseOneRow;
 import com.liang.common.dto.HbaseSchema;
 import com.liang.common.service.database.template.HbaseTemplate;
+import com.liang.common.util.ConfigUtils;
 import com.liang.common.util.JsonUtils;
 import com.liang.repair.service.ConfigHolder;
 
 public class ReadHbase extends ConfigHolder {
     public static void main(String[] args) {
+        // 连接
         HbaseTemplate hbaseTemplate = new HbaseTemplate("hbaseSink");
+        // 条件
         HbaseOneRow queryRow = new HbaseOneRow(HbaseSchema.JUDICIAL_RISK_SPLICE, "22822");
-        HbaseOneRow row = hbaseTemplate.getRow(queryRow);
-        log.info("row: {}", JsonUtils.toString(row));
+        // 查询
+        HbaseOneRow resultRow = hbaseTemplate.getRow(queryRow);
+        // 打印
+        if (String.valueOf(ConfigUtils.getConfig().getHbaseDbConfigs().get("hbaseSink")).split(",").length == 1) {
+            log.warn("\n\n醒目: 目前是测试Hbase\n");
+        } else {
+            log.warn("\n\n醒目: 目前是生产Hbase\n");
+        }
+        log.info("row: {}", JsonUtils.toString(resultRow));
+        // 更正并写入
+        //hbaseTemplate.update(resultRow.put("lawsuit_num_times_frequent_year","2022"));
     }
 }
