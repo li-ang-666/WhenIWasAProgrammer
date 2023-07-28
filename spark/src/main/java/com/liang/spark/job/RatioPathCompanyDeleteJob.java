b61fd15201408e1a02ffa2c873b64dd25dd47827
liang <li_ang_666@163.com>
2023-07-29 01:08:41 +0800

    1

--- spark/src/main/java/com/liang/spark/job/RatioPathCompanyDeleteJob.java
+++ spark/src/main/java/com/liang/spark/job/RatioPathCompanyDeleteJob.java
@@ -1,26 +1,46 @@
 package com.liang.spark.job;
 
+import com.liang.common.dto.Config;
+import com.liang.common.util.ConfigUtils;
+import com.liang.common.util.JsonUtils;
+import com.liang.flink.project.ratio.path.company.RatioPathCompanyDao;
 import com.liang.spark.basic.SparkSessionFactory;
 import com.liang.spark.basic.TableFactory;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.api.java.function.ForeachPartitionFunction;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Iterator;
+import java.util.Map;
 
 @Slf4j
 public class RatioPathCompanyDeleteJob {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SparkSession spark = SparkSessionFactory.createSpark(args);
         TableFactory.jdbc(spark, "prismShareholderPath", "investment_relation")
-                .createOrReplaceTempView("investment_relation");
+                .createOrReplaceTempView("t1");
+        TableFactory.jdbc(spark, "prismShareholderPath", "investment_relation")
+                .createOrReplaceTempView("t2");
+        spark.sql("select * from t1 union all select * from t2").limit(1).show();
+    }
 
-        TableFactory.jdbc(spark, "prismShareholderPath", "ratio_path_company")
-                .createOrReplaceTempView("ratio_path_company");
+    @Slf4j
+    @RequiredArgsConstructor
+    private final static class RatioPathCompanyDeleteSink implements ForeachPartitionFunction<Row> {
+        private final Config config;
 
-
-        spark.sql("select distinct t1.company_id from ratio_path_company t1 left join investment_relation t2 on t1.company_id = t2.company_id_invested where t2.company_id_invested is null")
-                .createOrReplaceTempView("t");
-
-        spark.sql("select * from t limit 1000")
-                .show(1000, false);
-        log.info("size: {}", spark.sql("select * from t").collectAsList().size());
+        @Override
+        public void call(Iterator<Row> t) throws Exception {
+            ConfigUtils.setConfig(config);
+            RatioPathCompanyDao dao = new RatioPathCompanyDao();
+            while (t.hasNext()) {
+                Row row = t.next();
+                Map<String, Object> columnMap = JsonUtils.parseJsonObj(row.json());
+                long companyId = Long.parseLong(String.valueOf(columnMap.get("company_id")));
+                dao.deleteAll(companyId);
+            }
+        }
     }
 }
