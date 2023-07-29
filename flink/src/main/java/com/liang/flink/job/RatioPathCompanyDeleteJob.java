22d1d85254dfd1b7ef8a8e48310d06c057b2ca3c
liang <li_ang_666@163.com>
2023-07-29 19:32:51 +0800

    1

--- flink/src/main/java/com/liang/flink/job/RatioPathCompanyDeleteJob.java
+++ flink/src/main/java/com/liang/flink/job/RatioPathCompanyDeleteJob.java
@@ -12,11 +12,16 @@ import com.liang.flink.project.ratio.path.company.RatioPathCompanyDao;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @LocalConfigFile("ratio-path-company-delete.yml")
@@ -33,10 +38,15 @@ public class RatioPathCompanyDeleteJob {
 
     @Slf4j
     @RequiredArgsConstructor
-    private final static class RatioPathCompanyDeleteSink extends RichSinkFunction<SingleCanalBinlog> {
+    private final static class RatioPathCompanyDeleteSink extends RichSinkFunction<SingleCanalBinlog> implements CheckpointedFunction {
+        private final Set<Long> companyIds = ConcurrentHashMap.newKeySet();
         private final Config config;
         private RatioPathCompanyDao dao;
         private JdbcTemplate jdbcTemplate;
+
+        @Override
+        public void initializeState(FunctionInitializationContext context) throws Exception {
+        }
 
         @Override
         public void open(Configuration parameters) {
@@ -49,9 +59,52 @@ public class RatioPathCompanyDeleteJob {
         public void invoke(SingleCanalBinlog singleCanalBinlog, Context context) {
             Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
             String companyIdString = String.valueOf(columnMap.get("company_id"));
-            String res = jdbcTemplate.queryForObject("select 1 from investment_relation where company_id_invested = " + companyIdString, rs -> rs.getString(1));
-            if (res == null) {
-                dao.deleteAll(Long.parseLong(companyIdString));
+            companyIds.add(Long.parseLong(companyIdString));
+            if (companyIds.size() >= 1024) {
+                synchronized (companyIds) {
+                    for (Long companyId : companyIds) {
+                        String res = jdbcTemplate.queryForObject("select 1 from investment_relation where company_id_invested = " + companyId, rs -> rs.getString(1));
+                        if (res == null) {
+                            dao.deleteAll(companyId);
+                        }
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void snapshotState(FunctionSnapshotContext context) throws Exception {
+            synchronized (companyIds) {
+                for (Long companyId : companyIds) {
+                    String res = jdbcTemplate.queryForObject("select 1 from investment_relation where company_id_invested = " + companyId, rs -> rs.getString(1));
+                    if (res == null) {
+                        dao.deleteAll(companyId);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void finish() throws Exception {
+            synchronized (companyIds) {
+                for (Long companyId : companyIds) {
+                    String res = jdbcTemplate.queryForObject("select 1 from investment_relation where company_id_invested = " + companyId, rs -> rs.getString(1));
+                    if (res == null) {
+                        dao.deleteAll(companyId);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            synchronized (companyIds) {
+                for (Long companyId : companyIds) {
+                    String res = jdbcTemplate.queryForObject("select 1 from investment_relation where company_id_invested = " + companyId, rs -> rs.getString(1));
+                    if (res == null) {
+                        dao.deleteAll(companyId);
+                    }
+                }
             }
         }
     }
