7f21af22947bf1cd44bb526d6c19fb44d83690a0
liang <li_ang_666@163.com>
2023-08-01 18:43:26 +0800

    1

--- flink/src/main/java/com/liang/flink/project/annual/report/dao/AnnualReportDao.java
+++ flink/src/main/java/com/liang/flink/project/annual/report/dao/AnnualReportDao.java
@@ -1,7 +1,16 @@
 package com.liang.flink.project.annual.report.dao;
 
+import com.liang.common.service.SQL;
 import com.liang.common.service.database.template.JdbcTemplate;
+
+import static com.liang.common.util.SqlUtils.formatValue;
 
 public class AnnualReportDao {
     private final JdbcTemplate jdbcTemplate = new JdbcTemplate("116prism");
+
+    public String delete(String tableName, Object id) {
+        return new SQL().DELETE_FROM(tableName)
+                .WHERE("id = " + formatValue(id))
+                .toString();
+    }
 }
--- flink/src/main/java/com/liang/flink/project/annual/report/impl/ReportOutboundInvestment.java
+++ flink/src/main/java/com/liang/flink/project/annual/report/impl/ReportOutboundInvestment.java
@@ -4,18 +4,22 @@ import com.liang.flink.dto.SingleCanalBinlog;
 import com.liang.flink.project.annual.report.dao.AnnualReportDao;
 import com.liang.flink.service.data.update.AbstractDataUpdate;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class ReportOutboundInvestment extends AbstractDataUpdate<String> {
     private final AnnualReportDao dao = new AnnualReportDao();
 
     @Override
     public List<String> updateWithReturn(SingleCanalBinlog singleCanalBinlog) {
-        return super.updateWithReturn(singleCanalBinlog);
+        Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+        return null;
     }
 
     @Override
     public List<String> deleteWithReturn(SingleCanalBinlog singleCanalBinlog) {
-        return super.deleteWithReturn(singleCanalBinlog);
+        Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+        return Collections.singletonList(dao.delete("report_outbound_investment", columnMap.get("id")));
     }
 }
