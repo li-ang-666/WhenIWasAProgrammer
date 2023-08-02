72be42ed9e080bfd3b8ae6fc7fd5b76f30858453
liang <li_ang_666@163.com>
2023-08-02 17:01:05 +0800

    1

--- flink/src/main/java/com/liang/flink/project/annual/report/impl/ReportEquityChangeInfo.java
+++ flink/src/main/java/com/liang/flink/project/annual/report/impl/ReportEquityChangeInfo.java
@@ -34,19 +34,18 @@ public class ReportEquityChangeInfo extends AbstractDataUpdate<String> {
         resultMap.put("entity_name_valid", info.f1);
         resultMap.put("entity_type_id", 1);
         //
-        resultMap.put("annual_report_tyc_unique_entity_id_shareholder", 0);
+        resultMap.put("annual_report_tyc_unique_entity_id_shareholder", -1);
         resultMap.put("annual_report_entity_name_valid_shareholder", investorName);
-        resultMap.put("annual_report_entity_type_id_shareholder", 0);
+        resultMap.put("annual_report_entity_type_id_shareholder", -1);
         //
         resultMap.put("annual_report_equity_ratio_before_change", ratioBefore.endsWith("%") ? ratioBefore : ratioBefore + "%");
         resultMap.put("annual_report_equity_ratio_after_change", ratioAfter.endsWith("%") ? ratioAfter : ratioAfter + "%");
         resultMap.put("annual_report_equity_ratio_change_time", changeTime.matches("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$") ? changeTime : null);
-        resultMap.put("is_display_annual_report_equity_ratio_change", 1);
         Tuple2<String, String> insert = SqlUtils.columnMap2Insert(resultMap);
         String sql = new SQL()
                 .REPLACE_INTO("entity_annual_report_shareholder_equity_change_details")
                 .INTO_COLUMNS(insert.f0)
-                .INTO_COLUMNS(insert.f1)
+                .INTO_VALUES(insert.f1)
                 .toString();
         return Collections.singletonList(sql);
     }
@@ -54,6 +53,6 @@ public class ReportEquityChangeInfo extends AbstractDataUpdate<String> {
     @Override
     public List<String> deleteWithReturn(SingleCanalBinlog singleCanalBinlog) {
         Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
-        return Collections.singletonList(dao.delete("report_equity_change_info", columnMap.get("id")));
+        return Collections.singletonList(dao.delete("entity_annual_report_shareholder_equity_change_details", columnMap.get("id")));
     }
 }
