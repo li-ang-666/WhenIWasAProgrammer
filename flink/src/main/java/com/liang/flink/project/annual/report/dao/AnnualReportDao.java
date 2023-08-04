162c2ef4e7bc8beae96b63ee898f1362b2aed268
liang <li_ang_666@163.com>
2023-08-04 14:50:01 +0800

    1

--- flink/src/main/java/com/liang/flink/project/annual/report/dao/AnnualReportDao.java
+++ flink/src/main/java/com/liang/flink/project/annual/report/dao/AnnualReportDao.java
@@ -2,7 +2,7 @@ package com.liang.flink.project.annual.report.dao;
 
 import com.liang.common.service.SQL;
 import com.liang.common.service.database.template.JdbcTemplate;
-import com.liang.common.util.SqlUtils;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 
 import java.util.Map;
@@ -10,33 +10,31 @@ import java.util.Map;
 import static com.liang.common.util.SqlUtils.formatValue;
 
 public class AnnualReportDao {
-    private final JdbcTemplate jdbcTemplate = new JdbcTemplate("116prism");
-    private final JdbcTemplate bdpEquity = new JdbcTemplate("bdpEquity");
+    private final JdbcTemplate prism116 = new JdbcTemplate("116prism");
+    private final JdbcTemplate prism464 = new JdbcTemplate("464prism");
 
     public Tuple3<String, String, String> getInfoAndNameByReportId(String reportId, Map<String, Object> columnMap) {
-        String sql = new SQL().SELECT("t2.graph_id", "t1.company_name", "t1.report_year")
-                .FROM("annual_report t1")
-                .JOIN("company_graph t2 on t1.company_id = t2.company_id")
-                .WHERE("t2.deleted = 0")
-                .WHERE("t1.id = " + formatValue(reportId))
+        String sql = new SQL()
+                .SELECT("company_id,report_year")
+                .FROM("annual_report")
+                .WHERE("id = " + formatValue(reportId))
                 .toString();
-        Tuple3<String, String, String> tuple3 = jdbcTemplate.queryForObject(sql,
-                rs -> Tuple3.of(rs.getString(1), rs.getString(2), rs.getString(3)));
-        if (tuple3 == null) {
+        Tuple2<String, String> companyCidAndReportYear = prism116.queryForObject(sql, rs -> Tuple2.of(rs.getString(1), rs.getString(2)));
+        // 查询report表后做一次判空
+        if (companyCidAndReportYear == null) {
             columnMap.put("delete_status", 2);
-            return Tuple3.of("-1", "缺少有效report_id或company_gid", null);
+            return Tuple3.of("-1", String.format("查询report表,id = %s异常", reportId), null);
         }
-        if (SqlUtils.isCompanyId(tuple3.f0)) {
-            sql = new SQL().SELECT("entity_name_valid")
-                    .FROM("tyc_entity_main_reference")
-                    .WHERE("entity_type_id = 1")
-                    .WHERE("tyc_unique_entity_id = " + formatValue(tuple3.f0))
-                    .toString();
-            String name = bdpEquity.queryForObject(sql, rs -> rs.getString(1));
-            if (name != null) {
-                tuple3.f1 = name;
-            }
+        sql = new SQL().SELECT("graph_id,name")
+                .FROM("enterprise")
+                .WHERE("deleted = 0")
+                .WHERE("id = " + formatValue(companyCidAndReportYear.f0))
+                .toString();
+        Tuple2<String, String> companyGidAndName = prism464.queryForObject(sql, rs -> Tuple2.of(rs.getString(1), rs.getString(2)));
+        if (companyGidAndName == null) {
+            columnMap.put("delete_status", 2);
+            return Tuple3.of("-1", String.format("查询enterprise表,id = %s异常", companyCidAndReportYear.f0), null);
         }
-        return tuple3;
+        return Tuple3.of(companyGidAndName.f0, companyGidAndName.f1, companyCidAndReportYear.f1);
     }
 }
