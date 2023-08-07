66c037951638309b267b83f41428b6d9ec641893
liang <li_ang_666@163.com>
2023-08-07 18:51:05 +0800

    1

--- common/src/main/java/com/liang/common/util/TycStringUtils.java
+++ common/src/main/java/com/liang/common/util/TycStringUtils.java
@@ -63,9 +63,7 @@ public class TycStringUtils {
 
     public static Tuple2<String, String> formatEquity(String equity) {
         if (equity == null || equity.isEmpty()) {
-            return Tuple2.of(
-                    new BigDecimal("0").setScale(12, RoundingMode.DOWN).toPlainString(),
-                    "");
+            return Tuple2.of(getDecimalString("0", 1), "");
         }
         StringBuilder numberBuilder = new StringBuilder();
         StringBuilder unitBuilder = new StringBuilder();
@@ -81,26 +79,30 @@ public class TycStringUtils {
         String number = numberBuilder.toString();
         String unit = unitBuilder.toString();
         int multiply = unit.contains("万") ? 10000 * 10 * 10 : 10 * 10;
+        number = getDecimalString(number, multiply);
         if (unit.contains("人民币") || "万元".equals(unit) || "元".equals(unit)) {
-            return Tuple2.of(getDecimalString(number, multiply), "人民币");
-        } else if (unit.contains("美国") || unit.contains("美元")) {
-            return Tuple2.of(getDecimalString(number, multiply), "美元");
+            return Tuple2.of(number, "人民币");
+        } else if (unit.contains("美国") || unit.contains("美元") || unit.contains("美币")) {
+            return Tuple2.of(number, "美元");
         } else if (unit.contains("香港") || unit.contains("港元") || unit.contains("港币")) {
-            return Tuple2.of(getDecimalString(number, multiply), "港元");
-        } else if (unit.contains("澳门")) {
-            return Tuple2.of(getDecimalString(number, multiply), "澳门元");
+            return Tuple2.of(number, "港元");
+        } else if (unit.contains("澳门") || unit.contains("澳元") || unit.contains("澳币")) {
+            return Tuple2.of(number, "澳门元");
         } else if (unit.contains("台湾") || unit.contains("台元") || unit.contains("台币")) {
-            return Tuple2.of(getDecimalString(number, multiply), "新台币");
-        } else if (unit.contains("日本") || unit.contains("日元")) {
-            return Tuple2.of(getDecimalString(number, multiply), "日元");
-        } else if (unit.contains("欧洲") || unit.contains("欧元") || unit.contains("欧")) {
-            return Tuple2.of(getDecimalString(number, multiply), "欧元");
-        } else if (unit.contains("英国") || unit.contains("英镑")) {
-            return Tuple2.of(getDecimalString(number, multiply), "英镑");
-        } else if (unit.contains("韩国") || unit.contains("韩元")) {
-            return Tuple2.of(getDecimalString(number, multiply), "韩元");
+            return Tuple2.of(number, "新台币");
+        } else if (unit.contains("日本") || unit.contains("日元") || unit.contains("日币")) {
+            return Tuple2.of(number, "日元");
+        } else if (unit.contains("欧洲") || unit.contains("欧元") || unit.contains("欧币")) {
+            return Tuple2.of(number, "欧元");
+        } else if (unit.contains("英国") || unit.contains("英镑") || unit.contains("英元") || unit.contains("英币")) {
+            return Tuple2.of(number, "英镑");
+        } else if (unit.contains("韩国") || unit.contains("韩元") || unit.contains("韩币")) {
+            return Tuple2.of(number, "韩元");
         } else {
-            return Tuple2.of(getDecimalString(number, multiply), unit.replaceAll("万元|万", ""));
+            unit = unit.replaceAll("万元|万", "");
+        }
+        if (!unit.isEmpty()) {
+            return Tuple2.of(number, unit);
         }
     }
 
