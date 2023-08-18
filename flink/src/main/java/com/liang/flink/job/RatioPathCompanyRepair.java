e0a16d15681af3d6cebd6c25f5e4947df14faaa6
liang <li_ang_666@163.com>
2023-08-18 18:20:41 +0800

    1

--- flink/src/main/java/com/liang/flink/job/RatioPathCompanyRepair.java
+++ flink/src/main/java/com/liang/flink/job/RatioPathCompanyRepair.java
@@ -13,7 +13,7 @@ public class RatioPathCompanyRepair {
         Config config = ConfigUtils.createConfig(null);
         ConfigUtils.setConfig(config);
         Set<Long> companyIds = new HashSet<>(Arrays.asList(
-                6145631249L, 4002271280L, 4536444204L, 3406860535L, 3388728012L, 3394281421L, 6087765423L, 5534712545L, 5568883908L
+                6141212981L
         ));
         new RatioPathCompanyService().invoke(companyIds);
         ConfigUtils.unloadAll();
