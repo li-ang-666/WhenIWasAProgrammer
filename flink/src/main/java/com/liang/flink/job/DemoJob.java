3ec255f1652730d1b8fdcafc51d31764cbc23205
liang <li_ang_666@163.com>
2023-08-18 14:42:42 +0800

    1

--- flink/src/main/java/com/liang/flink/job/DemoJob.java
+++ flink/src/main/java/com/liang/flink/job/DemoJob.java
@@ -1,16 +1,18 @@
 package com.liang.flink.job;
 
+import com.liang.common.dto.Config;
+import com.liang.common.service.filesystem.ObsWriter;
+import com.liang.common.util.ConfigUtils;
 import com.liang.common.util.JsonUtils;
 import com.liang.flink.basic.EnvironmentFactory;
 import com.liang.flink.basic.LocalConfigFile;
 import com.liang.flink.dto.SingleCanalBinlog;
 import com.liang.flink.high.level.api.StreamFactory;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.SinkFunction;
-
-import java.util.Map;
-import java.util.Objects;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 
 @Slf4j
 @LocalConfigFile("demo.yml")
@@ -18,16 +20,25 @@ public class DemoJob {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = EnvironmentFactory.create(args);
         StreamFactory.create(env)
-                .addSink(new SinkFunction<SingleCanalBinlog>() {
-                    @Override
-                    public void invoke(SingleCanalBinlog singleCanalBinlog, Context context) throws Exception {
-                        System.out.println(JsonUtils.toString(singleCanalBinlog));
-                        Map<String, Object> beforeColumnMap = singleCanalBinlog.getBeforeColumnMap();
-                        Map<String, Object> afterColumnMap = singleCanalBinlog.getAfterColumnMap();
-                        System.out.println("nameEquals?: " + Objects.deepEquals(beforeColumnMap.get("name"), afterColumnMap.get("name")));
-                        System.out.println("deletedEquals?: " + Objects.deepEquals(beforeColumnMap.get("deleted"), afterColumnMap.get("deleted")));
-                    }
-                });
+                .addSink(new DemoSink(ConfigUtils.getConfig())).setParallelism(1);
         env.execute("DemoJob");
+    }
+
+    @RequiredArgsConstructor
+    private final static class DemoSink extends RichSinkFunction<SingleCanalBinlog> {
+        private final Config config;
+        private ObsWriter obsWriter;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            ConfigUtils.setConfig(config);
+            obsWriter = new ObsWriter("obs://hadoop-obs/flink/enterprise");
+            obsWriter.enableCache();
+        }
+
+        @Override
+        public void invoke(SingleCanalBinlog value, Context context) throws Exception {
+            System.out.println((JsonUtils.toString(value)));
+        }
     }
 }
