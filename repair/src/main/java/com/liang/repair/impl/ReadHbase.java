d914a2baa7211df365d4fa7c529daee90e06d917
liang <li_ang_666@163.com>
2023-08-22 22:51:09 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/ReadHbase.java
+++ repair/src/main/java/com/liang/repair/impl/ReadHbase.java
@@ -15,7 +15,7 @@ public class ReadHbase extends ConfigHolder {
     }
 
     public static void main(String[] args) {
-        HbaseOneRow hbaseOneRow = new HbaseOneRow(HbaseSchema.COMPANY_ALL_COUNT, "2997594259");
+        HbaseOneRow hbaseOneRow = new HbaseOneRow(HbaseSchema.COMPANY_ALL_COUNT, "6427262749");
         HbaseOneRow queryResult = query(hbaseOneRow);
 //        queryResult.put("manage_dis_pute_count", null);
 //        update(queryResult);
