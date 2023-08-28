1544b77f4f56bbe7baaa126ef0ec8c3482ec415f
liang <li_ang_666@163.com>
2023-08-28 15:52:23 +0800

    1

--- spark/src/main/java/com/liang/spark/job/AnnualReport2HiveJob.java
+++ spark/src/main/java/com/liang/spark/job/AnnualReport2HiveJob.java
@@ -16,9 +16,20 @@ public class AnnualReport2HiveJob {
         TableFactory.jdbc(spark, "gauss", "entity_annual_report_ebusiness_details")
                 .createOrReplaceTempView("t7");
 
+        TableFactory.jdbc(spark, "463.bdp_equity", "entity_controller_details")
+                .createOrReplaceTempView("t");
+        TableFactory.jdbc(spark, "463.bdp_equity", "entity_beneficiary_details")
+                .createOrReplaceTempView("tt");
+        TableFactory.jdbc(spark, "463.bdp_equity", "shareholder_identity_type_details")
+                .createOrReplaceTempView("ttt");
+
         spark.sql("insert overwrite table test.entity_annual_report_shareholder_equity_change_details select /*+ REPARTITION(180) */ * from t3");
         spark.sql("insert overwrite table test.entity_annual_report_shareholder_equity_details select /*+ REPARTITION(360) */ * from t4");
         spark.sql("insert overwrite table test.entity_annual_report_investment_details select /*+ REPARTITION(180) */ * from t6");
         spark.sql("insert overwrite table test.entity_annual_report_ebusiness_details select /*+ REPARTITION(180) */ * from t7");
+
+        spark.sql("insert overwrite table test.entity_controller_details select /*+ REPARTITION(180) */ * from t");
+        spark.sql("insert overwrite table test.entity_beneficiary_details select /*+ REPARTITION(180) */ * from tt");
+        spark.sql("insert overwrite table test.shareholder_identity_type_details select /*+ REPARTITION(180) */ * from ttt");
     }
 }
