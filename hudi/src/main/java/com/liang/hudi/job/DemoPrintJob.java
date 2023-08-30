ddf388788a615dfbeffe134bd7bc81c5970bf6f6
liang <li_ang_666@163.com>
2023-08-30 17:22:04 +0800

    1

--- hudi/src/main/java/com/liang/hudi/job/DemoPrintJob.java
+++ hudi/src/main/java/com/liang/hudi/job/DemoPrintJob.java
@@ -11,17 +11,17 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-public class DemoJob {
+public class DemoPrintJob {
     public static void main(String[] args) throws Exception {
         // read file
-        InputStream stream1 = DemoJob.class.getClassLoader().getResourceAsStream("DemoJob/source.sql");
+        InputStream stream1 = DemoPrintJob.class.getClassLoader().getResourceAsStream("DemoJob/source.sql");
         String source = IOUtils.toString(stream1, StandardCharsets.UTF_8);
-        InputStream stream2 = DemoJob.class.getClassLoader().getResourceAsStream("DemoJob/sink.sql");
+        InputStream stream2 = DemoPrintJob.class.getClassLoader().getResourceAsStream("DemoJob/sink.sql");
         String sink = IOUtils.toString(stream2, StandardCharsets.UTF_8);
 
         // create env
         Configuration configuration = new Configuration();
-        configuration.setString("rest.bind-port", "54321");
+        configuration.setString("rest.bind-port", "12345");
         configuration.setString("state.checkpoints.dir", "file:///Users/liang/Desktop/flink-checkpoints/");
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(configuration);
         CheckpointConfig checkpointConfig = env.getCheckpointConfig();
@@ -32,6 +32,6 @@ public class DemoJob {
         tEnv.executeSql(source);
         tEnv.executeSql(sink);
 
-        tEnv.executeSql("insert into hudi_table select id,company_id,shareholder_id,shareholder_entity_type,shareholder_name_id,investment_ratio_total,is_controller,is_ultimate,is_big_shareholder,is_controlling_shareholder,equity_holding_path,create_time,update_time,is_deleted from ratio_path_company").print();
+        tEnv.executeSql("select * from hudi_table").print();
     }
 }
--- hudi/src/main/resources/DemoJob/sink.sql
+++ hudi/src/main/resources/DemoJob/sink.sql
@@ -2,25 +2,25 @@ create table hudi_table(
     id                         BIGINT,
     company_id                 BIGINT,
     shareholder_id             STRING,
-    shareholder_entity_type    SMALLINT,
+    shareholder_entity_type    BIGINT,
     shareholder_name_id        BIGINT,
     investment_ratio_total     DECIMAL(24, 12),
-    is_controller              SMALLINT,
-    is_ultimate                SMALLINT,
-    is_big_shareholder         SMALLINT,
-    is_controlling_shareholder SMALLINT,
+    is_controller              BIGINT,
+    is_ultimate                BIGINT,
+    is_big_shareholder         BIGINT,
+    is_controlling_shareholder BIGINT,
     equity_holding_path        STRING,
     create_time                TIMESTAMP(0),
     update_time                TIMESTAMP(0),
-    is_deleted                 SMALLINT,
+    is_deleted                 BIGINT,
   PRIMARY KEY (id) NOT ENFORCED
 ) with (
   'connector' = 'hudi',
-  'path' = 'hdfs:///liang/hudi_table',
+  'path' = 'obs://hadoop-obs/flink/hudi_table',
   'table.type' = 'MERGE_ON_READ',
   -- read
   'read.streaming.enabled' = 'true',
-  'read.streaming.start-commit' = '20230830010101',
+  'read.streaming.start-commit' = '00000000000000',
   'read.streaming.check-interval' = '3',
   -- compaction
   'compaction.schedule.enabled' = 'true',
@@ -30,5 +30,10 @@ create table hudi_table(
   'compaction.delta_seconds' = '30',
   'compaction.max_memory' = '512',
   -- changelog
-  'changelog.enabled' = 'true'
+  'changelog.enabled' = 'true',
+  -- obs
+  'fs.obs.impl' = 'org.apache.hadoop.fs.obs.OBSFileSystem',
+  'fs.obs.access.key' = 'NT5EWZ4FRH54R2R2CB8G',
+  'fs.obs.secret.key' = 'BJok3jQFTmFYUS68lFWegazYggw5anKsOFUb65bS',
+  'fs.obs.endpoint' = 'obs.cn-north-4.myhuaweicloud.com'
 )
