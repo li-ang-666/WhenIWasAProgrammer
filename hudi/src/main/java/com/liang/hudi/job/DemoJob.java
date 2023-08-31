97cccf80c13219a7693bcd2ca1e89a63136b5d12
liang <li_ang_666@163.com>
2023-08-31 19:20:37 +0800

    1

--- hudi/src/main/java/com/liang/hudi/job/DemoJob.java
+++ hudi/src/main/java/com/liang/hudi/job/DemoJob.java
@@ -1,25 +1,14 @@
 package com.liang.hudi.job;
 
 
+import com.liang.hudi.basic.TableEnvironmentFactory;
 import com.liang.hudi.basic.TableFactory;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.CheckpointingMode;
-import org.apache.flink.streaming.api.environment.CheckpointConfig;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 public class DemoJob {
     public static void main(String[] args) throws Exception {
         // create env
-        Configuration configuration = new Configuration();
-        configuration.setString("rest.bind-port", "54321");
-        configuration.setString("state.checkpoints.dir", "file:///Users/liang/Desktop/flink-checkpoints/");
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(configuration);
-        CheckpointConfig checkpointConfig = env.getCheckpointConfig();
-        checkpointConfig.enableUnalignedCheckpoints();
-        checkpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
-        checkpointConfig.setCheckpointInterval(1000 * 30);
-        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+        StreamTableEnvironment tEnv = TableEnvironmentFactory.create();
         // exec sql
         tEnv.executeSql(TableFactory.fromFile("cdc.sql"));
         tEnv.executeSql(TableFactory.fromFile("hudi.sql"));
