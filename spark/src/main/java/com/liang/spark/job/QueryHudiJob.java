188475a6f9cf050e22531a3728c38f362589f79a
liang <li_ang_666@163.com>
2023-09-02 16:11:38 +0800

    1

--- spark/src/main/java/com/liang/spark/job/QueryHudiJob.java
+++ spark/src/main/java/com/liang/spark/job/QueryHudiJob.java
@@ -3,12 +3,12 @@ package com.liang.spark.job;
 import com.liang.spark.basic.SparkSessionFactory;
 import org.apache.spark.sql.SparkSession;
 
-public class Hudi {
+public class QueryHudiJob {
     public static void main(String[] args) {
         SparkSession spark = SparkSessionFactory.createSpark(args);
         spark.read().format("hudi")
                 .load("obs://hadoop-obs/hudi/hudi_table")
                 .createOrReplaceTempView("hudi_table");
-        spark.sql("select count(1) from hudi_table").show();
+        spark.sql("select count(distinct id) from hudi_table").show();
     }
 }
