00ed3b1788e8137f0cbc608a22fa7fa827465326
liang <li_ang_666@163.com>
2023-09-05 18:07:48 +0800

    1

--- flink/src/main/java/com/liang/flink/project/company/base/info/CompanyBaseInfoService.java
+++ flink/src/main/java/com/liang/flink/project/company/base/info/CompanyBaseInfoService.java
@@ -144,8 +144,8 @@ public class CompanyBaseInfoService {
         columnMap.put("entity_register_address", ifNull(govMap, "address", ""));
         // 经营范围
         columnMap.put("business_registration_scope", ifNull(govMap, "scope", ""));
-        // 是否中央级事业单位
-        columnMap.put("is_national_public_institution", "4".equals(entityProperty));
+        // 实体性质
+        columnMap.put("entity_property", entityProperty);
         // 组织机构代码 基础数据:端上无
         columnMap.put("organization_code", ifNull(enterpriseMap, "org_number", ""));
         // 纳税人识别号 基础数据:端上无
