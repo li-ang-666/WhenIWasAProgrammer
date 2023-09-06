8a2b448863f73104564d7681ecfc1fcead3c2c5e
liang <li_ang_666@163.com>
2023-09-06 15:19:59 +0800

    1

--- flink/src/main/java/com/liang/flink/project/evaluation/institution/candidate/impl/CompanyLawHumanRealtion.java
+++ flink/src/main/java/com/liang/flink/project/evaluation/institution/candidate/impl/CompanyLawHumanRealtion.java
@@ -2,6 +2,7 @@ package com.liang.flink.project.evaluation.institution.candidate.impl;
 
 import com.liang.common.service.SQL;
 import com.liang.common.util.SqlUtils;
+import com.liang.common.util.TycUtils;
 import com.liang.flink.dto.SingleCanalBinlog;
 import com.liang.flink.service.data.update.AbstractDataUpdate;
 
@@ -15,11 +16,12 @@ public class CompanyLawHumanRealtion extends AbstractDataUpdate<String> {
     public List<String> updateWithReturn(SingleCanalBinlog singleCanalBinlog) {
         Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
         String sourceTable = String.valueOf(columnMap.get("source_table"));
+        String sourceId = String.valueOf(columnMap.get("source_id"));
         List<String> sqls = new ArrayList<>();
-        if ("zhixinginfo_evaluate".equals(sourceTable)) {
+        if ("zhixinginfo_evaluate".equals(sourceTable) && TycUtils.isUnsignedId(sourceId)) {
             String sql = new SQL().UPDATE("zhixinginfo_evaluate_index")
                     .SET("update_time = date_add(update_time, interval 1 second)")
-                    .WHERE("main_id = " + SqlUtils.formatValue(columnMap.get("source_id")))
+                    .WHERE("main_id = " + SqlUtils.formatValue(sourceId))
                     .toString();
             sqls.add(sql);
         }
