ce61ce274362722ddac2af48cea7d2cb16ddacaa
liang <li_ang_666@163.com>
2023-09-10 17:09:09 +0800

    1

--- spark/src/main/java/com/liang/spark/job/PxCheckJob.java
+++ spark/src/main/java/com/liang/spark/job/PxCheckJob.java
@@ -14,9 +14,9 @@ public class PxCheckJob {
                 .createOrReplaceTempView("tt");
         TableFactory.jdbc(spark, "463.bdp_equity", "shareholder_identity_type_details")
                 .createOrReplaceTempView("ttt");
-        spark.sql("insert overwrite table ads.ads_bdp_equity_entity_controller_details partition(pt = '20230908') select /*+ REPARTITION(360) */ * from t");
-        spark.sql("insert overwrite table ads.ads_bdp_equity_entity_beneficiary_details partition(pt = '20230908') select /*+ REPARTITION(360) */ * from tt");
-        spark.sql("insert overwrite table ads.ads_bdp_equity_shareholder_identity_type_details partition(pt = '20230908') select /*+ REPARTITION(360) */ * from ttt");
+        spark.sql("insert overwrite table ads.ads_bdp_equity_entity_controller_details partition(pt = '20230909') select /*+ REPARTITION(360) */ * from t");
+        spark.sql("insert overwrite table ads.ads_bdp_equity_entity_beneficiary_details partition(pt = '20230909') select /*+ REPARTITION(360) */ * from tt");
+        spark.sql("insert overwrite table ads.ads_bdp_equity_shareholder_identity_type_details partition(pt = '20230909') select /*+ REPARTITION(360) */ * from ttt");
         //年报
         TableFactory.jdbc(spark, "gauss", "entity_annual_report_shareholder_equity_change_details")
                 .createOrReplaceTempView("t3");
@@ -26,16 +26,16 @@ public class PxCheckJob {
                 .createOrReplaceTempView("t6");
         TableFactory.jdbc(spark, "gauss", "entity_annual_report_ebusiness_details")
                 .createOrReplaceTempView("t7");
-        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_annual_report_shareholder_equity_change_details partition(pt = '20230908') select /*+ REPARTITION(360) */ * from t3");
-        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_annual_report_shareholder_equity_details partition(pt = '20230908') select /*+ REPARTITION(360) */ * from t4");
-        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_annual_report_investment_details partition(pt = '20230908') select /*+ REPARTITION(360) */ * from t6");
-        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_annual_report_ebusiness_details partition(pt = '20230908') select /*+ REPARTITION(360) */ * from t7");
+        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_annual_report_shareholder_equity_change_details partition(pt = '20230909') select /*+ REPARTITION(360) */ * from t3");
+        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_annual_report_shareholder_equity_details partition(pt = '20230909') select /*+ REPARTITION(360) */ * from t4");
+        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_annual_report_investment_details partition(pt = '20230909') select /*+ REPARTITION(360) */ * from t6");
+        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_annual_report_ebusiness_details partition(pt = '20230909') select /*+ REPARTITION(360) */ * from t7");
         // 工商
         TableFactory.jdbc(spark, "469.entity_operation_development", "entity_mainland_general_registration_info_details")
                 .createOrReplaceTempView("t_company");
         TableFactory.jdbc(spark, "469.entity_operation_development", "entity_mainland_public_institution_registration_info_details")
                 .createOrReplaceTempView("t_gov");
-        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_mainland_general_registration_info_details partition(pt = '20230908') select /*+ REPARTITION(360) */ * from t_company");
-        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_mainland_public_institution_registration_info_details partition(pt = '20230908') select /*+ REPARTITION(180) */ * from t_gov");
+        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_mainland_general_registration_info_details partition(pt = '20230909') select /*+ REPARTITION(360) */ * from t_company");
+        spark.sql("insert overwrite table ads.ads_entity_operation_development_entity_mainland_public_institution_registration_info_details partition(pt = '20230909') select /*+ REPARTITION(180) */ * from t_gov");
     }
 }
