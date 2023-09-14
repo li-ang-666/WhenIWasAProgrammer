d7b393fb76ef2ce641d783a9afd5a12f9d7891e1
liang <li_ang_666@163.com>
2023-09-14 21:30:05 +0800

    1

--- hudi/src/main/java/com/liang/hudi/job/DemoJob.java
+++ hudi/src/main/java/com/liang/hudi/job/DemoJob.java
@@ -1,6 +1,7 @@
 package com.liang.hudi.job;
 
 
+import com.liang.common.util.ApolloUtils;
 import com.liang.hudi.basic.TableEnvironmentFactory;
 import com.liang.hudi.basic.TableFactory;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
@@ -11,7 +12,7 @@ public class DemoJob {
         StreamTableEnvironment tEnv = TableEnvironmentFactory.create();
         // exec sql
         tEnv.executeSql(TableFactory.fromFile("cdc.sql"));
-        tEnv.executeSql(TableFactory.fromFile("hudi.sql"));
+        tEnv.executeSql(ApolloUtils.get("hudi_table"));
         tEnv.executeSql("insert into hudi_table select id,company_id,shareholder_id,shareholder_entity_type,shareholder_name_id,investment_ratio_total,is_controller,is_ultimate,is_big_shareholder,is_controlling_shareholder,equity_holding_path,create_time,update_time,is_deleted from cdc_table").print();
     }
 }
