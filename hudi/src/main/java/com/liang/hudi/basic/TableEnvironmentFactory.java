11ac4d5d806db0553582dfdfa857e16284044db8
liang <li_ang_666@163.com>
2023-09-20 11:17:24 +0800

    1

--- hudi/src/main/java/com/liang/hudi/basic/TableEnvironmentFactory.java
+++ hudi/src/main/java/com/liang/hudi/basic/TableEnvironmentFactory.java
@@ -14,8 +14,8 @@ import static org.apache.flink.streaming.api.environment.CheckpointConfig.Extern
 @Slf4j
 @UtilityClass
 public class TableEnvironmentFactory {
-    private final static long CHECKPOINT_INTERVAL = 1000 * 90;
-    private final static long CHECKPOINT_TIMEOUT = 1000 * 60 * 10;
+    private final static long CHECKPOINT_INTERVAL = 1000 * 60 * 5;
+    private final static long CHECKPOINT_TIMEOUT = 1000 * 60 * 30;
 
     public static StreamTableEnvironment create() {
         StreamExecutionEnvironment env = initEnv();
