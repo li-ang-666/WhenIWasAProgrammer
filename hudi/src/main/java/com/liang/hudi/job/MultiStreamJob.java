a3c22ad7025a3444f2fa94cb9dcb1ba20428447c
liang <li_ang_666@163.com>
2023-10-12 18:41:38 +0800

    1

--- hudi/src/main/java/com/liang/hudi/job/MultiStreamJob.java
+++ hudi/src/main/java/com/liang/hudi/job/MultiStreamJob.java
@@ -1,14 +1,10 @@
 package com.liang.hudi.job;
 
 import com.liang.common.util.ApolloUtils;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.CheckpointingMode;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamStatementSet;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 public class MultiStreamJob {
@@ -19,17 +15,6 @@ public class MultiStreamJob {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(configuration);
         env.enableCheckpointing(1000 * 60, CheckpointingMode.EXACTLY_ONCE);
         env.setParallelism(1);
-
-
-        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
-                .setBootstrapServers("10.99.202.90:9092,10.99.206.80:9092,10.99.199.2:9092")
-                .setTopics("e1d4c.json.prism_shareholder_path.ratio_path_company")
-                .setGroupId("hudi-demo-job")
-                .setStartingOffsets(OffsetsInitializer.latest())
-                .setValueOnlyDeserializer(new SimpleStringSchema())
-                .build();
-        DataStreamSource<String> sourceStream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "KafkaSource");
-
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
         String content = ApolloUtils.get("flink-sqls");
         for (String sql : content.split(";")) {
@@ -37,12 +22,10 @@ public class MultiStreamJob {
                 tEnv.executeSql(sql);
             }
         }
+        StreamStatementSet statementSet = tEnv.createStatementSet();
+        statementSet.addInsertSql("insert into dwd_ratio_path_company select * from ods_ratio_path_company");
+        statementSet.addInsertSql("insert into dwd_enterprise select * from ods_enterprise");
+        statementSet.execute();
 
-//        StreamStatementSet statementSet = tEnv.createStatementSet();
-//        statementSet.addInsertSql("insert into dwd_ratio_path_company select * from ods_ratio_path_company");
-//        statementSet.addInsertSql("insert into dwd_enterprise select * from ods_enterprise");
-//        statementSet.execute();
-
-        env.execute();
     }
 }
