db3406864a17fbc741d324d3909182aadb15208c
liang <li_ang_666@163.com>
2023-10-12 19:25:21 +0800

    1

--- hudi/src/main/java/com/liang/hudi/job/DemoJob.java
+++ hudi/src/main/java/com/liang/hudi/job/DemoJob.java
@@ -3,9 +3,11 @@ package com.liang.hudi.job;
 
 import com.liang.common.util.ApolloUtils;
 import com.liang.hudi.basic.TableEnvironmentFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
+@Slf4j
 public class DemoJob {
     public static void main(String[] args) throws Exception {
         // create env
@@ -13,6 +15,7 @@ public class DemoJob {
         // exec sql
         for (String sql : ApolloUtils.get("flink-sqls").split(";")) {
             if (StringUtils.isNotBlank(sql)) {
+                log.info("sql: {}", sql);
                 tEnv.executeSql(sql);
             }
         }
