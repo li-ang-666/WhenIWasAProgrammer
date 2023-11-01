70e494af371b1c52e4fcd2fc398087b0bfe7e29e
liang <li_ang_666@163.com>
2023-11-01 19:17:54 +0800

    1

---
+++ repair/src/main/java/com/liang/repair/impl/benefitRepair.java
@@ -0,0 +1,20 @@
+package com.liang.repair.impl;
+
+import com.liang.common.service.SQL;
+import com.liang.common.service.database.template.JdbcTemplate;
+import com.liang.common.util.SqlUtils;
+import com.liang.repair.service.ConfigHolder;
+
+public class benefitRepair extends ConfigHolder {
+    public static void main(String[] args) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate("225.prism_shareholder_path");
+        String companyId = "2441510367";
+        long index = Long.parseLong(companyId) % 10000 % 64;
+        String table = index >= 10 ? "ratio_path_shareholder_0" + index : "ratio_path_shareholder_00" + index;
+        String sql = new SQL().UPDATE(table)
+                .SET("deleted = 1")
+                .WHERE("company_graph_id = " + SqlUtils.formatValue(companyId))
+                .toString();
+        jdbcTemplate.update(sql);
+    }
+}
