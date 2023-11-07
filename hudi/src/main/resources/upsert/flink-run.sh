d540bdd550d0bf128d7c8644e4d53d7650e243b8
liang <li_ang_666@163.com>
2023-11-07 18:54:56 +0800

    1

--- hudi/src/main/resources/upsert/flink-run.sh
+++ hudi/src/main/resources/upsert/flink-run.sh
@@ -5,9 +5,9 @@
 #                                  #
 ####################################
 # 类名
-export className=FlinkJob
+export className=DemoJob
 # 配置文件名 config.yml or repair.yml
-export configName=config.yml
+export configName=hudi.yml
 
 ####################################
 #                                  #
@@ -15,8 +15,9 @@ export configName=config.yml
 #                                  #
 ####################################
 # flink env
-export FLINK_HOME=/data/hudi/flink-1.16.2
-export FLINK_CONF_DIR=/data/hudi/flink-conf
+export FLINK_HOME=/home/hive/flink/flink-1.17.1
+export FLINK_CONF_DIR=/home/hive/flink/flink-conf
+export HADOOP_CLASSPATH=`hadoop classpath`
 # checkpoint dir
 export folderName=$(echo ${className} | sed -E 's/([A-Z])/-\1/g' | sed -E 's/^-//g' | tr 'A-Z' 'a-z')
 # yarn application name
@@ -28,12 +29,14 @@ export restoreDir=$(if [[ $1 == hdfs* ]] ; then echo '-s '$1 ; else echo '' ; fi
 #                                  #
 #    maybe change Memory or Slot   #
 #                                  #
-####################################
-/data/hudi/flink-1.16.2/bin/flink run-application -t yarn-application ${restoreDir} \
-  -D jobmanager.memory.process.size=2048mb \
-  -D taskmanager.memory.process.size=4096mb \
+##########u#########################
+/home/hive/flink/flink-1.17.1/bin/flink run-application -t yarn-application ${restoreDir} \
+  -D jobmanager.memory.process.size=1g \
+  -D taskmanager.memory.process.size=3g \
   -D taskmanager.numberOfTaskSlots=4 \
+  -D parallelism.default=1 \
   -D state.checkpoints.dir=hdfs:///hudi/flink-checkpoints/${folderName} \
+  -D yarn.application.priority=5 \
   -D yarn.ship-files=${configName} \
-  -D yarn.application.name=${jobName} \
+  -D yarn.application.name=HudiTest \
   -c com.liang.hudi.job.${className} hudi-1.0.jar ${configName}
