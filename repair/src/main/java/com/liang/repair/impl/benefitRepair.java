06a3cdc1bd7233611d5088a9da0b5982f4db6dc6
liang <li_ang_666@163.com>
2023-11-10 11:37:31 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/benefitRepair.java
+++ repair/src/main/java/com/liang/repair/impl/benefitRepair.java
@@ -8,7 +8,7 @@ import com.liang.repair.service.ConfigHolder;
 public class benefitRepair extends ConfigHolder {
     public static void main(String[] args) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate("225.prism_shareholder_path");
-        String companyId = "3149762659";
+        String companyId = "4339518056";
         long index = Long.parseLong(companyId) % 10000 % 64;
         String table = index >= 10 ? "ratio_path_shareholder_0" + index : "ratio_path_shareholder_00" + index;
         String sql = new SQL().UPDATE(table)
