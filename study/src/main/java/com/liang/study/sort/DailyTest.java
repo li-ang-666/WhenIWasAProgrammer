a9e6ba5331d3271cc7237a092f12f626a7972e05
liang <li_ang_666@163.com>
2023-11-10 15:41:48 +0800

    1

--- study/src/main/java/com/liang/study/sort/DailyTest.java
+++ study/src/main/java/com/liang/study/sort/DailyTest.java
@@ -2,8 +2,6 @@ package com.liang.study.sort;
 
 import org.junit.Test;
 
-import static com.liang.study.sort.ArrayUtils.swap;
-
 public class DailyTest {
     @Test
     public void test() {
@@ -11,13 +9,6 @@ public class DailyTest {
     }
 
     public void sort(int[] arr) {
-        int n = arr.length;
-        for (int incr = n / 2; incr >= 1; incr /= 2) {
-            for (int i = incr; i <= n - 1; i++) {
-                for (int j = i; j - incr >= 0 && arr[j - incr] > arr[j]; j -= incr) {
-                    swap(arr, j, j - incr);
-                }
-            }
-        }
+
     }
 }
