b635b9b7a8ec8017c8a68d1173ecc02d11b3616f
liang <li_ang_666@163.com>
2023-11-10 15:40:45 +0800

    1

--- study/src/main/java/com/liang/study/test/Test.java
+++ study/src/main/java/com/liang/study/test/Test.java
@@ -1,26 +1,6 @@
 package com.liang.study.test;
 
-import com.liang.study.sort.ArrayUtils;
-import com.liang.study.sort.ISort;
-
 public class Test {
     public static void main(String[] args) {
-        ArrayUtils.testSort(new ISort() {
-            @Override
-            public void sort(int[] arr) {
-                int length = arr.length;
-                for (int i = 0; i < length; i++) {
-                    int index = i;
-                    for (int j = i + 1; j < length; j++) {
-                        if (arr[j] <= arr[index]) {
-                            index = j;
-                        }
-                    }
-                    if (index != i) {
-                        ArrayUtils.swap(arr, i, index);
-                    }
-                }
-            }
-        });
     }
 }
