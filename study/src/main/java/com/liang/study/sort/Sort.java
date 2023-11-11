eeda778dadb2f8f70d4c315716c0b3441db06ba0
liang <li_ang_666@163.com>
2023-11-11 23:38:28 +0800

    1

---
+++ study/src/main/java/com/liang/study/sort/Sort.java
@@ -0,0 +1,12 @@
+package com.liang.study.sort;
+
+public class Sort implements ISort {
+    public static void main(String[] args) {
+        ArrayUtils.testSort(new Sort());
+    }
+
+    @Override
+    public void sort(int[] arr) {
+
+    }
+}
