967afe9523525e8f57a7906f95b41c3b520c33ad
liang <li_ang_666@163.com>
2023-11-11 17:57:11 +0800

    1

--- study/src/main/java/com/liang/study/sort/MergeSort.java
+++ study/src/main/java/com/liang/study/sort/MergeSort.java
@@ -1,48 +1,41 @@
 package com.liang.study.sort;
 
-import org.junit.Test;
-
-public class MergeSort {
-    @Test
-    public void test() {
-        ArrayUtils.testSort(this::sort);
-    }
-
-    public void sort(int[] arr) {
-        sort(arr, new int[arr.length], 0, arr.length - 1);
+public class MergeSort implements ISort {
+    public static void main(String[] args) {
+        ArrayUtils.testSort(new MergeSort());
     }
 
     /**
-     * 归并排序是
-     * 1、跳出
-     * 2、递归左边
-     * 3、递归右边
-     * 4、p1=... p2=... p3=... 复杂计算
+     * arr[p1]<=arr[p2] 时是稳定排序
      */
-    public void sort(int[] arr, int[] temp, int start, int end) {
-        if (start >= end) {
+    @Override
+    public void sort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1, new int[arr.length]);
+    }
+
+    private void mergeSort(int[] arr, int l, int r, int[] tmp) {
+        if (l >= r) {
             return;
         }
-        int mid = (start + end) / 2;
-        sort(arr, temp, start, mid);
-        sort(arr, temp, mid + 1, end);
-        int p1 = start, p2 = mid + 1, p3 = start;
-        //p1, p2两个指针指向两个数组的头, 谁小谁入位, 都小左边的先
-        while (p1 <= mid && p2 <= end) {
-            if (arr[p1] <= arr[p2])
-                temp[p3++] = arr[p1++];
-            else
-                temp[p3++] = arr[p2++];
+        int mid = (l + r) / 2;
+        mergeSort(arr, l, mid, tmp);
+        mergeSort(arr, mid + 1, r, tmp);
+        int p1 = l, p2 = mid + 1, p3 = l;
+        while (p1 <= mid && p2 <= r) {
+            if (arr[p1] <= arr[p2]) {
+                tmp[p3++] = arr[p1++];
+            } else {
+                tmp[p3++] = arr[p2++];
+            }
         }
-        //比较到最后, 总有一个数组已经都入位了, 另一个还差几个, 直接入位到尾部就好
         while (p1 <= mid) {
-            temp[p3++] = arr[p1++];
+            tmp[p3++] = arr[p1++];
         }
-        while (p2 <= end) {
-            temp[p3++] = arr[p2++];
+        while (p2 <= r) {
+            tmp[p3++] = arr[p2++];
         }
-        //比较的时候写temp, 都入位后, 把temp的数据复制回原数组
-        for (int i = start; i <= end; i++)
-            arr[i] = temp[i];
+        for (int i = l; i <= r; i++) {
+            arr[i] = tmp[i];
+        }
     }
 }
