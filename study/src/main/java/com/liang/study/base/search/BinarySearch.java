26f4af65d7b359e2598ce71f98092e21e9a9422d
liang <li_ang_666@163.com>
2023-11-13 15:54:58 +0800

    1

--- study/src/main/java/com/liang/study/base/search/BinarySearch.java
+++ study/src/main/java/com/liang/study/base/search/BinarySearch.java
@@ -2,23 +2,23 @@ package com.liang.study.base.search;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println(find(new int[]{1, 3, 5, 7, 9}, 1));
-        System.out.println(find(new int[]{1, 3, 5, 7, 9}, 2));
-        System.out.println(find(new int[]{1, 3, 5, 7, 9}, 3));
-        System.out.println(find(new int[]{1, 3, 5, 7, 9}, 4));
-        System.out.println(find(new int[]{1, 3, 5, 7, 9}, 5));
+        System.out.println(search(new int[]{1, 3, 5, 7, 9}, 1));
+        System.out.println(search(new int[]{1, 3, 5, 7, 9}, 2));
+        System.out.println(search(new int[]{1, 3, 5, 7, 9}, 3));
+        System.out.println(search(new int[]{1, 3, 5, 7, 9}, 4));
+        System.out.println(search(new int[]{1, 3, 5, 7, 9}, 5));
     }
 
-    public static int find(int[] arr, int num) {
+    private static int search(int[] arr, int num) {
         int l = 0, r = arr.length - 1;
         while (l <= r) {
             int mid = (l + r) / 2;
-            if (num == arr[mid]) {
-                return mid;
-            } else if (num < arr[mid]) {
+            if (num < arr[mid]) {
                 r = mid - 1;
-            } else {
+            } else if (num > arr[mid]) {
                 l = mid + 1;
+            } else {
+                return mid;
             }
         }
         //return l; //若是二分插入, 则返回l
