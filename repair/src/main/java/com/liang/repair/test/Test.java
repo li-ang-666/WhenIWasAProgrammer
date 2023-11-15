8b09698db1449933ab192817e7ce71c76dcabf00
liang <li_ang_666@163.com>
2023-11-15 21:27:28 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -6,10 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.UUID;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class Test extends ConfigHolder {
     public static void main(String[] args) throws Exception {
         System.out.println(ObjectSizeCalculator.getObjectSize(UUID.randomUUID() + StringUtils.repeat(" ", 455)));
+        synchronized (ConfigHolder.class) {
+            ConfigHolder.class.wait(1000);
+        }
+        new ReentrantLock().newCondition();
     }
 }
