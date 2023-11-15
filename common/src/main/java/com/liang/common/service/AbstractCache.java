99399b8df474a2e8f04095fc792f9a7fb4f85157
liang <li_ang_666@163.com>
2023-11-15 15:41:25 +0800

    1

--- common/src/main/java/com/liang/common/service/AbstractCache.java
+++ common/src/main/java/com/liang/common/service/AbstractCache.java
@@ -10,9 +10,12 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public abstract class AbstractCache<K, V> {
+    private final Lock lock = new ReentrantLock();
     private final Map<K, Queue<V>> cache = new ConcurrentHashMap<>();
     private final KeySelector<K, V> keySelector;
     private final AtomicBoolean enableCache = new AtomicBoolean(false);
@@ -34,33 +37,34 @@ public abstract class AbstractCache<K, V> {
     }
 
     public final void enableCache(int cacheMilliseconds, int cacheRecords) {
-        if (!enableCache.get()) {
-            synchronized (cache) {
-                if (!enableCache.get()) {
-                    this.cacheMilliseconds = cacheMilliseconds;
-                    this.cacheRecords = cacheRecords;
-                    DaemonExecutor.launch("AbstractCacheThread", new Runnable() {
-                        private long lastSendTime = System.currentTimeMillis();
+        if (enableCache.get()) return;
+        lock.lock();
+        try {
+            if (enableCache.get()) return;
+            this.cacheMilliseconds = cacheMilliseconds;
+            this.cacheRecords = cacheRecords;
+            DaemonExecutor.launch("AbstractCacheThread", new Runnable() {
+                private long lastSendTime = System.currentTimeMillis();
 
-                        @Override
-                        @SneakyThrows(InterruptedException.class)
-                        public void run() {
-                            while (true) {
-                                TimeUnit.MILLISECONDS.sleep(100);
-                                if (!cache.isEmpty() && System.currentTimeMillis() - lastSendTime >= cacheMilliseconds) {
-                                    // 时间触发
-                                    flush();
-                                    lastSendTime = System.currentTimeMillis();
-                                } else if (cache.values().stream().anyMatch(queue -> queue.size() >= cacheRecords)) {
-                                    // 大小触发
-                                    flush();
-                                }
-                            }
+                @Override
+                @SneakyThrows(InterruptedException.class)
+                public void run() {
+                    while (true) {
+                        TimeUnit.MILLISECONDS.sleep(100);
+                        if (!cache.isEmpty() && System.currentTimeMillis() - lastSendTime >= cacheMilliseconds) {
+                            // 时间触发
+                            flush();
+                            lastSendTime = System.currentTimeMillis();
+                        } else if (cache.values().stream().anyMatch(queue -> queue.size() >= cacheRecords)) {
+                            // 大小触发
+                            flush();
                         }
-                    });
-                    enableCache.set(true);
+                    }
                 }
-            }
+            });
+            enableCache.set(true);
+        } finally {
+            lock.unlock();
         }
     }
 
@@ -76,29 +80,32 @@ public abstract class AbstractCache<K, V> {
         values.removeIf(Objects::isNull);
         if (values.isEmpty()) return;
         // 保证内存不超出限制
-        long pre, after, i = 0;
+        long pre, after;
         long sizeOfValues = values.stream()
                 .map(ObjectSizeCalculator::getObjectSize)
                 .reduce(0L, Long::sum);
         do {
-            if (i++ > 999) throw new RuntimeException("CAS loop too many times");
             pre = bufferUsed.get();
             after = pre + sizeOfValues;
         } while (after > bufferMax || !bufferUsed.compareAndSet(pre, after));
         // 同一批次的写入, 不拆开
-        synchronized (cache) {
+        lock.lock();
+        try {
             for (V value : values) {
                 K key = keySelector.selectKey(value);
                 cache.putIfAbsent(key, new ConcurrentLinkedQueue<>());
                 cache.get(key).add(value);
             }
             if (!enableCache.get()) flush();
+        } finally {
+            lock.unlock();
         }
     }
 
     public final void flush() {
         if (cache.isEmpty()) return;
-        synchronized (cache) {
+        lock.lock();
+        try {
             if (cache.isEmpty()) return;
             long sizeOfValues = 0;
             for (Map.Entry<K, Queue<V>> entry : cache.entrySet()) {
@@ -110,6 +117,8 @@ public abstract class AbstractCache<K, V> {
             }
             cache.clear();
             bufferUsed.getAndAdd(-sizeOfValues);
+        } finally {
+            lock.unlock();
         }
     }
 
