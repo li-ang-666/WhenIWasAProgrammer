6844e22fad421419e7f745060124ba2f2305ddce
liang <li_ang_666@163.com>
2023-11-15 13:52:01 +0800

    1

--- study/src/main/java/com/liang/study/base/sort/SortTest.java
+++ study/src/main/java/com/liang/study/base/sort/SortTest.java
@@ -2,10 +2,6 @@ package com.liang.study.base.sort;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 public class SortTest {
     @Test
     public void testSort() {
@@ -13,7 +9,5 @@ public class SortTest {
     }
 
     private void sort(int[] arr) {
-        new ArrayList<String>().stream().filter(Objects::nonNull)
-                .collect(Collectors.joining());
     }
 }
