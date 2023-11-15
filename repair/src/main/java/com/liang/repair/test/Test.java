7eebcd3873d69229164f21e4844d36f11e40ac77
liang <li_ang_666@163.com>
2023-11-15 14:34:12 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -1,19 +1,18 @@
 package com.liang.repair.test;
 
-import com.liang.common.util.ObjectSizeCalculator;
 import com.liang.repair.service.ConfigHolder;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class Test extends ConfigHolder {
     @SneakyThrows
     public static void main(String[] args) throws Exception {
-        System.out.println(ObjectSizeCalculator.getObjectSize("a"));
-        System.out.println(ObjectSizeCalculator.getObjectSize("aa"));
-        System.out.println(ObjectSizeCalculator.getObjectSize("aaa"));
-        System.out.println(ObjectSizeCalculator.getObjectSize("aaaa"));
-        System.out.println(ObjectSizeCalculator.getObjectSize("aaaaa"));
-        System.out.println(ObjectSizeCalculator.getObjectSize("aaaaaa"));
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ReentrantLock lock = new ReentrantLock();
+        lock.newCondition();
     }
 }
