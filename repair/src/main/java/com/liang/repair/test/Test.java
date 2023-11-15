445146276074d47651d799aca886ccf28a5c9dbb
liang <li_ang_666@163.com>
2023-11-15 15:57:40 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -1,7 +1,6 @@
 package com.liang.repair.test;
 
 import com.liang.repair.service.ConfigHolder;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.ByteBuffer;
@@ -9,10 +8,9 @@ import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class Test extends ConfigHolder {
-    @SneakyThrows
     public static void main(String[] args) throws Exception {
         ByteBuffer buffer = ByteBuffer.allocate(1024);
         ReentrantLock lock = new ReentrantLock();
-        lock.newCondition();
+        lock.newCondition().await();
     }
 }
