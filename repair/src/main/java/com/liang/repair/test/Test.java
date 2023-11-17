877f670a47b837623b8cd7a2cb2474aac62b2d55
liang <li_ang_666@163.com>
2023-11-17 21:21:04 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -1,29 +1,10 @@
 package com.liang.repair.test;
 
 import com.liang.repair.service.ConfigHolder;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.concurrent.CyclicBarrier;
 
 @Slf4j
 public class Test extends ConfigHolder {
-    private final static CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
-
-
-    @org.junit.Test
-    public void test() {
-        for (int i = 0; i < 3; i++) {
-            new Thread(new R()).start();
-        }
-        System.out.println(1);
-    }
-
-    private final static class R implements Runnable {
-        @Override
-        @SneakyThrows
-        public void run() {
-            cyclicBarrier.await();
-        }
+    public static void main(String[] args) {
     }
 }
