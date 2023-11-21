a3f8b9646af38ab729ba6ae5ee56666bac6be9b2
liang <li_ang_666@163.com>
2023-11-21 16:26:04 +0800

    1

--- study/src/main/java/com/liang/study/test/Test.java
+++ study/src/main/java/com/liang/study/test/Test.java
@@ -1,21 +1,26 @@
 package com.liang.study.test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
-    private final static ThreadLocal<int[]> THREAD_LOCAL = ThreadLocal.withInitial(() -> new int[Integer.MAX_VALUE]);
-
     public static void main(String[] args) throws Exception {
-        ExecutorService executorService = Executors.newFixedThreadPool(Integer.MAX_VALUE);
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            executorService.submit(() -> {
-                THREAD_LOCAL.get();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(f());
+            }
+
+            private boolean f() {
                 LockSupport.park();
-            });
-            System.out.println(i);
-        }
-        System.out.println("done");
+                return Thread.interrupted();
+            }
+        });
+        thread.start();
+        TimeUnit.SECONDS.sleep(2);
+        thread.interrupt();
+        TimeUnit.SECONDS.sleep(2);
+        new ReentrantLock().newCondition().awaitUninterruptibly();
     }
 }
