609c49404f587ffb1bb45a0be0f2a166af704515
liang <li_ang_666@163.com>
2023-11-22 14:36:45 +0800

    1

--- study/src/main/java/com/liang/study/test/Test.java
+++ study/src/main/java/com/liang/study/test/Test.java
@@ -1,26 +1,6 @@
 package com.liang.study.test;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.LockSupport;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Test {
     public static void main(String[] args) throws Exception {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(f());
-            }
-
-            private boolean f() {
-                LockSupport.park();
-                return Thread.interrupted();
-            }
-        });
-        thread.start();
-        TimeUnit.SECONDS.sleep(2);
-        thread.interrupt();
-        TimeUnit.SECONDS.sleep(2);
-        new ReentrantLock().newCondition().awaitUninterruptibly();
     }
 }
