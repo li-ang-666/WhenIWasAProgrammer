5ecbd1557d599f56ca3627414f3810cb34ebad6b
liang <li_ang_666@163.com>
2023-11-28 22:30:52 +0800

    1

---
+++ hudi/src/main/java/com/liang/hudi/job/UpsertJob.java
@@ -0,0 +1,25 @@
+package com.liang.hudi.job;
+
+import com.liang.hudi.basic.TableEnvironmentFactory;
+import com.liang.hudi.basic.TableFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import static org.apache.hudi.common.model.WriteOperationType.UPSERT;
+
+@Slf4j
+public class UpsertJob {
+    public static void main(String[] args) {
+        // create env
+        StreamTableEnvironment tEnv = TableEnvironmentFactory.create();
+        // exec sql
+        String sqls = TableFactory.fromTemplate(UPSERT, args[1], args[2]);
+        for (String sql : sqls.split(";")) {
+            if (StringUtils.isNotBlank(sql)) {
+                log.info("sql: {}", sql);
+                tEnv.executeSql(sql);
+            }
+        }
+    }
+}
