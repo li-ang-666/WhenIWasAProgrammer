d8fe9cb6b831bca58f05adf6b2942453b39465ce
liang <li_ang_666@163.com>
2023-12-04 18:35:45 +0800

    1

---
+++ spark/src/main/java/com/liang/spark/job/CooperationPartnerJob.java
@@ -0,0 +1,20 @@
+package com.liang.spark.job;
+
+import com.liang.spark.basic.SparkSessionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.sql.SparkSession;
+
+@Slf4j
+public class CooperationPartnerJob {
+    public static void main(String[] args) throws Exception {
+        SparkSession spark = SparkSessionFactory.createSpark(args);
+        String sqls = IOUtils.toString(CooperationPartnerJob.class.getClassLoader().getResourceAsStream("cooperation-partner.sql"));
+        for (String sql : sqls.split(";")) {
+            if (StringUtils.isBlank(sql)) continue;
+            log.info("sql: {}", sql);
+            spark.sql(sql);
+        }
+    }
+}
