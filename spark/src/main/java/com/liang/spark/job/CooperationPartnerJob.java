04ad93c9a334fc1d09440a5eb5a46e51a5e47305
liang <li_ang_666@163.com>
2023-12-05 20:16:27 +0800

    1

--- spark/src/main/java/com/liang/spark/job/CooperationPartnerJob.java
+++ spark/src/main/java/com/liang/spark/job/CooperationPartnerJob.java
@@ -8,8 +8,27 @@ import org.apache.spark.sql.SparkSession;
 
 @Slf4j
 public class CooperationPartnerJob {
+    private final static String[] HUDI_TABLES = new String[]{
+            // 主要人员
+            "company_human_relation",
+            "company_bond_plates",
+            "senior_executive",
+            "personnel",
+            // 股东
+            "company_equity_relation_details",
+            // 法人
+            "company_legal_person",
+            // 维表
+            "company_index"
+    };
+
     public static void main(String[] args) throws Exception {
         SparkSession spark = SparkSessionFactory.createSpark(args);
+        for (String hudiTable : HUDI_TABLES) {
+            spark.read().format("hudi")
+                    .load("obs://hadoop-obs/hudi_ods/" + hudiTable)
+                    .createOrReplaceGlobalTempView(hudiTable);
+        }
         String sqls = ApolloUtils.get("cooperation-partner.sql");
         for (String sql : sqls.split(";")) {
             if (StringUtils.isBlank(sql)) continue;
