a72ac0a7618a3b4317fcf729b849158250fa6581
liang <li_ang_666@163.com>
2023-12-05 23:01:36 +0800

    1

--- spark/src/main/java/com/liang/spark/basic/SparkSessionFactory.java
+++ spark/src/main/java/com/liang/spark/basic/SparkSessionFactory.java
@@ -5,12 +5,30 @@ import com.liang.common.util.ConfigUtils;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
+
+import static org.apache.hudi.DataSourceReadOptions.QUERY_TYPE;
+import static org.apache.hudi.DataSourceReadOptions.QUERY_TYPE_READ_OPTIMIZED_OPT_VAL;
 
 @Slf4j
 @UtilityClass
 public class SparkSessionFactory {
+    public static SparkSession createSparkWithHudi(String[] args) {
+        SparkSession spark = createSpark(args);
+        for (Row row : spark.sql("show tables from hudi_ods").collect()) {
+            String tabName = row.getAs("tab_name");
+            String path = String.format("obs://hadoop-obs/hudi_ods/%s", tabName);
+            log.info("load hudi: {} -> {}", tabName, path);
+            spark.read().format("hudi")
+                    .option(QUERY_TYPE().key(), QUERY_TYPE_READ_OPTIMIZED_OPT_VAL())
+                    .load(path)
+                    .createOrReplaceTempView(tabName);
+        }
+        return spark;
+    }
+
     public static SparkSession createSpark(String[] args) {
         String file = (args != null && args.length > 0) ? args[0] : null;
         initConfig(file);
@@ -25,11 +43,11 @@ public class SparkSessionFactory {
     private static SparkSession initSpark() {
         SparkSession spark;
         try {
-            spark = ConfigSparkBuilder(SparkSession.builder())
+            spark = configSparkBuilder(SparkSession.builder())
                     .enableHiveSupport()
                     .getOrCreate();
         } catch (Exception e) {
-            spark = ConfigSparkBuilder(SparkSession.builder())
+            spark = configSparkBuilder(SparkSession.builder())
                     .master("local[*]")
                     .getOrCreate();
         }
@@ -37,7 +55,7 @@ public class SparkSessionFactory {
         return spark;
     }
 
-    private SparkSession.Builder ConfigSparkBuilder(SparkSession.Builder builder) {
+    private SparkSession.Builder configSparkBuilder(SparkSession.Builder builder) {
         return builder
                 .config("spark.debug.maxToStringFields", "256")
                 .config("spark.sql.extensions", "org.apache.spark.sql.hudi.HoodieSparkSessionExtension")
--- spark/src/main/java/com/liang/spark/job/CooperationPartnerJob.java
+++ spark/src/main/java/com/liang/spark/job/CooperationPartnerJob.java
@@ -6,40 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.sql.SparkSession;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.apache.hudi.DataSourceReadOptions.QUERY_TYPE;
-import static org.apache.hudi.DataSourceReadOptions.QUERY_TYPE_READ_OPTIMIZED_OPT_VAL;
-
 @Slf4j
 public class CooperationPartnerJob {
-    private final static List<String> HUDI_TABLES = new ArrayList<>();
-
-    static {
-        // 主要人员
-        HUDI_TABLES.add("company_human_relation");
-        HUDI_TABLES.add("company_bond_plates");
-        HUDI_TABLES.add("senior_executive");
-        HUDI_TABLES.add("personnel");
-        // 股东
-        HUDI_TABLES.add("company_equity_relation_details");
-        // 法人
-        HUDI_TABLES.add("company_legal_person");
-        // 维表
-        HUDI_TABLES.add("company_index");
-    }
-
     public static void main(String[] args) throws Exception {
-        SparkSession spark = SparkSessionFactory.createSpark(args);
-        for (String hudiTable : HUDI_TABLES) {
-            String path = String.format("obs://hadoop-obs/hudi_ods/%s", hudiTable);
-            log.info("load hudi: {} -> {}", hudiTable, path);
-            spark.read().format("hudi")
-                    .option(QUERY_TYPE().key(), QUERY_TYPE_READ_OPTIMIZED_OPT_VAL())
-                    .load(path)
-                    .createOrReplaceTempView(hudiTable);
-        }
+        SparkSession spark = SparkSessionFactory.createSparkWithHudi(args);
         String sqls = ApolloUtils.get("cooperation-partner.sql");
         for (String sql : sqls.split(";")) {
             if (StringUtils.isBlank(sql)) continue;
