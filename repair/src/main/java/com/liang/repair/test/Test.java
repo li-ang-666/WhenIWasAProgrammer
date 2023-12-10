134edc1a79982cf91b64785e288cc5068bccd18b
liang <li_ang_666@163.com>
2023-12-10 12:20:03 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -3,8 +3,22 @@ package com.liang.repair.test;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.stream.Stream;
+
 @Slf4j
 public class Test extends ConfigHolder {
     public static void main(String[] args) {
+        System.out.println(Stream.of(
+                75356550,
+                75220849,
+                75276705,
+                75308659,
+                75315607,
+                75281967,
+                75289875,
+                75277226,
+                75288175,
+                75337035
+        ).mapToLong(e -> e).sum());
     }
 }
