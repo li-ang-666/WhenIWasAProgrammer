b1f547449889d7828fc42f196bec2e15d9debd74
liang <li_ang_666@163.com>
2023-12-12 12:52:47 +0800

    1

--- flink/src/main/java/com/liang/flink/job/RatioPathCompanyRepair.java
+++ flink/src/main/java/com/liang/flink/job/RatioPathCompanyRepair.java
@@ -1,8 +1,10 @@
 package com.liang.flink.job;
 
 import com.liang.common.dto.Config;
+import com.liang.common.service.SQL;
 import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.common.util.ConfigUtils;
+import com.liang.common.util.SqlUtils;
 import com.liang.flink.project.ratio.path.company.RatioPathCompanyService;
 import org.apache.commons.lang3.StringUtils;
 
@@ -24,17 +26,17 @@ public class RatioPathCompanyRepair {
             if (StringUtils.isNumeric(e)) {
                 res.add(Long.parseLong(e));
             }
-            //String sql = new SQL().SELECT("distinct company_id")
-            //        .FROM("ratio_path_company")
-            //        .WHERE("shareholder_id = " + SqlUtils.formatValue(e))
-            //        .toString();
-            //jdbcTemplate.queryForList(sql, rs -> {
-            //    String companyId = rs.getString(1);
-            //    if (StringUtils.isNumeric(companyId)) {
-            //        res.add(Long.parseLong(companyId));
-            //    }
-            //    return null;
-            //});
+            String sql = new SQL().SELECT("distinct company_id")
+                    .FROM("ratio_path_company")
+                    .WHERE("shareholder_id = " + SqlUtils.formatValue(e))
+                    .toString();
+            jdbcTemplate.queryForList(sql, rs -> {
+                String companyId = rs.getString(1);
+                if (StringUtils.isNumeric(companyId)) {
+                    res.add(Long.parseLong(companyId));
+                }
+                return null;
+            });
             return res.stream();
         }).collect(Collectors.toSet());
 
