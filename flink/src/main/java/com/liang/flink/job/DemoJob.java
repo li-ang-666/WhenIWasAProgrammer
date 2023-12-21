c8143aa8bf98887b24fe673e33a2393956eb9cfc
liang <li_ang_666@163.com>
2023-12-21 10:40:35 +0800

    1

--- flink/src/main/java/com/liang/flink/job/DemoJob.java
+++ flink/src/main/java/com/liang/flink/job/DemoJob.java
@@ -1,7 +1,7 @@
 package com.liang.flink.job;
 
 import com.liang.common.dto.Config;
-import com.liang.common.service.DemoTemplate;
+import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.common.util.ConfigUtils;
 import com.liang.flink.basic.EnvironmentFactory;
 import com.liang.flink.basic.LocalConfigFile;
@@ -19,34 +19,24 @@ public class DemoJob {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = EnvironmentFactory.create(args);
         Config config = ConfigUtils.getConfig();
-        StreamFactory.create(env)
-                .rebalance()
-                .addSink(new DemoSink(config)).name("DemoSink").setParallelism(config.getFlinkConfig().getOtherParallel());
+        StreamFactory.create(env).rebalance().addSink(new DemoSink(config)).name("DemoSink").setParallelism(config.getFlinkConfig().getOtherParallel());
         env.execute("DemoJob");
     }
 
     @RequiredArgsConstructor
     private final static class DemoSink extends RichSinkFunction<SingleCanalBinlog> {
         private final Config config;
-        private DemoTemplate demoTemplate;
+        private JdbcTemplate jdbcTemplate;
 
         @Override
         public void open(Configuration parameters) {
             ConfigUtils.setConfig(config);
-            demoTemplate = new DemoTemplate();
-            demoTemplate.enableCache();
+            jdbcTemplate = new JdbcTemplate("427.test");
+            jdbcTemplate.enableCache();
         }
 
         @Override
         public void invoke(SingleCanalBinlog singleCanalBinlog, Context context) {
-            //Map<String, Object> beforeColumnMap = singleCanalBinlog.getBeforeColumnMap();
-            //Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
-            //String id = String.valueOf(columnMap.get("id"));
-            //if (id.equals("2476911709")) {
-            //    Object bef = beforeColumnMap.get("social_security_staff_num");
-            //    Object after = columnMap.get("social_security_staff_num");
-            //    log.info("id: {}, bef: {}, aft: {}", id, bef, after);
-            //}
         }
     }
 }
