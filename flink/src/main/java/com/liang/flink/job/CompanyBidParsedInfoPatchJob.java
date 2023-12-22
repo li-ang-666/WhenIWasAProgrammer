59b6bea833c86732035abc47f177ae21590941e4
liang <li_ang_666@163.com>
2023-12-22 14:54:50 +0800

    1

--- flink/src/main/java/com/liang/flink/job/CompanyBidParsedInfoPatchJob.java
+++ flink/src/main/java/com/liang/flink/job/CompanyBidParsedInfoPatchJob.java
@@ -64,9 +64,7 @@ public class CompanyBidParsedInfoPatchJob {
             String uuid = String.valueOf(columnMap.get("bid_uuid"));
             List<Map<String, Object>> queryResult = service.query(uuid);
             List<Map<String, Object>> result = !queryResult.isEmpty() ? queryResult : service.post(content, uuid);
-            if (log.isDebugEnabled()) {
-                log.debug("AI return: {} -> {}", uuid, JsonUtils.toString(result));
-            }
+            if (log.isDebugEnabled()) log.debug("AI return: {} -> {}", uuid, JsonUtils.toString(result));
             // owner 招标方
             String sourceOwner = String.valueOf(columnMap.get("purchaser"));
             List<Map<String, Object>> newOwner = service.newJson(sourceOwner);
