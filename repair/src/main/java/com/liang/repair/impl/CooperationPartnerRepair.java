12ce101dd65ae02b0f1c05c5f1ccb331aefd8f58
liang <li_ang_666@163.com>
2023-12-22 10:30:59 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/CooperationPartnerRepair.java
+++ repair/src/main/java/com/liang/repair/impl/CooperationPartnerRepair.java
@@ -11,8 +11,9 @@ import java.util.zip.CRC32;
 @Slf4j
 public class CooperationPartnerRepair extends ConfigHolder {
     public static void main(String[] args) {
-        String humanGraphId = "1774556338";
-        String companyGraphId = "2341362906";
+        //2349866589-c2350626174
+        String humanGraphId = "2349866589";
+        String companyGraphId = "2350626174";
         String queryHumanPidSql = String.format("select human_pid from company_human_relation where human_graph_id = %s and company_graph_id = %s",
                 SqlUtils.formatValue(humanGraphId), SqlUtils.formatValue(companyGraphId));
         String humanPid = new JdbcTemplate("157.prism_boss").queryForObject(queryHumanPidSql, rs -> rs.getString(1));
