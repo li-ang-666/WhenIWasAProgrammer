fcde87bb735f07e5bc54773d1dbeeb099720b789
liang <li_ang_666@163.com>
2023-12-26 14:17:13 +0800

    1

--- hudi/src/main/resources/sh/bulk_insert.sh
+++ hudi/src/main/resources/sh/bulk_insert.sh
@@ -6,7 +6,7 @@ export HADOOP_CLASSPATH=`hadoop classpath`
   -D jobmanager.memory.process.size=2g \
   -D taskmanager.memory.process.size=6g \
   -D taskmanager.numberOfTaskSlots=8 \
-  -D parallelism.default=16 \
+  -D parallelism.default=32 \
   -D state.checkpoints.dir=hdfs:///hudi/flink-checkpoints/bulk-insert/${2} \
   -D yarn.application.name=hudi_bulk_insert_${2} \
   -c com.liang.hudi.job.HudiJob hudi-1.0.jar BULK_INSERT ${1} ${2} "${3}"
