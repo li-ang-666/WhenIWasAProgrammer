c36a994b30af80a5055031d6b7b10a836e38ff2e
liang <li_ang_666@163.com>
2023-12-28 10:27:33 +0800

    1

--- flink/src/main/java/com/liang/flink/job/RatioPathCompanyRepair.java
+++ flink/src/main/java/com/liang/flink/job/RatioPathCompanyRepair.java
@@ -19,7 +19,7 @@ public class RatioPathCompanyRepair {
         ConfigUtils.setConfig(config);
         JdbcTemplate jdbcTemplate = new JdbcTemplate("457.prism_shareholder_path");
         Set<String> companyIds = new HashSet<>(Arrays.asList(
-                "404065450"
+                "5824576548"
         ));
         Set<Long> allCompanyIds = companyIds.stream().flatMap(e -> {
             HashSet<Long> res = new HashSet<>();
