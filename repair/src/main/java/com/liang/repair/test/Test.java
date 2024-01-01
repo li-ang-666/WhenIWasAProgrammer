ef6f44c7e9122af01d5d53fba23a7244c20cade0
liang <li_ang_666@163.com>
2024-01-01 17:49:06 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -6,6 +6,5 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Test extends ConfigHolder {
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
     }
 }
