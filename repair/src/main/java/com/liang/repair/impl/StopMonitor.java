0f721db22ff78c8165590f7882498bf3d0c1e1fa
liang <li_ang_666@163.com>
2024-01-02 15:07:07 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/StopMonitor.java
+++ repair/src/main/java/com/liang/repair/impl/StopMonitor.java
@@ -4,39 +4,29 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 import com.liang.common.util.JsonUtils;
+import org.apache.flink.api.java.tuple.Tuple3;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class StopMonitor {
     private final static String URL = "http://10.99.205.87:8990/flink/cancelMonitor";
-    private final static Map<String, Object> TEMPLATE_MAP = new HashMap<>();
+    private final static List<Tuple3<String, String, String>> LIST = new ArrayList<>();
 
     static {
-        TEMPLATE_MAP.put("sshPassWord", "Moka20190520");
-        TEMPLATE_MAP.put("isMonitored", 0);
+        LIST.add(Tuple3.of("liang", "Moka20190520", "CompanyBidParsedInfoPatchJob"));
     }
 
     public static void main(String[] args) {
         HttpRequest post = HttpUtil.createPost(URL);
-        String user = "omm";
-        List<String> jobs = Arrays.asList(
-                "hudi_upsert_company_clean_info",
-                "hudi_upsert_company_index",
-                "hudi_upsert_company_legal_person",
-                "hudi_upsert_company_equity_relation_details",
-                "hudi_upsert_personnel",
-                "hudi_upsert_senior_executive_hk",
-                "hudi_upsert_senior_executive",
-                "hudi_upsert_company_bond_plates",
-                "hudi_upsert_company_human_relation"
-        );
-        jobs.forEach(job -> {
-            HashMap<String, Object> map = new HashMap<>(TEMPLATE_MAP);
-            map.put("sshUserName", user);
-            map.put("yarnName", job);
+        LIST.forEach(info -> {
+            Map<String, Object> map = new HashMap<>();
+            map.put("sshUserName", info.f0);
+            map.put("sshPassWord", info.f1);
+            map.put("yarnName", info.f2);
+            map.put("isMonitored", 0);
             HttpResponse response = post.body(JsonUtils.toString(map), "application/json").execute();
             System.out.println(response.body());
         });
