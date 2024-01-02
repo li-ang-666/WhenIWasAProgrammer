97da7c5264523d2a4e42ea8796339907144f9c91
liang <li_ang_666@163.com>
2024-01-02 21:52:40 +0800

    1

--- hudi/src/main/resources/sh/clean.sh
+++ hudi/src/main/resources/sh/clean.sh
@@ -2,8 +2,8 @@
 export num=`yarn app -list | grep hudi_ | wc -l`
 if [[ ${num} == 0* ]] ; then
   echo 'no hudi job running, clean hdfs'
-  hdfs dfs -rm -r /user/hive/.flink/*
-  hdfs dfs -rm -r /user/hive/.Trash/Current/user/hive/.flink/*
 else
-  echo 'find running hudi job, return'
+  echo 'find running hudi job, return' && exit 0
 fi
+hdfs dfs -rm -r /user/hive/.flink/*
+hdfs dfs -rm -r /user/hive/.Trash/Current/user/hive/.flink/*
