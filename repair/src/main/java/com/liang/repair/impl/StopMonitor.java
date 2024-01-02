b53e271e5957f85d96ae6e115202b46c7f17684d
liang <li_ang_666@163.com>
2024-01-02 11:15:37 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/StopMonitor.java
+++ repair/src/main/java/com/liang/repair/impl/StopMonitor.java
@@ -11,18 +11,27 @@ import java.util.List;
 import java.util.Map;
 
 public class StopMonitor {
+    private final static String URL = "http://10.99.205.87:8990/flink/cancelMonitor";
     private final static Map<String, Object> TEMPLATE_MAP = new HashMap<>();
 
     static {
-        TEMPLATE_MAP.put("sshPassWord", "flink_tyc");
+        TEMPLATE_MAP.put("sshPassWord", "Moka20190520");
         TEMPLATE_MAP.put("isMonitored", 0);
     }
 
     public static void main(String[] args) {
-        HttpRequest post = HttpUtil.createPost("http://10.99.205.87:8990/flink/cancelMonitor");
-        String user = "liguangwei";
+        HttpRequest post = HttpUtil.createPost(URL);
+        String user = "omm";
         List<String> jobs = Arrays.asList(
-                "company_equity_relation_details_prod"
+                "hudi_upsert_company_clean_info",
+                "hudi_upsert_company_index",
+                "hudi_upsert_company_legal_person",
+                "hudi_upsert_company_equity_relation_details",
+                "hudi_upsert_personnel",
+                "hudi_upsert_senior_executive_hk",
+                "hudi_upsert_senior_executive",
+                "hudi_upsert_company_bond_plates",
+                "hudi_upsert_company_human_relation"
         );
         jobs.forEach(job -> {
             HashMap<String, Object> map = new HashMap<>(TEMPLATE_MAP);
