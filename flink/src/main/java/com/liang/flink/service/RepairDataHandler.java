f1763b1b5092a942e5127d8537e2bd103d9641e8
liang <li_ang_666@163.com>
2024-01-04 18:17:56 +0800

    1

--- flink/src/main/java/com/liang/flink/service/RepairDataHandler.java
+++ flink/src/main/java/com/liang/flink/service/RepairDataHandler.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -20,7 +21,7 @@ import static com.liang.common.dto.config.RepairTask.ScanMode.TumblingWindow;
 
 @Slf4j
 @RequiredArgsConstructor
-public class RepairDataHandler implements Runnable {
+public class RepairDataHandler implements Runnable, Iterator<List<Map<String, Object>>> {
     private final static int QUERY_BATCH_SIZE = 1024;
     private final static int MAX_QUEUE_SIZE = 10240;
     private final static int WRITE_REDIS_INTERVAL_MILLISECONDS = 1000 * 5;
@@ -41,9 +42,9 @@ public class RepairDataHandler implements Runnable {
         jdbcTemplate = new JdbcTemplate(task.getSourceName());
         redisTemplate = new RedisTemplate("metadata");
         ConcurrentLinkedQueue<SingleCanalBinlog> queue = task.getPendingQueue();
-        while (hasNextBatch() && running.get()) {
+        while (hasNext() && running.get()) {
             if ((queue.size() + QUERY_BATCH_SIZE) > MAX_QUEUE_SIZE) continue;
-            List<Map<String, Object>> columnMaps = nextBatch();
+            List<Map<String, Object>> columnMaps = next();
             synchronized (repairKey) {
                 for (Map<String, Object> columnMap : columnMaps) {
                     queue.offer(new SingleCanalBinlog(task.getSourceName(), task.getTableName(), -1L, CanalEntry.EventType.INSERT, columnMap, new HashMap<>(), columnMap));
@@ -57,11 +58,13 @@ public class RepairDataHandler implements Runnable {
     /**
      * `currentId` is unprocessed
      */
-    private boolean hasNextBatch() {
+    @Override
+    public boolean hasNext() {
         return task.getCurrentId() <= task.getTargetId();
     }
 
-    private List<Map<String, Object>> nextBatch() {
+    @Override
+    public List<Map<String, Object>> next() {
         StringBuilder sqlBuilder = new StringBuilder(baseSql);
         if (task.getScanMode() == TumblingWindow) {
             sqlBuilder.append(String.format(" and %s <= id and id < %s", task.getCurrentId(), task.getCurrentId() + QUERY_BATCH_SIZE));
@@ -71,10 +74,6 @@ public class RepairDataHandler implements Runnable {
 
     private void commit() {
         task.setCurrentId(task.getScanMode() == Direct ? 1 : task.getCurrentId() + QUERY_BATCH_SIZE);
-        report();
-    }
-
-    private void report() {
         long currentTimeMillis = System.currentTimeMillis();
         if (currentTimeMillis - lastWriteTimeMillis >= WRITE_REDIS_INTERVAL_MILLISECONDS) {
             long currentId = task.getCurrentId();
