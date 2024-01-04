b2cd83cfa3edf20e4d55120c8e2320805fa3122b
liang <li_ang_666@163.com>
2024-01-04 19:19:03 +0800

    1

--- repair/src/main/java/com/liang/repair/test/Test.java
+++ repair/src/main/java/com/liang/repair/test/Test.java
@@ -3,14 +3,8 @@ package com.liang.repair.test;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-
 @Slf4j
 public class Test extends ConfigHolder {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<String, String>() {{
-            //put("key", "abc");
-        }};
-        System.out.println(map.values().stream().anyMatch(e -> !e.equals("aaa")));
     }
 }
