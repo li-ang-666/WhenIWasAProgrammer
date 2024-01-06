36098046ea82b5fd75acb994a0ae136d2fafc593
liang <li_ang_666@163.com>
2024-01-06 16:09:25 +0800

    1

--- flink/src/main/java/com/liang/flink/basic/kafka/KafkaLagReporter.java
+++ flink/src/main/java/com/liang/flink/basic/kafka/KafkaLagReporter.java
@@ -20,7 +20,7 @@ import java.util.concurrent.locks.LockSupport;
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaLagReporter implements Runnable {
-    private static final int READ_REDIS_INTERVAL_MILLISECONDS = 1000 * 60;
+    private static final int READ_REDIS_INTERVAL_MILLISECONDS = 1000 * 3;
     private static final String SEPARATOR = "\u0001";
     private static final Comparator<TopicPartition> TOPIC_PARTITION_COMPARATOR = (e1, e2) -> e1.topic().equals(e2.topic()) ? e1.partition() - e2.partition() : e1.topic().compareTo(e2.topic());
     private final RedisTemplate redisTemplate = new RedisTemplate("metadata");
@@ -32,44 +32,58 @@ public class KafkaLagReporter implements Runnable {
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, ConfigUtils.getConfig().getKafkaConfigs().get("kafkaSource").getBootstrapServers());
         KafkaConsumer<byte[], byte[]> kafkaConsumer = new KafkaConsumer<>(properties, new ByteArrayDeserializer(), new ByteArrayDeserializer());
+        String lastReadOffsetLag = "{}";
+        String lastReadTimeInfo = "{}";
         while (true) {
             LockSupport.parkUntil(System.currentTimeMillis() + READ_REDIS_INTERVAL_MILLISECONDS);
-            Map<String, String> offsetMap = redisTemplate.hScan(kafkaOffsetKey);
-            // 格式化
-            Map<TopicPartition, Long> copyOffsetMap = new TreeMap<>(TOPIC_PARTITION_COMPARATOR);
-            for (Map.Entry<String, String> entry : offsetMap.entrySet()) {
-                String key = entry.getKey();
-                String value = entry.getValue();
-                String[] split = key.split(SEPARATOR);
-                copyOffsetMap.put(new TopicPartition(split[0], Integer.parseInt(split[1])), Long.parseLong(value));
-            }
-            // 获取最大值
-            Map<TopicPartition, Long> maxOffsetMap = kafkaConsumer.endOffsets(copyOffsetMap.keySet());
-            int i = 0;
-            // 计算
-            for (Map.Entry<TopicPartition, Long> entry : maxOffsetMap.entrySet()) {
-                TopicPartition key = entry.getKey();
-                long lag = entry.getValue() - copyOffsetMap.get(key);
-                copyOffsetMap.put(key, lag);
-                if (lag > 100L) i++;
-            }
-            if (i == 0) {
-                log.info("本轮周期内 kafka 所有分区 lag 均小于 100");
+            Map<String, String> offsetMap = new TreeMap<>(redisTemplate.hScan(kafkaOffsetKey));
+            Map<String, String> timeMap = new TreeMap<>(redisTemplate.hScan(kafkaTimeKey));
+            String currentReadOffsetLag = offsetMap.toString();
+            String currentReadTimeInfo = timeMap.toString();
+            if (currentReadOffsetLag.equals(lastReadOffsetLag) && currentReadTimeInfo.equals(lastReadTimeInfo))
                 continue;
+            lastReadOffsetLag = currentReadOffsetLag;
+            lastReadTimeInfo = currentReadTimeInfo;
+            // kafka offset lag
+            {
+                // 格式化
+                Map<TopicPartition, Long> copyOffsetMap = new TreeMap<>(TOPIC_PARTITION_COMPARATOR);
+                for (Map.Entry<String, String> entry : offsetMap.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    String[] split = key.split(SEPARATOR);
+                    copyOffsetMap.put(new TopicPartition(split[0], Integer.parseInt(split[1])), Long.parseLong(value));
+                }
+                // 获取最大值
+                Map<TopicPartition, Long> maxOffsetMap = kafkaConsumer.endOffsets(copyOffsetMap.keySet());
+                int i = 0;
+                // 计算
+                for (Map.Entry<TopicPartition, Long> entry : maxOffsetMap.entrySet()) {
+                    TopicPartition key = entry.getKey();
+                    long lag = entry.getValue() - copyOffsetMap.get(key);
+                    copyOffsetMap.put(key, lag);
+                    if (lag > 100L) i++;
+                }
+                if (i == 0) {
+                    log.info("本轮周期内 kafka 所有分区 lag 均小于 100");
+                    continue;
+                }
+                log.warn("kafka offset lag: {}", JsonUtils.toString(copyOffsetMap));
             }
-            log.warn("kafka offset lag: {}", JsonUtils.toString(copyOffsetMap));
-            Map<String, String> timeMap = redisTemplate.hScan(kafkaTimeKey);
-            // 格式化 and 计算
-            Map<TopicPartition, String> copyTimeMap = new TreeMap<>(TOPIC_PARTITION_COMPARATOR);
-            for (Map.Entry<String, String> entry : timeMap.entrySet()) {
-                String key = entry.getKey();
-                String value = entry.getValue();
-                String[] split = key.split(SEPARATOR);
-                TopicPartition topicPartition = new TopicPartition(split[0], Integer.parseInt(split[1]));
-                String time = DateTimeUtils.fromUnixTime(Long.parseLong(value), "yyyy-MM-dd HH:mm:ss");
-                copyTimeMap.put(topicPartition, time);
+            // kafka time info
+            {
+                // 格式化 and 计算
+                Map<TopicPartition, String> copyTimeMap = new TreeMap<>(TOPIC_PARTITION_COMPARATOR);
+                for (Map.Entry<String, String> entry : timeMap.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    String[] split = key.split(SEPARATOR);
+                    TopicPartition topicPartition = new TopicPartition(split[0], Integer.parseInt(split[1]));
+                    String time = DateTimeUtils.fromUnixTime(Long.parseLong(value), "yyyy-MM-dd HH:mm:ss");
+                    copyTimeMap.put(topicPartition, time);
+                }
+                log.warn("kafka time info: {}", JsonUtils.toString(copyTimeMap));
             }
-            log.warn("kafka time info: {}", JsonUtils.toString(copyTimeMap));
         }
     }
 }
--- flink/src/main/java/com/liang/flink/basic/kafka/KafkaMonitor.java
+++ flink/src/main/java/com/liang/flink/basic/kafka/KafkaMonitor.java
@@ -10,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.util.Collector;
 
 import java.util.HashMap;
@@ -17,16 +20,18 @@ import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
-public class KafkaMonitor extends RichFlatMapFunction<KafkaRecord<BatchCanalBinlog>, SingleCanalBinlog> {
-    private static final int WRITE_REDIS_INTERVAL_MILLISECONDS = 1000 * 5;
+public class KafkaMonitor extends RichFlatMapFunction<KafkaRecord<BatchCanalBinlog>, SingleCanalBinlog> implements CheckpointedFunction {
     private static final String SEPARATOR = "\001";
     private final Map<String, String> offsetMap = new HashMap<>();
     private final Map<String, String> timeMap = new HashMap<>();
     private final Config config;
     private final String kafkaOffsetKey;
     private final String kafkaTimeKey;
-    private long lastWriteTimeMillis = System.currentTimeMillis();
     private RedisTemplate redisTemplate;
+
+    @Override
+    public void initializeState(FunctionInitializationContext context) {
+    }
 
     @Override
     public void open(Configuration parameters) {
@@ -39,13 +44,13 @@ public class KafkaMonitor extends RichFlatMapFunction<KafkaRecord<BatchCanalBinl
         String key = kafkaRecord.getTopic() + SEPARATOR + kafkaRecord.getPartition();
         offsetMap.put(key, String.valueOf(kafkaRecord.getOffset()));
         timeMap.put(key, String.valueOf(kafkaRecord.getReachMilliseconds() / 1000));
-        long currentTimeMillis = System.currentTimeMillis();
-        if (currentTimeMillis - lastWriteTimeMillis >= WRITE_REDIS_INTERVAL_MILLISECONDS) {
-            redisTemplate.hMSet(kafkaOffsetKey, offsetMap);
-            redisTemplate.hMSet(kafkaTimeKey, timeMap);
-            lastWriteTimeMillis = currentTimeMillis;
-        }
         kafkaRecord.getValue().getSingleCanalBinlogs().forEach(out::collect);
+    }
+
+    @Override
+    public void snapshotState(FunctionSnapshotContext context) {
+        redisTemplate.hMSet(kafkaOffsetKey, offsetMap);
+        redisTemplate.hMSet(kafkaTimeKey, timeMap);
     }
 
     @Override
