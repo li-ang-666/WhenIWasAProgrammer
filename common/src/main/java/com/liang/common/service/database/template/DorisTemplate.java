fd1bcba1f39959de7b5c2921dbe82f226ef9b611
liang <li_ang_666@163.com>
2024-01-12 17:01:01 +0800

    1

--- common/src/main/java/com/liang/common/service/database/template/DorisTemplate.java
+++ common/src/main/java/com/liang/common/service/database/template/DorisTemplate.java
@@ -111,6 +111,7 @@ public class DorisTemplate extends AbstractCache<DorisSchema, DorisOneRow> {
         put.setHeader("strip_outer_array", "true");
         put.setHeader("num_as_string", "true");
         put.setHeader("send_batch_parallelism", "1");
+        put.setHeader("two_phase_commit", "false");
         // for unique delete
         if (schema.getUniqueDeleteOn() != null) {
             put.setHeader("merge_type", "MERGE");
