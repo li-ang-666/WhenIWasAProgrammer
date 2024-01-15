53ad5a721526271a39009c5fc34f1ed05515aeb2
liang <li_ang_666@163.com>
2024-01-15 20:30:09 +0800

    1

--- spark/src/main/java/com/liang/spark/job/DorisJob.java
+++ spark/src/main/java/com/liang/spark/job/DorisJob.java
@@ -44,7 +44,7 @@ public class DorisJob {
     @Slf4j
     @RequiredArgsConstructor
     public final static class DorisSink implements ForeachPartitionFunction<Row> {
-        private static final int BATCH_SIZE = 512000;
+        private static final int BATCH_SIZE = 1_048_576;
         private final List<DorisOneRow> rows = new ArrayList<>(BATCH_SIZE);
         private final Config config;
         private final String database;
