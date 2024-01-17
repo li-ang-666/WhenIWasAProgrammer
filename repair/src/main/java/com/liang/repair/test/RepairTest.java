cf6a32510e27a1bf3cd150e60005deefcc68cb9a
liang <li_ang_666@163.com>
2024-01-17 16:24:53 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -1,10 +1,36 @@
 package com.liang.repair.test;
 
+import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.LockSupport;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
+        String source = "dwd.dwd_user_register_details";
+        String sink = "bak." + source.split("\\.")[1];
+        JdbcTemplate doris = new JdbcTemplate("doris");
+        // drop sink
+        String dropSql = String.format("drop table if exists %s force", sink);
+        doris.update(dropSql);
+        // write sink
+        String descSql = "desc " + source;
+        String createSql = doris.queryForList(descSql, rs -> rs.getString(1))
+                .stream()
+                .map(e -> String.format("cast(%s as string) %s", e, e))
+                .collect(Collectors.joining(", ", String.format("create table %s as select ", sink), String.format(" from %s", source)));
+        doris.update(createSql);
+        // check
+        String sourceCount = doris.queryForObject("select count(1) from " + source, rs -> rs.getString(1));
+        String sinkCount = doris.queryForObject("select count(1) from " + sink, rs -> rs.getString(1));
+        while (Math.abs(Long.parseLong(sourceCount) - Long.parseLong(sinkCount)) > 100_000) {
+            log.warn("sourceCnt {} not like sinkCnt {}", sourceCount, sinkCount);
+            LockSupport.parkUntil(System.currentTimeMillis() + 1000 * 5);
+            sourceCount = doris.queryForObject("select count(1) from " + source, rs -> rs.getString(1));
+            sinkCount = doris.queryForObject("select count(1) from " + sink, rs -> rs.getString(1));
+        }
     }
 }
--- spark/src/main/java/com/liang/spark/job/DorisCopyJob.java
+++ spark/src/main/java/com/liang/spark/job/DorisCopyJob.java
@@ -1,42 +1,16 @@
 package com.liang.spark.job;
 
-import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.common.util.ConfigUtils;
 import com.liang.spark.basic.SparkSessionFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.SparkSession;
 
-import java.util.Arrays;
-import java.util.concurrent.locks.LockSupport;
-import java.util.stream.Collectors;
-
 @Slf4j
 public class DorisCopyJob {
     public static void main(String[] args) {
-        log.info("args: {}", Arrays.toString(args));
         SparkSession spark = SparkSessionFactory.createSpark(null);
         String source = args[0];
-        log.info("source: {}", source);
         String sink = "bak." + source.split("\\.")[1];
-        log.info("sink: {}", sink);
-        JdbcTemplate doris = new JdbcTemplate("doris");
-        // drop sink
-        String dropSql = String.format("drop table if exists %s force", sink);
-        doris.update(dropSql);
-        // write sink
-        String descSql = "desc " + source;
-        String createSql = doris.queryForList(descSql, rs -> rs.getString(1))
-                .stream()
-                .map(e -> String.format("cast(%s as string) %s", e, e))
-                .collect(Collectors.joining(", ", String.format("create table %s as select ", sink), String.format(" from %s", source)));
-        doris.update(createSql);
-        // check
-        String sourceCount = doris.queryForObject("select count(1) from " + source, rs -> rs.getString(1));
-        String sinkCount = doris.queryForObject("select count(1) from " + sink, rs -> rs.getString(1));
-        while (Math.abs(Long.parseLong(sourceCount) - Long.parseLong(sinkCount)) > 100_000) {
-            log.warn("sourceCnt {} not like sinkCnt {}", sourceCount, sinkCount);
-            LockSupport.parkUntil(System.currentTimeMillis() + 1000 * 5);
-        }
         // spark
         spark.read().format("doris")
                 .option("doris.fenodes", "10.99.197.34:8030,10.99.202.71:8030,10.99.203.88:8030")
