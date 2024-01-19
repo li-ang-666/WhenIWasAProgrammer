0a2124ebb183a90890c9c75bd7ed0d2c0bd94b54
liang <li_ang_666@163.com>
2024-01-19 22:55:41 +0800

    1

--- spark/src/main/java/com/liang/spark/job/CompanyNumControlAbilityJob.java
+++ spark/src/main/java/com/liang/spark/job/CompanyNumControlAbilityJob.java
@@ -17,17 +17,18 @@ import org.apache.spark.sql.SparkSession;
 import java.util.Iterator;
 import java.util.Map;
 
+@Slf4j
 public class CompanyNumControlAbilityJob {
     public static void main(String[] args) {
         SparkSession spark = SparkSessionFactory.createSpark(args);
         TableFactory.jdbc(spark, "457.prism_shareholder_path", "ratio_path_company")
-                //.where("investment_ratio_total >= 0.05")
+                .where("investment_ratio_total >= 0.05")
                 .where("shareholder_entity_type = 1")
-                //.where("shareholder_id is not null and shareholder_id <> '' and shareholder_id <> 0")
+                .where("shareholder_id is not null and shareholder_id <> '' and shareholder_id <> '0'")
                 .createOrReplaceTempView("ttt");
-        spark.sql("select shareholder_id, count(1) cnt from ttt group by shareholder_id")
-                .repartition(256)
-                .foreachPartition(new CompanyNumControlAbilitySink(ConfigUtils.getConfig()));
+        for (Row row : spark.sql("select count(distinct shareholder_id) count_distinct_shareholder_id from ttt").collectAsList()) {
+            log.info("json: {}", row.json());
+        }
     }
 
     @Slf4j
