f82b9b28c227bab3cd7268e7f0462bbe4fc39212
liang <li_ang_666@163.com>
2024-01-20 16:40:07 +0800

    1

--- hive-udf/src/main/resources/readme.sql
+++ hive-udf/src/main/resources/readme.sql
@@ -5,36 +5,24 @@
 add jar hdfs:///liang/hive-udf-1.0.jar;
 
 -- 创建UDAF函数
-drop temporary function if exists to_bitmap;
-create temporary function to_bitmap as 'com.liang.udf.ToBitmapUDAF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
-drop temporary function if exists bitmap_union;
-create temporary function bitmap_union as 'com.liang.udf.BitmapUnionUDAF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
+drop function if exists doris.to_bitmap;
+create function doris.to_bitmap as 'com.liang.udf.ToBitmapUDAF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
+drop function if exists doris.bitmap_union;
+create function doris.bitmap_union as 'com.liang.udf.BitmapUnionUDAF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
 
 -- 创建UDF函数
-drop temporary function if exists bitmap_count;
-create temporary function bitmap_count as 'com.liang.udf.BitmapCountUDF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
-drop temporary function if exists bitmap_and;
-create temporary function bitmap_and as 'com.liang.udf.BitmapAndUDF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
-drop temporary function if exists bitmap_or;
-create temporary function bitmap_or as 'com.liang.udf.BitmapOrUDF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
-drop temporary function if exists bitmap_xor;
-create temporary function bitmap_xor as 'com.liang.udf.BitmapXorUDF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
+drop function if exists doris.bitmap_count;
+create function doris.bitmap_count as 'com.liang.udf.BitmapCountUDF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
+drop function if exists doris.bitmap_and;
+create function doris.bitmap_and as 'com.liang.udf.BitmapAndUDF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
+drop function if exists doris.bitmap_or;
+create function doris.bitmap_or as 'com.liang.udf.BitmapOrUDF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
+drop function if exists doris.bitmap_xor;
+create function doris.bitmap_xor as 'com.liang.udf.BitmapXorUDF' USING JAR 'hdfs:///liang/hive-udf-1.0.jar';
 
 -- 测试
 show functions;
-desc function extended to_bitmap;
-
--- 测试
-drop table if exists test.bitmap_test;
-create table if not exists test.bitmap_test(id int, tb string, bitmap binary);
-with t as(
-  select 1 id, 'company_bond_plates' tb, to_bitmap(id) bitmap from hudi_ods.company_bond_plates
-  union all
-  select 2 id, 'senior_executive' tb, to_bitmap(id) bitmap from hudi_ods.senior_executive
-  union all
-  select 3 id, 'senior_executive_hk' tb, to_bitmap(id) bitmap from hudi_ods.senior_executive_hk
-)insert overwrite table test.bitmap_test select * from t;
-select id, tb, bitmap_count(bitmap) from test.bitmap_test;
+desc function extended doris.to_bitmap;
 
 ------------------------------------------------------------------------------------------------------------------------
 
