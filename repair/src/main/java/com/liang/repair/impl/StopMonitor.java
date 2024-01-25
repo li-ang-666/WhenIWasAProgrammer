d7bbf68d95eab63c04057d8c9a9049ce7cc76e49
liang <li_ang_666@163.com>
2024-01-25 16:31:26 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/StopMonitor.java
+++ repair/src/main/java/com/liang/repair/impl/StopMonitor.java
@@ -16,7 +16,7 @@ public class StopMonitor {
     private final static List<Tuple3<String, String, String>> LIST = new ArrayList<>();
 
     static {
-        LIST.add(Tuple3.of("liang", "Moka20190520", "doris.dwd_app_active"));
+        LIST.add(Tuple3.of("liang", "Moka20190520", "doris.crowd_user_bitmap"));
     }
 
     public static void main(String[] args) {
