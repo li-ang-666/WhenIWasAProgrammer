2f54d48c370b1bbb0b0252b0bcdf546d2ee1b94e
liang <li_ang_666@163.com>
2024-01-31 17:31:45 +0800

    1

--- flink/src/main/java/com/liang/flink/service/equity/bfs/dto/mysql/RatioPathCompanyDto.java
+++ flink/src/main/java/com/liang/flink/service/equity/bfs/dto/mysql/RatioPathCompanyDto.java
@@ -2,6 +2,7 @@ package com.liang.flink.service.equity.bfs.dto.mysql;
 
 import com.liang.flink.service.equity.bfs.dto.pojo.Path;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -10,11 +11,12 @@ import java.util.List;
 import static java.math.BigDecimal.ZERO;
 
 @Data
+@RequiredArgsConstructor
 public class RatioPathCompanyDto {
-    private final List<Path> paths = new ArrayList<>();
     private final String shareholderId;
     private final String shareholderName;
     private final String shareholderNameId;
+    private List<Path> paths = new ArrayList<>();
     private BigDecimal totalValidRatio = ZERO;
     private boolean isDirectShareholder = false;
     private BigDecimal directRatio = ZERO;
