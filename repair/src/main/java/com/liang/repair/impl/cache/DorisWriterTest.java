cd997c6f4d5ba2a8bf894fdda661c8b5f216dffc
liang <li_ang_666@163.com>
2024-02-01 16:09:49 +0800

    lo

--- repair/src/main/java/com/liang/repair/impl/cache/DorisWriterTest.java
+++ repair/src/main/java/com/liang/repair/impl/cache/DorisWriterTest.java
@@ -6,24 +6,22 @@ import com.liang.common.dto.DorisSchema;
 import com.liang.common.service.database.template.DorisParquetWriter;
 import com.liang.repair.service.ConfigHolder;
 
-import java.util.Arrays;
-
 public class DorisWriterTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        DorisParquetWriter dorisWriter = new DorisParquetWriter("dorisSink", 1024 * 1024 * 1024);
+        DorisParquetWriter dorisWriter = new DorisParquetWriter("dorisSink", 1024 * 1024);
         DorisSchema uniqueSchema = DorisSchema.builder()
                 .database("test")
                 .tableName("parquet_test")
                 .uniqueDeleteOn(DorisSchema.DEFAULT_UNIQUE_DELETE_ON)
-                .derivedColumns(Arrays.asList("id = id + 10", "name = concat(name, '(modified by derived column setting)')"))
+                //.derivedColumns(Arrays.asList("id = id + 10", "name = concat(name, '(modified by derived column setting)')"))
                 .build();
         DorisOneRow unique = new DorisOneRow(uniqueSchema)
                 .put("id", 0)
-                .put("name", "UNIQUE")
+                .put("name", null)
                 .put(DorisSchema.DEFAULT_UNIQUE_DELETE_COLUMN, 0);
         for (int i = 1; i <= 1024 * 1024 * 1024; i++) {
             DorisOneRow clone = SerializeUtil.clone(unique);
-            clone.put("id", i);
+            clone.put("id", new Integer(i));
             dorisWriter.write(clone);
         }
         dorisWriter.flush();
