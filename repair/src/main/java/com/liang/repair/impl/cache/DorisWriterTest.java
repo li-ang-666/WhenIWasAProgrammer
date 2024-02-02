8e67b9d724d794cc537aac3da354e002b9c81816
liang <li_ang_666@163.com>
2024-02-02 15:12:30 +0800

    lo

--- repair/src/main/java/com/liang/repair/impl/cache/DorisWriterTest.java
+++ repair/src/main/java/com/liang/repair/impl/cache/DorisWriterTest.java
@@ -3,12 +3,12 @@ package com.liang.repair.impl.cache;
 import cn.hutool.core.util.SerializeUtil;
 import com.liang.common.dto.DorisOneRow;
 import com.liang.common.dto.DorisSchema;
-import com.liang.common.service.database.template.DorisParquetWriter;
+import com.liang.common.service.database.template.DorisWriter;
 import com.liang.repair.service.ConfigHolder;
 
 public class DorisWriterTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        DorisParquetWriter dorisWriter = new DorisParquetWriter("dorisSink");
+        DorisWriter dorisWriter = new DorisWriter("dorisSink", 1024 * 1024);
         DorisSchema uniqueSchema = DorisSchema.builder()
                 .database("test")
                 .tableName("parquet_test")
@@ -21,7 +21,7 @@ public class DorisWriterTest extends ConfigHolder {
                 .put(DorisSchema.DEFAULT_UNIQUE_DELETE_COLUMN, 0);
         for (int i = 1; i <= 1024 * 1024 * 1024; i++) {
             DorisOneRow clone = SerializeUtil.clone(unique);
-            clone.put("id", new Integer(i));
+            clone.put("id", i);
             dorisWriter.write(clone);
         }
         dorisWriter.flush();
