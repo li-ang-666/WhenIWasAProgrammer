4eb16d489a6ceea6bebc7ecf25410c9d4ddd80bb
liang <li_ang_666@163.com>
2024-02-02 17:13:01 +0800

    lo

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -11,15 +11,7 @@ import java.util.stream.Collectors;
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
         String[] sources = new String[]{
-                //"dwd.dwd_user_register_details",
-                //"dwd.dwd_coupon_info",
-                //"dwd.dwd_app_active",
-                //"dim.dim_user_comparison",
-                "dwd.dwd_order_info"
-                //"dwd.dwd_pay_point_com_detail",
-                //"dwd.dwd_dispatch_task",
-                //"dwd.dwd_basic_data_collect_monitor_hours",
-                //"ads.ads_user_tag"
+                "test_db.test_ads_user_tag"
         };
         for (String source : sources) {
             String sink = "bak." + source.split("\\.")[1];
--- spark/src/main/java/com/liang/spark/job/DorisCopyJob.java
+++ spark/src/main/java/com/liang/spark/job/DorisCopyJob.java
@@ -9,18 +9,15 @@ import org.apache.spark.sql.SparkSession;
 public class DorisCopyJob {
     public static void main(String[] args) {
         SparkSession spark = SparkSessionFactory.createSpark(null);
-        String source = args[0];
-        String sink = "bak." + source.split("\\.")[1];
-        // spark
         spark.read().format("doris")
                 .option("doris.fenodes", "10.99.197.34:8030,10.99.202.71:8030,10.99.203.88:8030")
-                .option("doris.table.identifier", sink)
+                .option("doris.table.identifier", "bak.test_ads_user_tag")
                 .option("user", "dba")
                 .option("password", "Tyc@1234")
                 .option("doris.batch.size", "10240")
                 .option("doris.request.tablet.size", "1")
                 .load()
-                .repartition()
-                .foreachPartition(new DorisJob.DorisSink(ConfigUtils.getConfig(), source.split("\\.")[0], source.split("\\.")[1]));
+                .repartition(1)
+                .foreachPartition(new DorisJob.DorisSink(ConfigUtils.getConfig(), "ads", "ads_user_tag_v2"));
     }
 }
