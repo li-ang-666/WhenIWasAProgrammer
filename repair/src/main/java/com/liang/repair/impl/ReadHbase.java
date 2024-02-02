9a4ab54ad4ff51604cce195684e73c5cd8b3a013
liang <li_ang_666@163.com>
2024-02-02 14:21:03 +0800

    lo

--- repair/src/main/java/com/liang/repair/impl/ReadHbase.java
+++ repair/src/main/java/com/liang/repair/impl/ReadHbase.java
@@ -15,7 +15,7 @@ public class ReadHbase extends ConfigHolder {
     }
 
     public static void main(String[] args) {
-        HbaseOneRow hbaseOneRow = new HbaseOneRow(HbaseSchema.HUMAN_ALL_COUNT, "K001N90002B0E2JCY");
+        HbaseOneRow hbaseOneRow = new HbaseOneRow(HbaseSchema.HUMAN_ALL_COUNT, "U0CTK9H00593KA5MF");
         HbaseOneRow queryResult = query(hbaseOneRow);
         queryResult.put("num_control_ability", null);
         queryResult.put("num_benefit_ability", null);
