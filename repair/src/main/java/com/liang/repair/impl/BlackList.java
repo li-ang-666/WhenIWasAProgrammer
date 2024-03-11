3be3e22ce5f95d0fb31b2c2b1a31385856b539e0
liang <li_ang_666@163.com>
2024-03-11 11:49:27 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/BlackList.java
+++ repair/src/main/java/com/liang/repair/impl/BlackList.java
@@ -32,7 +32,11 @@ public class BlackList extends ConfigHolder {
                 .update("delete from cooperation_partner where boss_human_pid = 'B0LM3MN026619B5PV' ");
         // 合作伙伴
         new JdbcTemplate("467.company_base")
-                .update("delete from cooperation_partner where boss_human_pid = '604CHMA09HHC3DQ2V' ");
+                .update("delete from cooperation_partner where boss_human_pid = '604CHMA09HHC3DQ2V' ");// 合作伙伴
+        new JdbcTemplate("467.company_base")
+                .update("delete from cooperation_partner where boss_human_pid = '4007VMD00G8D0BNHB' ");// 合作伙伴
+        new JdbcTemplate("467.company_base")
+                .update("delete from cooperation_partner where boss_human_pid = '1017NMG0128P0BNHV' ");
         // 数字安徽有限责任公司 实控人
         new JdbcTemplate("457.prism_shareholder_path")
                 .update("delete from investment_relation where company_id_invested = 5565229730 and company_entity_inlink like '%:1186041:company%'");
