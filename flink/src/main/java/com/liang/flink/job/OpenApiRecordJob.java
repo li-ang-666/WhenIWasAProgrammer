a04015108579aedaecfbc92c9541c0d08707f7d7
liang <li_ang_666@163.com>
2024-03-11 16:11:11 +0800

    1

--- flink/src/main/java/com/liang/flink/job/OpenApiRecordJob.java
+++ flink/src/main/java/com/liang/flink/job/OpenApiRecordJob.java
@@ -30,13 +30,28 @@ import java.util.concurrent.locks.LockSupport;
 
 /*
  * drop table if exists flink.open_api_record;
- * create external table if not exists flink.open_api_record(
- *   id bigint,
- *   name string,
- *   age int
- * )partitioned by(pt string)
+ *
+ * create external table if not exists flink.open_api_record (
+ *   org_name string,
+ *   order_code string,
+ *   token string,
+ *   interface_id string,
+ *   interface_name string,
+ *   billing_rules string,
+ *   request_ip string,
+ *   request_timestamp string,
+ *   response_timestamp string,
+ *   cost string,
+ *   error_code string,
+ *   error_message string,
+ *   charge_status string,
+ *   return_status string,
+ *   params string
+ * )
+ * partitioned by(pt string)
  * ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
  * STORED AS TEXTFILE;
+ *
  * MSCK REPAIR TABLE flink.open_api_record;
  */
 @Slf4j
