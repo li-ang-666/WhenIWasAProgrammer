c61f8c47d9c608aaaef6c9ad4403645bb9fe44ab
liang <li_ang_666@163.com>
2024-03-14 16:54:32 +0800

    1

--- flink/src/main/java/com/liang/flink/job/BidJob.java
+++ flink/src/main/java/com/liang/flink/job/BidJob.java
@@ -2,14 +2,19 @@ package com.liang.flink.job;
 
 
 import cn.hutool.http.HttpUtil;
+import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.liang.common.dto.Config;
+import com.liang.common.service.SQL;
+import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.common.util.ConfigUtils;
+import com.liang.common.util.SqlUtils;
 import com.liang.flink.basic.EnvironmentFactory;
 import com.liang.flink.basic.StreamFactory;
 import com.liang.flink.dto.SingleCanalBinlog;
 import com.liang.flink.service.LocalConfigFile;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
@@ -24,7 +29,7 @@ public class BidJob {
         StreamExecutionEnvironment env = EnvironmentFactory.create(args);
         Config config = ConfigUtils.getConfig();
         StreamFactory.create(env)
-                .keyBy(e -> e.getColumnMap().get("uuid"))
+                .keyBy(e -> e.getColumnMap().get("id"))
                 .addSink(new BidSink(config))
                 .setParallelism(config.getFlinkConfig().getOtherParallel())
                 .name("BidSink")
@@ -36,27 +41,46 @@ public class BidJob {
     private static final class BidSink extends RichSinkFunction<SingleCanalBinlog> {
         private static final int TIMEOUT = 1000 * 60;
         private static final String URL = "http://10.99.199.173:10040/linking_yuqing_rank";
+        private static final String SINK_TABlE = "company_bid_with_post_result";
         private final Config config;
+        private JdbcTemplate sink;
 
         @Override
         public void open(Configuration parameters) {
             ConfigUtils.setConfig(config);
+            sink = new JdbcTemplate("448.operating_info");
         }
 
         @Override
         public void invoke(SingleCanalBinlog singleCanalBinlog, Context context) {
+            // read map
             Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+            String id = String.valueOf(columnMap.get("id"));
             String uuid = String.valueOf(columnMap.get("uuid"));
             String content = String.valueOf(columnMap.get("content"));
-            doPost(content, uuid);
+            // delete
+            String deleteSql = new SQL().DELETE_FROM(SINK_TABlE)
+                    .WHERE("id = " + SqlUtils.formatValue(id))
+                    .toString();
+            if (singleCanalBinlog.getEventType() == CanalEntry.EventType.DELETE) {
+                sink.update(deleteSql);
+                return;
+            }
+            // write map
+            columnMap.put("post_result", doPost(content, uuid));
+            Tuple2<String, String> insert = SqlUtils.columnMap2Insert(columnMap);
+            String insertSql = new SQL().REPLACE_INTO(SINK_TABlE)
+                    .INTO_COLUMNS(insert.f0)
+                    .INTO_VALUES(insert.f1)
+                    .toString();
+            sink.update(deleteSql, insertSql);
         }
 
-        private void doPost(String content, String uuid) {
+        private String doPost(String content, String uuid) {
             Map<String, Object> paramMap = new HashMap<>();
             paramMap.put("text", content);
             paramMap.put("bid_uuid", uuid);
-            String result = HttpUtil.post(URL, paramMap, TIMEOUT);
-            log.info("post result: {}", result);
+            return HttpUtil.post(URL, paramMap, TIMEOUT);
         }
     }
 }
