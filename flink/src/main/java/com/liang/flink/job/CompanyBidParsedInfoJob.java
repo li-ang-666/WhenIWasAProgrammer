96dee48491da6061cd62b23eeb640e219c82c846
liang <li_ang_666@163.com>
2024-03-25 11:21:39 +0800

    1

--- flink/src/main/java/com/liang/flink/job/CompanyBidParsedInfoJob.java
+++ flink/src/main/java/com/liang/flink/job/CompanyBidParsedInfoJob.java
@@ -1,15 +1,12 @@
 package com.liang.flink.job;
 
-import cn.hutool.core.util.ObjUtil;
 import com.alibaba.otter.canal.protocol.CanalEntry;
-import com.google.common.collect.Lists;
 import com.liang.common.dto.Config;
 import com.liang.common.service.SQL;
 import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.common.util.ConfigUtils;
 import com.liang.common.util.JsonUtils;
 import com.liang.common.util.SqlUtils;
-import com.liang.common.util.TycUtils;
 import com.liang.flink.basic.EnvironmentFactory;
 import com.liang.flink.basic.StreamFactory;
 import com.liang.flink.dto.SingleCanalBinlog;
@@ -21,10 +18,14 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.liang.flink.project.bid.BidUtils.parseBidInfo;
 
 @Slf4j
-@SuppressWarnings("unchecked")
 @LocalConfigFile("company-bid-parsed-info.yml")
 public class CompanyBidParsedInfoJob {
     public static void main(String[] args) throws Exception {
@@ -117,118 +118,6 @@ public class CompanyBidParsedInfoJob {
             columnMap.put("is_dirty", result.getOrDefault("is_dirty", "0"));
             columnMap.putAll(parseBidInfo(String.valueOf(result.getOrDefault("bid_info", "[]"))));
             return columnMap;
-        }
-
-        private Map<String, Object> parseBidInfo(String bidInfo) {
-            Map<String, Object> columnMap = new LinkedHashMap<>();
-            // prepare
-            List<String> itemNos = Lists.newArrayList();
-            List<String> contractNos = Lists.newArrayList();
-            List<Map<String, Object>> purchasers = Lists.newArrayList();
-            List<Map<String, Object>> candidates = Lists.newArrayList();
-            List<Map<String, Object>> winners = Lists.newArrayList();
-            List<Map<String, Object>> winnerRawAmounts = Lists.newArrayList();
-            List<Map<String, Object>> winnerAmounts = Lists.newArrayList();
-            List<Map<String, Object>> budgetRawAmounts = Lists.newArrayList();
-            List<Map<String, Object>> budgetAmounts = Lists.newArrayList();
-            // parse
-            List<Object> parsedObjects = ObjUtil.defaultIfNull(JsonUtils.parseJsonArr(bidInfo), new ArrayList<>());
-            for (Object parsedObject : parsedObjects) {
-                Map<String, Object> parsedMap = (Map<String, Object>) parsedObject;
-                // item_no
-                itemNos.add(String.valueOf(parsedMap.getOrDefault("item_no", "")));
-                // contract_no
-                contractNos.add(String.valueOf(parsedMap.getOrDefault("contract_no", "")));
-                // action
-                List<Map<String, Object>> actionMaps = (List<Map<String, Object>>) (parsedMap.getOrDefault("action", new ArrayList<>()));
-                for (Map<String, Object> actionMap : actionMaps) {
-                    // raw budget
-                    String rawBalance = String.valueOf(actionMap.getOrDefault("raw_balance", ""));
-                    // 预算, 不为空就保留
-                    if (TycUtils.isValidName(rawBalance)) {
-                        budgetRawAmounts.add(new HashMap<String, Object>() {{
-                            put("amount", rawBalance);
-                        }});
-                    }
-                    // budget
-                    String balance = String.valueOf(actionMap.getOrDefault("balance", ""));
-                    // 预算, 不为空就保留
-                    if (TycUtils.isValidName(balance)) {
-                        budgetAmounts.add(new HashMap<String, Object>() {{
-                            put("amount", balance);
-                        }});
-                    }
-                    // purchaser
-                    List<Map<String, Object>> clientMaps = (List<Map<String, Object>>) (actionMap.getOrDefault("client", new ArrayList<>()));
-                    for (Map<String, Object> clientMap : clientMaps) {
-                        String name = String.valueOf(clientMap.getOrDefault("name", ""));
-                        // 只要名字正确, 就保留
-                        if (TycUtils.isValidName(name)) {
-                            purchasers.add(new LinkedHashMap<String, Object>() {{
-                                put("gid", queryCompanyIdByCompanyName(name));
-                                put("name", name);
-                            }});
-                        }
-                    }
-                    // candidate
-                    Map<String, Object> candidateMap = (Map<String, Object>) (actionMap.getOrDefault("supplier_candidate", new LinkedHashMap<>()));
-                    List<Map<String, Object>> candidateEntities = (List<Map<String, Object>>) (candidateMap.getOrDefault("entity", new ArrayList<>()));
-                    for (Map<String, Object> candidateEntity : candidateEntities) {
-                        String name = String.valueOf(candidateEntity.getOrDefault("name", ""));
-                        // 只要名字正确, 就保留
-                        if (TycUtils.isValidName(name)) {
-                            candidates.add(new LinkedHashMap<String, Object>() {{
-                                put("gid", queryCompanyIdByCompanyName(name));
-                                put("name", name);
-                                put("raw_offer_price", String.valueOf(candidateEntity.getOrDefault("raw_offer_price", "")));
-                                put("offer_price", String.valueOf(candidateEntity.getOrDefault("offer_price", "")));
-                            }});
-                        }
-                    }
-                    // winner
-                    Map<String, Object> winnerMap = (Map<String, Object>) (actionMap.getOrDefault("supplier", new LinkedHashMap<>()));
-                    List<Map<String, Object>> winnerEntities = (List<Map<String, Object>>) (winnerMap.getOrDefault("entity", new ArrayList<>()));
-                    for (Map<String, Object> winnerEntity : winnerEntities) {
-                        String name = String.valueOf(winnerEntity.getOrDefault("name", ""));
-                        // 只要名字正确, 就保留
-                        if (TycUtils.isValidName(name)) {
-                            // winner
-                            winners.add(new LinkedHashMap<String, Object>() {{
-                                put("gid", queryCompanyIdByCompanyName(name));
-                                put("name", name);
-                            }});
-                            // raw winner amount
-                            winnerRawAmounts.add(new HashMap<String, Object>() {{
-                                put("amount", String.valueOf(winnerEntity.getOrDefault("raw_offer_price", "")));
-                            }});
-                            // winner amount
-                            winnerAmounts.add(new HashMap<String, Object>() {{
-                                put("amount", String.valueOf(winnerEntity.getOrDefault("offer_price", "")));
-                            }});
-                        }
-                    }
-                }
-            }
-            columnMap.put("item_no", itemNos.isEmpty() ? "" : itemNos.get(0));
-            columnMap.put("contract_no", contractNos.isEmpty() ? "" : contractNos.get(0));
-            columnMap.put("purchasers", purchasers.isEmpty() ? "" : JsonUtils.toString(purchasers));
-            columnMap.put("candidates", candidates.isEmpty() ? "" : JsonUtils.toString(candidates));
-            columnMap.put("winners", winners.isEmpty() ? "" : JsonUtils.toString(winners));
-            columnMap.put("winner_raw_amounts", winnerRawAmounts.isEmpty() ? "" : JsonUtils.toString(winnerRawAmounts));
-            columnMap.put("winner_amounts", winnerAmounts.isEmpty() ? "" : JsonUtils.toString(winnerAmounts));
-            columnMap.put("budget_raw_amounts", budgetRawAmounts.isEmpty() ? "" : JsonUtils.toString(budgetRawAmounts));
-            columnMap.put("budget_amounts", budgetAmounts.isEmpty() ? "" : JsonUtils.toString(budgetAmounts));
-            return columnMap;
-        }
-
-        private String queryCompanyIdByCompanyName(String companyName) {
-            String sql = new SQL()
-                    .SELECT("company_id")
-                    .FROM("company_index")
-                    .WHERE("company_name = " + SqlUtils.formatValue(companyName))
-                    .toString();
-            String res = companyBase435.queryForObject(sql, rs -> rs.getString(1));
-            return ObjUtil.defaultIfNull(res, "");
         }
     }
 }
---
+++ flink/src/main/java/com/liang/flink/project/bid/BidUtils.java
@@ -0,0 +1,126 @@
+package com.liang.flink.project.bid;
+
+import cn.hutool.core.util.ObjUtil;
+import com.google.common.collect.Lists;
+import com.liang.common.service.SQL;
+import com.liang.common.service.database.template.JdbcTemplate;
+import com.liang.common.util.JsonUtils;
+import com.liang.common.util.SqlUtils;
+import com.liang.common.util.TycUtils;
+
+import java.util.*;
+
+@SuppressWarnings("unchecked")
+public class BidUtils {
+    public static Map<String, Object> parseBidInfo(String bidInfo) {
+        Map<String, Object> columnMap = new LinkedHashMap<>();
+        // prepare
+        List<String> itemNos = Lists.newArrayList();
+        List<String> contractNos = Lists.newArrayList();
+        List<Map<String, Object>> purchasers = Lists.newArrayList();
+        List<Map<String, Object>> candidates = Lists.newArrayList();
+        List<Map<String, Object>> winners = Lists.newArrayList();
+        List<Map<String, Object>> winnerRawAmounts = Lists.newArrayList();
+        List<Map<String, Object>> winnerAmounts = Lists.newArrayList();
+        List<Map<String, Object>> budgetRawAmounts = Lists.newArrayList();
+        List<Map<String, Object>> budgetAmounts = Lists.newArrayList();
+        // parse
+        List<Object> parsedObjects = ObjUtil.defaultIfNull(JsonUtils.parseJsonArr(bidInfo), new ArrayList<>());
+        for (Object parsedObject : parsedObjects) {
+            Map<String, Object> parsedMap = (Map<String, Object>) parsedObject;
+            // item_no
+            itemNos.add(String.valueOf(parsedMap.getOrDefault("item_no", "")));
+            // contract_no
+            contractNos.add(String.valueOf(parsedMap.getOrDefault("contract_no", "")));
+            // action
+            List<Map<String, Object>> actionMaps = (List<Map<String, Object>>) (parsedMap.getOrDefault("action", new ArrayList<>()));
+            for (Map<String, Object> actionMap : actionMaps) {
+                // raw budget
+                String rawBalance = String.valueOf(actionMap.getOrDefault("raw_balance", ""));
+                // 预算, 不为空就保留
+                if (TycUtils.isValidName(rawBalance)) {
+                    budgetRawAmounts.add(new HashMap<String, Object>() {{
+                        put("amount", rawBalance);
+                    }});
+                }
+                // budget
+                String balance = String.valueOf(actionMap.getOrDefault("balance", ""));
+                // 预算, 不为空就保留
+                if (TycUtils.isValidName(balance)) {
+                    budgetAmounts.add(new HashMap<String, Object>() {{
+                        put("amount", balance);
+                    }});
+                }
+                // purchaser
+                List<Map<String, Object>> clientMaps = (List<Map<String, Object>>) (actionMap.getOrDefault("client", new ArrayList<>()));
+                for (Map<String, Object> clientMap : clientMaps) {
+                    String name = String.valueOf(clientMap.getOrDefault("name", ""));
+                    // 只要名字正确, 就保留
+                    if (TycUtils.isValidName(name)) {
+                        purchasers.add(new LinkedHashMap<String, Object>() {{
+                            put("gid", queryCompanyIdByCompanyName(name));
+                            put("name", name);
+                        }});
+                    }
+                }
+                // candidate
+                Map<String, Object> candidateMap = (Map<String, Object>) (actionMap.getOrDefault("supplier_candidate", new LinkedHashMap<>()));
+                List<Map<String, Object>> candidateEntities = (List<Map<String, Object>>) (candidateMap.getOrDefault("entity", new ArrayList<>()));
+                for (Map<String, Object> candidateEntity : candidateEntities) {
+                    String name = String.valueOf(candidateEntity.getOrDefault("name", ""));
+                    // 只要名字正确, 就保留
+                    if (TycUtils.isValidName(name)) {
+                        candidates.add(new LinkedHashMap<String, Object>() {{
+                            put("gid", queryCompanyIdByCompanyName(name));
+                            put("name", name);
+                            put("raw_offer_price", String.valueOf(candidateEntity.getOrDefault("raw_offer_price", "")));
+                            put("offer_price", String.valueOf(candidateEntity.getOrDefault("offer_price", "")));
+                        }});
+                    }
+                }
+                // winner
+                Map<String, Object> winnerMap = (Map<String, Object>) (actionMap.getOrDefault("supplier", new LinkedHashMap<>()));
+                List<Map<String, Object>> winnerEntities = (List<Map<String, Object>>) (winnerMap.getOrDefault("entity", new ArrayList<>()));
+                for (Map<String, Object> winnerEntity : winnerEntities) {
+                    String name = String.valueOf(winnerEntity.getOrDefault("name", ""));
+                    // 只要名字正确, 就保留
+                    if (TycUtils.isValidName(name)) {
+                        // winner
+                        winners.add(new LinkedHashMap<String, Object>() {{
+                            put("gid", queryCompanyIdByCompanyName(name));
+                            put("name", name);
+                        }});
+                        // raw winner amount
+                        winnerRawAmounts.add(new HashMap<String, Object>() {{
+                            put("amount", String.valueOf(winnerEntity.getOrDefault("raw_offer_price", "")));
+                        }});
+                        // winner amount
+                        winnerAmounts.add(new HashMap<String, Object>() {{
+                            put("amount", String.valueOf(winnerEntity.getOrDefault("offer_price", "")));
+                        }});
+                    }
+                }
+            }
+        }
+        columnMap.put("item_no", itemNos.isEmpty() ? "" : itemNos.get(0));
+        columnMap.put("contract_no", contractNos.isEmpty() ? "" : contractNos.get(0));
+        columnMap.put("purchasers", purchasers.isEmpty() ? "" : JsonUtils.toString(purchasers));
+        columnMap.put("candidates", candidates.isEmpty() ? "" : JsonUtils.toString(candidates));
+        columnMap.put("winners", winners.isEmpty() ? "" : JsonUtils.toString(winners));
+        columnMap.put("winner_raw_amounts", winnerRawAmounts.isEmpty() ? "" : JsonUtils.toString(winnerRawAmounts));
+        columnMap.put("winner_amounts", winnerAmounts.isEmpty() ? "" : JsonUtils.toString(winnerAmounts));
+        columnMap.put("budget_raw_amounts", budgetRawAmounts.isEmpty() ? "" : JsonUtils.toString(budgetRawAmounts));
+        columnMap.put("budget_amounts", budgetAmounts.isEmpty() ? "" : JsonUtils.toString(budgetAmounts));
+        return columnMap;
+    }
+
+    private static String queryCompanyIdByCompanyName(String companyName) {
+        String sql = new SQL()
+                .SELECT("company_id")
+                .FROM("company_index")
+                .WHERE("company_name = " + SqlUtils.formatValue(companyName))
+                .toString();
+        String res = new JdbcTemplate("435.company_base").queryForObject(sql, rs -> rs.getString(1));
+        return ObjUtil.defaultIfNull(res, "");
+    }
+}
