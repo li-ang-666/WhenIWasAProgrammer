ddfdd1bc7e2818b67885f700cde7b019813b4ab6
liang <li_ang_666@163.com>
2024-04-26 15:33:42 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -1,16 +1,44 @@
 package com.liang.repair.test;
 
-import com.liang.common.dto.Config;
-import com.liang.common.util.ApolloUtils;
-import com.liang.common.util.ConfigUtils;
+import cn.hutool.core.io.IoUtil;
+import com.liang.common.service.SQL;
+import com.liang.common.service.database.template.JdbcTemplate;
+import com.liang.common.util.SqlUtils;
+import com.liang.common.util.TycUtils;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        Config config = ConfigUtils.createConfig("");
-        ConfigUtils.setConfig(config);
-        ApolloUtils.get("queryHasController");
+        JdbcTemplate prism116 = new JdbcTemplate("116.prism");
+        JdbcTemplate graphData430 = new JdbcTemplate("430.graph_data");
+        prism116.enableCache();
+        graphData430.enableCache();
+        InputStream resourceAsStream = RepairTest.class.getClassLoader().getResourceAsStream("diff-company-ids.txt");
+        String[] companyIds = IoUtil.readUtf8(resourceAsStream).split("\n");
+        for (String companyId : companyIds) {
+            if (!TycUtils.isUnsignedId(companyId)) {
+                continue;
+            }
+            String deleteSql = new SQL().DELETE_FROM("company_equity_relation_details")
+                    .WHERE("company_id_invested = " + SqlUtils.formatValue(companyId))
+                    .toString();
+            graphData430.update(deleteSql);
+        }
+        graphData430.flush();
+        for (String companyId : companyIds) {
+            if (!TycUtils.isUnsignedId(companyId)) {
+                continue;
+            }
+            String updateSql = new SQL().UPDATE("equity_ratio")
+                    .SET("update_time = now()")
+                    .WHERE("company_graph_id = " + SqlUtils.formatValue(companyId))
+                    .toString();
+            prism116.update(updateSql);
+        }
+        prism116.flush();
     }
 }
