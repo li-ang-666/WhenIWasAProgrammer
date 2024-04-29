f97e8b689958841ef68c75e02ed51d9dff7f05c2
liang <li_ang_666@163.com>
2024-04-29 19:08:16 +0800

    1

--- flink/src/main/java/com/liang/flink/test/FlinkTest.java
+++ flink/src/main/java/com/liang/flink/test/FlinkTest.java
@@ -1,7 +1,9 @@
 package com.liang.flink.test;
 
+import cn.hutool.core.io.IoUtil;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -15,22 +17,25 @@ public class FlinkTest {
 
     public static void main(String[] args) throws Exception {
         Class.forName(DRIVER);
-        String[] sqls = new String[]{
-                "set spark.yarn.queue=offline",
-                "set spark.yarn.priority=999",
-                "set spark.executor.memory=16g",
-                "set spark.executor.memoryOverhead=1g",
-                "set spark.driver.memory=2g",
-                "set spark.driver.memoryOverhead=1g",
-                "set mapred.max.split.size=9223372036854775807",
-                "set mapred.min.split.size.per.node=9223372036854775807",
-                "set mapred.min.split.size.per.rack=9223372036854775807",
-                "set mapred.reduce.tasks=1",
-        };
+//        String[] sqls = new String[]{
+//                "set spark.yarn.queue=offline",
+//                "set spark.yarn.priority=999",
+//                "set spark.executor.memory=16g",
+//                "set spark.executor.memoryOverhead=1g",
+//                "set spark.driver.memory=2g",
+//                "set spark.driver.memoryOverhead=1g",
+//                "set mapred.max.split.size=9223372036854775807",
+//                "set mapred.min.split.size.per.node=9223372036854775807",
+//                "set mapred.min.split.size.per.rack=9223372036854775807",
+//                "set mapred.reduce.tasks=1",
+//        };
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            for (String sql : sqls) {
-                connection.prepareStatement(sql).executeUpdate();
-            }
+//            for (String sql : sqls) {
+//                connection.prepareStatement(sql).executeUpdate();
+//            }
+            InputStream resourceAsStream = FlinkTest.class.getClassLoader().getResourceAsStream("sql.txt");
+            String sql = IoUtil.readUtf8(resourceAsStream);
+            connection.prepareStatement(sql).executeUpdate();
         } catch (Exception e) {
             log.error("error", e);
         }
