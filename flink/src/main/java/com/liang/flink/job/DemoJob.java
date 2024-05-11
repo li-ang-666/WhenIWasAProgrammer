3cbbc39dc0a18c6d6f76ceaececbc181ade991b7
liang <li_ang_666@163.com>
2024-05-11 14:29:11 +0800

    1

--- flink/src/main/java/com/liang/flink/job/DemoJob.java
+++ flink/src/main/java/com/liang/flink/job/DemoJob.java
@@ -19,7 +19,12 @@ public class DemoJob {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = EnvironmentFactory.create(args);
         Config config = ConfigUtils.getConfig();
-        StreamFactory.create(env).rebalance().addSink(new DemoSink(config)).name("DemoSink").setParallelism(config.getFlinkConfig().getOtherParallel());
+        StreamFactory.create(env)
+                .rebalance()
+                .addSink(new DemoSink(config))
+                .name("DemoSink")
+                .uid("DemoSink")
+                .setParallelism(config.getFlinkConfig().getOtherParallel());
         env.execute("DemoJob");
     }
 
