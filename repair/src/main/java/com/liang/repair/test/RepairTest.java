27bfb78f1ba622249df8fa2f03b532bb525f6493
liang <li_ang_666@163.com>
2024-05-13 16:26:12 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -2,11 +2,21 @@ package com.liang.repair.test;
 
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.roaringbitmap.longlong.Roaring64Bitmap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        int i = 5 / 2;
-        System.out.println(i);
+        Map<Integer, Roaring64Bitmap> count2Bitmap = new HashMap<Integer, Roaring64Bitmap>();
+        Integer count = count2Bitmap
+                .entrySet()
+                .parallelStream()
+                .filter(entry -> entry.getValue().contains(2318455639L))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .get();
     }
 }
