c0adcdbd577d7c8d9139d81a3eccf7f37d9205cb
liang <li_ang_666@163.com>
2024-05-14 09:59:35 +0800

    1

--- spark/src/main/java/com/liang/spark/job/RdsToHiveJob.java
+++ spark/src/main/java/com/liang/spark/job/RdsToHiveJob.java
@@ -5,13 +5,14 @@ import com.liang.spark.basic.TableFactory;
 import org.apache.spark.sql.SparkSession;
 
 public class RdsToHiveJob {
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+    public static void main(String[] args) {
         SparkSession spark = SparkSessionFactory.createSpark(args);
-        String rds = "491.prism_shareholder_path";
-        String table = "tyc_group";
+        String rds = "467.company_base";
+        String table = "cooperation_partner";
         TableFactory
                 .jdbc(spark, rds, table)
+                .drop("id", "create_time", "update_time")
                 .createOrReplaceTempView("t");
-        spark.sql("insert overwrite table test.tyc_group select /*+ REPARTITION(32) */ * from t");
+        spark.sql("insert overwrite table hudi_ads.cooperation_partner(pt=1) select /*+ REPARTITION(32) */ * from t");
     }
 }
