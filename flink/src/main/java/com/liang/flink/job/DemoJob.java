1d9e23fc555e7d815def7f4450351bce225846f4
liang <li_ang_666@163.com>
2024-05-14 11:20:11 +0800

    1

--- flink/src/main/java/com/liang/flink/job/DemoJob.java
+++ flink/src/main/java/com/liang/flink/job/DemoJob.java
@@ -2,6 +2,7 @@ package com.liang.flink.job;
 
 import com.liang.common.dto.Config;
 import com.liang.common.service.database.template.JdbcTemplate;
+import com.liang.common.service.storage.ObsWriter;
 import com.liang.common.util.ConfigUtils;
 import com.liang.flink.basic.EnvironmentFactory;
 import com.liang.flink.basic.StreamFactory;
@@ -10,8 +11,13 @@ import com.liang.flink.service.LocalConfigFile;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+
+import java.util.Map;
 
 @Slf4j
 @LocalConfigFile("demo.yml")
@@ -29,19 +35,48 @@ public class DemoJob {
     }
 
     @RequiredArgsConstructor
-    private final static class DemoSink extends RichSinkFunction<SingleCanalBinlog> {
+    private final static class DemoSink extends RichSinkFunction<SingleCanalBinlog> implements CheckpointedFunction {
         private final Config config;
         private JdbcTemplate jdbcTemplate;
+        private ObsWriter obsWriter;
+
+        @Override
+        public void initializeState(FunctionInitializationContext context) {
+        }
 
         @Override
         public void open(Configuration parameters) {
             ConfigUtils.setConfig(config);
             jdbcTemplate = new JdbcTemplate("427.test");
             jdbcTemplate.enableCache();
+            obsWriter = new ObsWriter("obs://hadoop-obs/flink/", ObsWriter.FileFormat.TXT);
+            obsWriter.enableCache();
         }
 
         @Override
         public void invoke(SingleCanalBinlog singleCanalBinlog, Context context) {
+            Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+            String id = String.valueOf(columnMap.get("id"));
+            obsWriter.update(id);
+        }
+
+        @Override
+        public void snapshotState(FunctionSnapshotContext context) {
+            flush();
+        }
+
+        @Override
+        public void finish() {
+            flush();
+        }
+
+        @Override
+        public void close() {
+            flush();
+        }
+
+        private void flush() {
+            obsWriter.flush();
         }
     }
 }
