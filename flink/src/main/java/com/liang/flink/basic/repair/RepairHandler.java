87b16d7170737fa951c1f952d02711b543c00324
liang <li_ang_666@163.com>
2024-05-17 19:11:41 +0800

    1

--- flink/src/main/java/com/liang/flink/basic/repair/RepairHandler.java
+++ flink/src/main/java/com/liang/flink/basic/repair/RepairHandler.java
@@ -1,5 +1,6 @@
 package com.liang.flink.basic.repair;
 
+import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.liang.common.dto.Config;
 import com.liang.common.dto.config.RepairTask;
 import com.liang.common.service.database.template.JdbcTemplate;
@@ -10,6 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.util.Collector;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -31,14 +35,8 @@ public class RepairHandler extends RichFlatMapFunction<RepairSplit, SingleCanalB
 
     @Override
     public void flatMap(RepairSplit repairSplit, Collector<SingleCanalBinlog> out) {
-        //try {
-        //    log.info("get repairSplit: {}", repairSplit);
-        //    TimeUnit.HOURS.sleep(1);
-        //} catch (Exception ignore) {
-        //}
-        //List<Map<String, Object>> columnMaps = jdbcTemplate.queryForColumnMaps(repairSplit.getSql());
-        //for (Map<String, Object> columnMap : columnMaps) {
-        //    out.collect(new SingleCanalBinlog(repairTaskOfThisSubtask.getSourceName(), repairTaskOfThisSubtask.getTableName(), -1L, CanalEntry.EventType.INSERT, columnMap, new HashMap<>(), columnMap));
-        //}
+        for (Map<String, Object> columnMap : jdbcTemplate.queryForColumnMaps(repairSplit.getSql())) {
+            out.collect(new SingleCanalBinlog(repairTaskOfThisSubtask.getSourceName(), repairTaskOfThisSubtask.getTableName(), -1L, CanalEntry.EventType.INSERT, columnMap, new HashMap<>(), columnMap));
+        }
     }
 }
