60e7e3fe27e8b073203e91b713329efe892e7dda
liang <li_ang_666@163.com>
2024-05-21 14:50:05 +0800

    1

--- flink/src/main/java/com/liang/flink/basic/repair/RepairHandler.java
+++ flink/src/main/java/com/liang/flink/basic/repair/RepairHandler.java
@@ -13,30 +13,32 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.util.Collector;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
 public class RepairHandler extends RichFlatMapFunction<RepairSplit, SingleCanalBinlog> {
     private final Config config;
-    private RepairTask repairTaskOfThisSubtask;
     private JdbcTemplate jdbcTemplate;
 
     @Override
     public void open(Configuration parameters) {
         ConfigUtils.setConfig(config);
         int indexOfThisSubtask = getRuntimeContext().getIndexOfThisSubtask();
-        repairTaskOfThisSubtask = config.getRepairTasks().parallelStream()
-                .filter(repairTask -> repairTask.getChannels().contains(indexOfThisSubtask))
-                .findFirst()
-                .orElseThrow(RuntimeException::new);
-        jdbcTemplate = new JdbcTemplate(repairTaskOfThisSubtask.getSourceName());
+        for (RepairTask repairTask : config.getRepairTasks()) {
+            if (repairTask.getChannels().contains(indexOfThisSubtask)) {
+                jdbcTemplate = new JdbcTemplate(repairTask.getSourceName());
+                return;
+            }
+        }
     }
 
     @Override
     public void flatMap(RepairSplit repairSplit, Collector<SingleCanalBinlog> out) {
-        for (Map<String, Object> columnMap : jdbcTemplate.queryForColumnMaps(repairSplit.getSql())) {
-            out.collect(new SingleCanalBinlog(repairTaskOfThisSubtask.getSourceName(), repairTaskOfThisSubtask.getTableName(), -1L, CanalEntry.EventType.INSERT, columnMap, new HashMap<>(), columnMap));
+        List<Map<String, Object>> columnMaps = jdbcTemplate.queryForColumnMaps(repairSplit.getSql());
+        for (Map<String, Object> columnMap : columnMaps) {
+            out.collect(new SingleCanalBinlog(repairSplit.getSourceName(), repairSplit.getTableName(), -1L, CanalEntry.EventType.INSERT, columnMap, new HashMap<>(), columnMap));
         }
     }
 }
