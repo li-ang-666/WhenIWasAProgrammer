ac49e112e60fff8842e6141686302fa2c2f203eb
liang <li_ang_666@163.com>
2024-05-23 17:44:55 +0800

    xiwei

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -6,25 +6,5 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        // DBConfig dbConfig = ConfigUtils.getConfig().getDbConfigs().get("430.graph_data");
-        // String url = String.format("jdbc:mysql://%s:3306/%s", dbConfig.getHost(), dbConfig.getDatabase());
-        // Connection connection = DriverManager.getConnection(url, dbConfig.getUser(), dbConfig.getPassword());
-        // Statement statement = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-        // statement.setFetchSize(Integer.MIN_VALUE);
-        // ResultSet resultSet = statement.executeQuery("select id from company_equity_relation_details where reference_pt_year = 2024");
-        // int i = 0;
-        // while (resultSet.next()) {
-        //     i++;
-        //     if (i % 10000 == 0) {
-        //         System.out.println(resultSet.getString(1));
-        //     }
-        // }
-        try {
-
-        } catch (Exception ignore) {
-
-        } finally {
-            int i = 1 / 0;
-        }
     }
 }
--- spark/src/main/java/com/liang/spark/test/SparkTest.java
+++ spark/src/main/java/com/liang/spark/test/SparkTest.java
@@ -1,9 +1,39 @@
 package com.liang.spark.test;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetWriter;
+import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
 
 @Slf4j
 public class SparkTest {
     public static void main(String[] args) throws Exception {
+        Schema schema = SchemaBuilder.record("ColumnMap").fields()
+                .optionalString("id")
+                .optionalString("name")
+                .optionalString("age")
+                .endRecord();
+        HashMap<String, Object> columnMap = new HashMap<>();
+        columnMap.put("id", 1);
+        columnMap.put("name", "LA");
+        columnMap.put("age", new BigDecimal("11"));
+        GenericRecordBuilder genericRecordBuilder = new GenericRecordBuilder(schema);
+        columnMap.forEach((k, v) -> genericRecordBuilder.set(k, StrUtil.toStringOrNull(v)));
+        ParquetWriter<GenericRecord> parquetWriter = AvroParquetWriter.<GenericRecord>builder(new Path("obs://hadoop-obs/flink/test1/bbb.parquet"))
+                .withCompressionCodec(CompressionCodecName.BROTLI)
+                .withSchema(schema)
+                .withRowGroupSize(1024 * 1024 * 2)
+                .build();
+        parquetWriter.write(genericRecordBuilder.build());
+        parquetWriter.close();
     }
 }
