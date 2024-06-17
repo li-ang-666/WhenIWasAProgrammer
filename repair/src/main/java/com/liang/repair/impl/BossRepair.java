15ba611e322034b370f1f32a8e02cd6199d1bd17
liang <li_ang_666@163.com>
2024-06-17 17:40:21 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/BossRepair.java
+++ repair/src/main/java/com/liang/repair/impl/BossRepair.java
@@ -4,11 +4,16 @@ import cn.hutool.http.HttpUtil;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public class BossRepair extends ConfigHolder {
+    private static final int TIMEOUT = (int) TimeUnit.HOURS.toMillis(24);
+
     public static void main(String[] args) {
         String body = HttpUtil.createPost("http://10.99.138.255:9060/update_boss")
-                .form("hgid", 1964456817)
+                .form("hgid", "1811666845")
+                .timeout(TIMEOUT)
                 .execute()
                 .body();
         System.out.println(body);
