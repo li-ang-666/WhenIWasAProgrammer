28a82b8ffa71aed7dc53acbf45394e35ac3818db
liang <li_ang_666@163.com>
2024-07-05 12:23:04 +0800

    1

--- flink/src/main/java/com/liang/flink/job/EquityTotalJob.java
+++ flink/src/main/java/com/liang/flink/job/EquityTotalJob.java
@@ -49,7 +49,7 @@ public class EquityTotalJob {
     private static final String QUERY_RDS = "491.prism_shareholder_path";
     private static final String QUERY_TABLE = "ratio_path_company_new";
 
-    private static final String SINK_RDS = "457.bdp_equity";
+    private static final String SINK_RDS = "463.bdp_equity";
     private static final String SINK_TABLE = "shareholder_investment_ratio_total";
 
     public static void main(String[] args) throws Exception {
