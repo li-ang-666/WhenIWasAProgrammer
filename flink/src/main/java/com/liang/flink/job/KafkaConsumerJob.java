8a816350cf38d7ec8de9d430f82436998102e00e
liang <li_ang_666@163.com>
2024-07-08 11:47:11 +0800

    1

---
+++ flink/src/main/java/com/liang/flink/job/KafkaConsumerJob.java
@@ -0,0 +1,73 @@
+package com.liang.flink.job;
+
+import com.liang.common.dto.Config;
+import com.liang.common.util.ConfigUtils;
+import com.liang.flink.basic.EnvironmentFactory;
+import com.liang.flink.basic.kafka.KafkaSourceFactory;
+import com.liang.flink.dto.KafkaRecord;
+import com.liang.flink.service.LocalConfigFile;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+
+@Slf4j
+@LocalConfigFile("kafka-consumer.yml")
+public class KafkaConsumerJob {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = EnvironmentFactory.create(args);
+        Config config = ConfigUtils.getConfig();
+        KafkaSource<KafkaRecord<String>> kafkaSource = KafkaSourceFactory.create(String::new);
+        env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "KafkaSource")
+                .setParallelism(1)
+                .map(KafkaRecord::getKey)
+                .returns(String.class)
+                .setParallelism(1)
+                .addSink(new KafkaConsumerSink(config))
+                .setParallelism(1);
+        env.execute("KafkaConsumerJob");
+    }
+
+    @RequiredArgsConstructor
+    private final static class KafkaConsumerSink extends RichSinkFunction<String> implements CheckpointedFunction {
+        private final Config config;
+
+        @Override
+        public void initializeState(FunctionInitializationContext context) {
+        }
+
+        @Override
+        public void open(Configuration parameters) {
+            ConfigUtils.setConfig(config);
+        }
+
+        @Override
+        public void invoke(String singleCanalBinlog, Context context) {
+            System.out.println();
+        }
+
+        @Override
+        public void snapshotState(FunctionSnapshotContext context) {
+            flush();
+        }
+
+        @Override
+        public void finish() {
+            flush();
+        }
+
+        @Override
+        public void close() {
+            flush();
+        }
+
+        private void flush() {
+        }
+    }
+}
--- hudi/src/main/java/com/liang/hudi/job/TestJob.java
+++ hudi/src/main/java/com/liang/hudi/job/TestJob.java
@@ -1,27 +1,25 @@
 package com.liang.hudi.job;
 
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.StrUtil;
 import com.liang.hudi.basic.TableEnvironmentFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
 public class TestJob {
     public static void main(String[] args) {
         StreamTableEnvironment tEnv = TableEnvironmentFactory.create();
+        tEnv.getConfig().set("pipeline.operator-chaining", "false");
         tEnv.getConfig().set("parallelism.default", "1");
-        tEnv.executeSql("CREATE TABLE ods (\n" +
-                "  id decimal(20,0),\n" +
-                "  op_ts as CAST(CURRENT_TIMESTAMP AS TIMESTAMP(3)),\n" +
-                "  PRIMARY KEY (id) NOT ENFORCED\n" +
-                ") WITH (\n" +
-                "  'connector' = 'jdbc',\n" +
-                "  'url' = 'jdbc:mysql://ee59dd05fc0f4bb9a2497c8d9146a53cin01.internal.cn-north-4.mysql.rds.myhuaweicloud.com:3306/company_base?useSSL=false&zeroDateTimeBehavior=convertToNull&tinyInt1isBit=false',\n" +
-                "  'table-name' = 'company_index',\n" +
-                "  'username' = 'jdhw_d_data_dml',\n" +
-                "  'password' = '2s0^tFa4SLrp72',\n" +
-                "  'scan.partition.column' = 'id',\n" +
-                "  'scan.partition.lower-bound' = '1',\n" +
-                "  'scan.partition.upper-bound' = '102400',\n" +
-                "  'scan.partition.num' = '10'\n" +
-                ")");
-        tEnv.executeSql("select * from ods").print();
+        String sqls = ResourceUtil.readStr("sql/test.sql", StandardCharsets.UTF_8);
+        for (String sql : sqls.split(";")) {
+            if (StrUtil.isNotBlank(sql)) {
+                log.info("execute sql: {}", sql);
+                tEnv.executeSql(sql);
+            }
+        }
     }
 }
