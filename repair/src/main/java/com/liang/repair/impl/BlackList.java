b0c148a612c424468621d8172dc01df287157780
liang <li_ang_666@163.com>
2024-07-11 16:27:12 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/BlackList.java
+++ repair/src/main/java/com/liang/repair/impl/BlackList.java
@@ -100,6 +100,8 @@ public class BlackList extends ConfigHolder {
         new JdbcTemplate("457.prism_shareholder_path").update("delete from investment_relation where company_id_invested in (1017,1065,1101,1172,1258,1301,1307,1347,1418,1617)");
         // 删除老板所有数据
         deleteBossAll("20TVMMN02E33YF576");
+        deleteBossAll("R01QY9G090C7122ZZ");
+        deleteBossAll("500QD9G00AC1122ZU");
         deleteBossAll("L0QNUML00TNSDBLF9");
         deleteBossAll("70LJQMK02T8UEVGDB");
         deleteBossAll("T0GC9M80M84KQSVBV");
--- repair/src/main/java/com/liang/repair/impl/CooperationPartnerRepair.java
+++ repair/src/main/java/com/liang/repair/impl/CooperationPartnerRepair.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CooperationPartnerRepair extends ConfigHolder {
     public static void main(String[] args) {
-        String url = "https://www.tianyancha.com/human/1869929361-c3454988355".replaceAll("(.*?)(\\d+.*)", "$2");
+        String url = "https://www.tianyancha.com/human/2257070243-c6292563".replaceAll("(.*?)(\\d+.*)", "$2");
         String humanGraphId = url.split("-c")[0];
         String companyGraphId = url.split("-c")[1];
         String queryHumanPidSql = String.format("select human_pid from company_human_relation where human_graph_id = %s and company_graph_id = %s",
