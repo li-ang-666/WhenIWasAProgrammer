ea52e284eae9e40f57a9d88d22a84de7eef087db
liang <li_ang_666@163.com>
2024-07-15 18:55:35 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -15,20 +15,6 @@ import java.util.HashMap;
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        ObsWriter obsWriter = new ObsWriter("obs://hadoop-obs/flink/group/", ObsWriter.FileFormat.JSON);
-        obsWriter.enableCache();
-        InputStream resourceAsStream = RepairTest.class.getClassLoader().getResourceAsStream("aaa.txt");
-        LineIter iterator = IoUtil.lineIter(resourceAsStream, StandardCharsets.UTF_8);
-        while (iterator.hasNext()) {
-            String line = iterator.next();
-            String[] split = line.split("\\s+");
-            System.out.println(Arrays.toString(split));
-            HashMap<String, Object> columnMap = new HashMap<String, Object>() {{
-                put("company_id", split[0]);
-                put("company_name", split[1]);
-            }};
-            obsWriter.update(JsonUtils.toString(columnMap));
-        }
-        obsWriter.flush();
+        System.out.println(Integer.MIN_VALUE);
     }
 }
