aac70df50ea8ae7e7008c66e3d8c3fee0c93e124
liang <li_ang_666@163.com>
2024-07-22 14:10:54 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -6,8 +6,5 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 99; i++) {
-            System.out.println(String.format("alter table ratio_path_company_new_%s engine = innodb;", i));
-        }
     }
 }
