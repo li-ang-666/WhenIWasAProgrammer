b9df5219821402c83f48dd4d94463f3c7182776e
liang <li_ang_666@163.com>
2024-07-26 19:16:47 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/CooperationPartnerRepair.java
+++ repair/src/main/java/com/liang/repair/impl/CooperationPartnerRepair.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CooperationPartnerRepair extends ConfigHolder {
     public static void main(String[] args) {
-        String url = "ttps://www.tianyancha.com/human/2310572893-c3422303675".replaceAll("(.*?)(\\d+.*)", "$2");
+        String url = "https://www.tianyancha.com/human/2245080409-c5304004869".replaceAll("(.*?)(\\d+.*)", "$2");
         String humanGraphId = url.split("-c")[0];
         String companyGraphId = url.split("-c")[1];
         String queryHumanPidSql = String.format("select human_pid from company_human_relation where human_graph_id = %s and company_graph_id = %s",
