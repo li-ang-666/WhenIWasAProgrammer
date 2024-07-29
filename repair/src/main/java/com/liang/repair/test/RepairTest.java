7f156cfbb903a4545c68deddb7bb4a20caaa48ee
liang <li_ang_666@163.com>
2024-07-29 09:46:03 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -6,5 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(String.format("drop table ratio_path_company_new_%s;", i));
+            System.out.println(String.format("alter table ratio_path_company_new_tmp_%s rename ratio_path_company_new_%s;", i, i));
+        }
     }
 }
