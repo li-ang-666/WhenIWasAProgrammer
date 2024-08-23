79e2cd537882c971d235e96561157a0ae09ffb93
liang <li_ang_666@163.com>
2024-08-23 14:10:52 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/BlackList.java
+++ repair/src/main/java/com/liang/repair/impl/BlackList.java
@@ -227,6 +227,7 @@ public class BlackList extends ConfigHolder {
         deleteBossAll("20AMSMH02KM31745B");
         deleteBossAll("30U4NMR09775BY57U");
         deleteBossAll("T0BC698092T52BR5Y");
+        deleteBossAll("S0MBDMB00VP1L79GM");
         new JdbcTemplate("457.bdp_equity").update("delete from shareholder_investment_ratio_total_new where company_id = 6135078045");
         new JdbcTemplate("491.prism_shareholder_path").update("delete from ratio_path_company_new_54 where company_id = '761462154'");
         new JdbcTemplate("463.bdp_equity").update("delete from entity_controller_details_new where company_id_controlled = '761462154'");
