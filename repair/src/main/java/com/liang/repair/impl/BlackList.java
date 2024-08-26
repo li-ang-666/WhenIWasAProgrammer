388c5aa5732a24ce5b4ecb25409b96bef2e8dceb
liang <li_ang_666@163.com>
2024-08-26 16:38:12 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/BlackList.java
+++ repair/src/main/java/com/liang/repair/impl/BlackList.java
@@ -228,6 +228,8 @@ public class BlackList extends ConfigHolder {
         deleteBossAll("30U4NMR09775BY57U");
         deleteBossAll("T0BC698092T52BR5Y");
         deleteBossAll("S0MBDMB00VP1L79GM");
+        deleteBossAll("T0ANQMN026RSZVCRV");
+        deleteBossAll("40MDPM6005G0EN95C");
         new JdbcTemplate("457.bdp_equity").update("delete from shareholder_investment_ratio_total_new where company_id = 6135078045");
         new JdbcTemplate("491.prism_shareholder_path").update("delete from ratio_path_company_new_54 where company_id = '761462154'");
         new JdbcTemplate("463.bdp_equity").update("delete from entity_controller_details_new where company_id_controlled = '761462154'");
--- repair/src/main/java/com/liang/repair/impl/CooperationPartnerRepair.java
+++ repair/src/main/java/com/liang/repair/impl/CooperationPartnerRepair.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CooperationPartnerRepair extends ConfigHolder {
     public static void main(String[] args) {
-        String url = "ttps://www.tianyancha.com/human/1807347938-c3151193732".replaceAll("(.*?)(\\d+.*)", "$2");
+        String url = "tps://www.tianyancha.com/human/1769459042-c3153613898".replaceAll("(.*?)(\\d+.*)", "$2");
         String humanGraphId = url.split("-c")[0];
         String companyGraphId = url.split("-c")[1];
         String queryHumanPidSql = String.format("select human_pid from company_human_relation where human_graph_id = %s and company_graph_id = %s",
