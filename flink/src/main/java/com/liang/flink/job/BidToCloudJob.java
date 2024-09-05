d430afdbc5fa98835a125b7779aaea1bf903d8a3
liang <li_ang_666@163.com>
2024-09-05 15:11:25 +0800

    1

--- flink/src/main/java/com/liang/flink/job/BidToCloudJob.java
+++ flink/src/main/java/com/liang/flink/job/BidToCloudJob.java
@@ -14,6 +14,7 @@ import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
 import com.vladsch.flexmark.util.data.MutableDataSet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.state.FunctionInitializationContext;
@@ -21,6 +22,7 @@ import org.apache.flink.runtime.state.FunctionSnapshotContext;
 import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.util.Collector;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -49,16 +51,48 @@ public class BidToCloudJob {
         StreamExecutionEnvironment env = EnvironmentFactory.create(args);
         Config config = ConfigUtils.getConfig();
         StreamFactory.create(env)
-                .keyBy(e -> (String) e.getColumnMap().get("id"))
-                .addSink(new BidToCloudSink(config))
+                .rebalance()
+                .flatMap(new BidToCloudMapper())
                 .setParallelism(config.getFlinkConfig().getOtherParallel())
+                .name("BidToCloudSink")
+                .uid("BidToCloudSink")
+                .keyBy(e -> (String) e.get("id"))
+                .addSink(new BidToCloudSink(config))
+                .setParallelism(11)
                 .name("BidToCloudSink")
                 .uid("BidToCloudSink");
         env.execute("BidToCloudJob");
     }
 
     @RequiredArgsConstructor
-    private static final class BidToCloudSink extends RichSinkFunction<SingleCanalBinlog> implements CheckpointedFunction {
+    private static final class BidToCloudMapper extends RichFlatMapFunction<SingleCanalBinlog, Map<String, Object>> {
+        @Override
+        public void flatMap(SingleCanalBinlog singleCanalBinlog, Collector<Map<String, Object>> out) {
+            Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
+            HashMap<String, Object> resultMap = new HashMap<>();
+            resultMap.put("id", columnMap.get("id"));
+            resultMap.put("uuid", columnMap.get("uuid"));
+            resultMap.put("title", columnMap.get("title"));
+            resultMap.put("content", htmlToMd((String) columnMap.get("uuid"), (String) columnMap.get("content")));
+            resultMap.put("deleted", columnMap.get("deleted"));
+            resultMap.put("type", columnMap.get("type"));
+            out.collect(resultMap);
+        }
+
+        private String htmlToMd(String uuid, String html) {
+            try {
+                MutableDataSet options = new MutableDataSet();
+                FlexmarkHtmlConverter converter = FlexmarkHtmlConverter.builder(options).build();
+                return converter.convert(html);
+            } catch (Exception e) {
+                log.error("html to md error, uuid: {}", uuid, e);
+                return html;
+            }
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static final class BidToCloudSink extends RichSinkFunction<Map<String, Object>> implements CheckpointedFunction {
         private final Config config;
         private JdbcTemplate sink;
 
@@ -77,32 +111,13 @@ public class BidToCloudJob {
         }
 
         @Override
-        public void invoke(SingleCanalBinlog singleCanalBinlog, Context context) {
-            Map<String, Object> columnMap = singleCanalBinlog.getColumnMap();
-            HashMap<String, Object> resultMap = new HashMap<>();
-            resultMap.put("id", columnMap.get("id"));
-            resultMap.put("uuid", columnMap.get("uuid"));
-            resultMap.put("title", columnMap.get("title"));
-            resultMap.put("content", htmlToMd((String) columnMap.get("uuid"), (String) columnMap.get("content")));
-            resultMap.put("deleted", columnMap.get("deleted"));
-            resultMap.put("type", columnMap.get("type"));
+        public void invoke(Map<String, Object> resultMap, Context context) {
             Tuple2<String, String> insert = SqlUtils.columnMap2Insert(resultMap);
             String sql = new SQL().REPLACE_INTO(SINK_TABlE)
                     .INTO_COLUMNS(insert.f0)
                     .INTO_VALUES(insert.f1)
                     .toString();
             sink.update(sql);
-        }
-
-        private String htmlToMd(String uuid, String html) {
-            try {
-                MutableDataSet options = new MutableDataSet();
-                FlexmarkHtmlConverter converter = FlexmarkHtmlConverter.builder(options).build();
-                return converter.convert(html);
-            } catch (Exception e) {
-                log.error("html to md error, uuid: {}", uuid, e);
-                return html;
-            }
         }
 
         @Override
