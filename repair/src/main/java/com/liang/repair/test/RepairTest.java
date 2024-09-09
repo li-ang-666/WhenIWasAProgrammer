a5503692fd7e5bd346b3e4dd9c9963a106ad0748
liang <li_ang_666@163.com>
2024-09-09 20:13:23 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -2,14 +2,9 @@ package com.liang.repair.test;
 
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
-import org.roaringbitmap.longlong.Roaring64Bitmap;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        Roaring64Bitmap bitmap = new Roaring64Bitmap();
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            bitmap.add(i);
-        }
     }
 }
