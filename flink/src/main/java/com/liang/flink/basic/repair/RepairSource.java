5779db6d0b1ec90daa47b07af71f5616eb4ca109
liang <li_ang_666@163.com>
2024-09-09 15:06:38 +0800

    1

--- flink/src/main/java/com/liang/flink/basic/repair/RepairSource.java
+++ flink/src/main/java/com/liang/flink/basic/repair/RepairSource.java
@@ -2,17 +2,14 @@ package com.liang.flink.basic.repair;
 
 import com.liang.common.dto.Config;
 import com.liang.common.dto.config.RepairTask;
-import com.liang.common.service.SQL;
 import com.liang.common.util.ConfigUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import static com.liang.common.dto.config.RepairTask.ScanMode.Direct;
-import static com.liang.common.dto.config.RepairTask.ScanMode.TumblingWindow;
 
 /*
  * https://nightlies.apache.org/flink/flink-docs-release-1.17/zh/docs/dev/datastream/fault-tolerance/checkpointing
@@ -22,21 +19,16 @@ import static com.liang.common.dto.config.RepairTask.ScanMode.TumblingWindow;
 @RequiredArgsConstructor
 public class RepairSource extends RichParallelSourceFunction<RepairSplit> {
     private static final int QUERY_BATCH_SIZE = 100_000_000;
-    private static final int DIRECT_SCAN_COMPLETE_FLAG = -1;
     private final AtomicBoolean canceled = new AtomicBoolean(false);
     private final Config config;
-    private RepairTask task;
-    private String baseSplitSql;
 
     @Override
     public void open(Configuration parameters) {
         ConfigUtils.setConfig(config);
-        task = config.getRepairTasks().get(getRuntimeContext().getIndexOfThisSubtask());
-        baseSplitSql = new SQL()
-                .SELECT(task.getColumns())
-                .FROM(task.getTableName())
-                .WHERE(task.getWhere())
-                .toString();
+        List<RepairTask> repairTasks = config.getRepairTasks();
+        for (RepairTask repairTask : repairTasks) {
+
+        }
     }
 
     @Override
@@ -52,26 +44,6 @@ public class RepairSource extends RichParallelSourceFunction<RepairSplit> {
             }
         }
         cancel();
-    }
-
-    private boolean hasNextSplit() {
-        return task.getScanMode() == Direct ?
-                task.getPivot() != DIRECT_SCAN_COMPLETE_FLAG : task.getPivot() < task.getUpperBound();
-    }
-
-    private RepairSplit nextSplit(int channel) {
-        String sql = baseSplitSql + (task.getScanMode() == TumblingWindow ? String.format(" AND %s <= id AND id < %s", task.getPivot(), nextPivot()) : "");
-        return new RepairSplit(task.getTaskId(), task.getSourceName(), task.getTableName(), channel, sql);
-    }
-
-    // 写pivot
-    private void commit() {
-        long nextPivot = task.getScanMode() == Direct ? DIRECT_SCAN_COMPLETE_FLAG : nextPivot();
-        task.setPivot(nextPivot);
-    }
-
-    private long nextPivot() {
-        return Math.min(task.getPivot() + QUERY_BATCH_SIZE, task.getUpperBound());
     }
 
     @Override
