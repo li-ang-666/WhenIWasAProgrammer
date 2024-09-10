9bd7af12f136318a87fb0a734626b5313b135275
liang <li_ang_666@163.com>
2024-09-10 19:42:32 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -4,23 +4,12 @@ import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        AtomicBoolean running = new AtomicBoolean(true);
-        new Thread(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (Exception ignore) {
-            }
-            running.set(false);
-        }).start();
         JdbcTemplate jdbcTemplate = new JdbcTemplate("435.company_base");
         String sql = "select 1 from company_index where 1=1 order by id";
-        jdbcTemplate.streamQueryInterruptible(sql, running, rs -> {
+        jdbcTemplate.streamQuery(sql, rs -> {
         });
         System.out.println(111);
     }
