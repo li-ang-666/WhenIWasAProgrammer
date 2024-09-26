ab3f688c15df8fe6399eb5427ef988af29f2d8e0
liang <li_ang_666@163.com>
2024-09-26 18:56:34 +0800

    1

--- flink/src/main/java/com/liang/flink/basic/repair/RepairState.java
+++ flink/src/main/java/com/liang/flink/basic/repair/RepairState.java
@@ -13,7 +13,9 @@ import java.util.stream.Collectors;
 
 @Data
 public class RepairState {
-    private final Map<RepairTask, State> states = new ConcurrentSkipListMap<>();
+    private final Map<RepairTask, State> states = new ConcurrentSkipListMap<>((task1, task2) ->
+            task1.equals(task2) ? 0 : 1
+    );
 
     public RepairState(List<RepairTask> repairTasks) {
         for (RepairTask repairTask : repairTasks) {
