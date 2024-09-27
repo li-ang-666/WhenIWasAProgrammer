ffd7167a9741bdd26247da187261c20b8149cf4b
liang <li_ang_666@163.com>
2024-09-28 04:30:08 +0800

    1

--- flink/src/main/java/com/liang/flink/basic/repair/RepairSplitEnumerator.java
+++ flink/src/main/java/com/liang/flink/basic/repair/RepairSplitEnumerator.java
@@ -27,8 +27,8 @@ public class RepairSplitEnumerator {
     public static void main(String[] args) throws Exception {
         ConfigUtils.setConfig(ConfigUtils.createConfig(null));
         RepairTask repairTask = new RepairTask();
-        repairTask.setSourceName("116.prism");
-        repairTask.setTableName("equity_ratio");
+        repairTask.setSourceName("104.data_bid");
+        repairTask.setTableName("company_bid");
 
         long sec1 = System.currentTimeMillis() / 1000;
         Roaring64Bitmap allIds = new RepairSplitEnumerator().getAllIds(repairTask);
