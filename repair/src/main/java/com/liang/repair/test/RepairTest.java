56ace188a6474d6694319aa007bc2e63674abdc2
liang <li_ang_666@163.com>
2024-10-09 10:24:46 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -1,19 +1,14 @@
 package com.liang.repair.test;
 
-import com.liang.common.service.database.template.JdbcTemplate;
+import com.liang.common.dto.config.RepairTask;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
-import org.roaringbitmap.longlong.Roaring64Bitmap;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        log.info("");
-        Roaring64Bitmap emptyCompany = new Roaring64Bitmap();
-        new JdbcTemplate("116.prism")
-                .streamQuery(true,
-                        "select graph_id from entity_empty_index where entity_type = 2 and deleted = 0",
-                        rs -> emptyCompany.add(rs.getLong(1)));
-        log.info("");
+        RepairTask repairTask1 = new RepairTask();
+        RepairTask repairTask2 = new RepairTask();
+        System.out.println(repairTask1.equals(repairTask2));
     }
 }
