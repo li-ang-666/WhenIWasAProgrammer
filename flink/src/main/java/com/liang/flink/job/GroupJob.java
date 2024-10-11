80ef8e9c832a519d2f5ad0b7207b8327097097d5
liang <li_ang_666@163.com>
2024-10-11 12:20:13 +0800

    1

--- flink/src/main/java/com/liang/flink/job/GroupJob.java
+++ flink/src/main/java/com/liang/flink/job/GroupJob.java
@@ -1,38 +1,123 @@
 package com.liang.flink.job;
 
+import cn.hutool.core.util.StrUtil;
 import com.liang.common.service.SQL;
 import com.liang.common.service.database.template.JdbcTemplate;
+import com.liang.common.util.JsonUtils;
 import com.liang.common.util.SqlUtils;
 import com.liang.flink.basic.EnvironmentFactory;
 import com.liang.flink.service.LocalConfigFile;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @LocalConfigFile("group.yml")
 public class GroupJob {
     public static void main(String[] args) {
+        Map<Node, Path> result = new LinkedHashMap<>();
         EnvironmentFactory.create(args);
         JdbcTemplate graphData430 = new JdbcTemplate("430.graph_data");
         String companyId = "27624827";
-        String sql = new SQL().SELECT("*")
-                .FROM("company_equity_relation_details")
-                .WHERE("shareholder_id = " + SqlUtils.formatValue(companyId))
-                .toString();
-        List<Map<String, Object>> columnMaps = graphData430.queryForColumnMaps(sql);
-        for (Map<String, Object> columnMap : columnMaps) {
-            String investedCompanyId = (String) columnMap.get("company_id");
-            String equityRatio = (String) columnMap.get("equity_ratio");
-            String maxEquityRatioSql = new SQL().SELECT("max(equity_ratio)")
-                    .FROM("company_equity_relation_details")
-                    .WHERE("company_id = " + SqlUtils.formatValue(investedCompanyId))
-                    .toString();
-            String maxRatio = graphData430.queryForObject(maxEquityRatioSql, rs -> rs.getString(1));
-            if (maxRatio.equals(equityRatio)) {
-                System.out.println(investedCompanyId);
+        String companyName = "上海宝信软件股份有限公司";
+        Queue<Path> queue = new ArrayDeque<>();
+        queue.add(Path.newPath(new Node(companyId, companyName)));
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                Path polledPath = queue.poll();
+                String polledShareholderId = polledPath.getLast().getId();
+                String sql = new SQL().SELECT("*")
+                        .FROM("company_equity_relation_details")
+                        .WHERE("shareholder_id = " + SqlUtils.formatValue(polledShareholderId))
+                        .ORDER_BY("equity_ratio desc")
+                        .toString();
+                List<Map<String, Object>> columnMaps = graphData430.queryForColumnMaps(sql);
+                for (Map<String, Object> columnMap : columnMaps) {
+                    String investedCompanyId = (String) columnMap.get("company_id");
+                    String investedCompanyName = (String) columnMap.get("company_name");
+                    String equityRatio = (String) columnMap.get("equity_ratio");
+                    System.out.println(equityRatio);
+                    String maxEquityRatioSql = new SQL().SELECT("max(equity_ratio)")
+                            .FROM("company_equity_relation_details")
+                            .WHERE("company_id = " + SqlUtils.formatValue(investedCompanyId))
+                            .toString();
+                    String maxRatio = graphData430.queryForObject(maxEquityRatioSql, rs -> rs.getString(1));
+                    System.out.println(maxRatio);
+                    if (maxRatio.equals(equityRatio)) {
+                        Edge edge = new Edge(new BigDecimal(equityRatio).toPlainString());
+                        Node node = new Node(investedCompanyId, investedCompanyName);
+                        Path path = Path.newPath(polledPath, edge, node);
+                        if (!result.containsKey(node)) {
+                            result.put(node, path);
+                            queue.add(path);
+                        }
+                    }
+                }
             }
+        }
+        result.forEach((k, v) -> {
+            System.out.println(StrUtil.fillAfter(k.toString(), ' ', 130 - (k.toString().length())) + " -> " + v);
+        });
+    }
+
+    private interface Element {
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static final class Node implements Element {
+        String id;
+        String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static final class Edge implements Element {
+        String info;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static final class Path {
+        private List<Element> elements = new ArrayList<>();
+
+        public static Path newPath(Node node) {
+            Path path = new Path();
+            path.elements.add(node);
+            return path;
+        }
+
+        public static Path newPath(Path path, Edge edge, Node node) {
+            Path newPath = new Path();
+            newPath.elements.addAll(path.elements);
+            newPath.elements.add(edge);
+            newPath.elements.add(node);
+            return newPath;
+        }
+
+        public Node getLast() {
+            return (Node) elements.get(elements.size() - 1);
+        }
+
+        @Override
+        public String toString() {
+            return elements.stream().map(element -> {
+                        if (element instanceof Edge) {
+                            return ((Edge) element).info;
+                        } else {
+                            return JsonUtils.toString(element);
+                        }
+                    })
+                    .collect(Collectors.joining(" "));
         }
     }
 }
--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -1,14 +1,22 @@
 package com.liang.repair.test;
 
-import com.liang.common.dto.config.RepairTask;
+import com.liang.common.service.SQL;
+import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        RepairTask repairTask1 = new RepairTask();
-        RepairTask repairTask2 = new RepairTask();
-        System.out.println(repairTask1.equals(repairTask2));
+        JdbcTemplate graphData430 = new JdbcTemplate("430.graph_data");
+        String sql = new SQL().SELECT("*")
+                .FROM("company_equity_relation_details")
+                .WHERE("company_id = 1163921791")
+                .toString();
+        List<Map<String, Object>> columnMaps = graphData430.queryForColumnMaps(sql);
+        System.out.println(columnMaps);
     }
 }
