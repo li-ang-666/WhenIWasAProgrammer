de44662f5b020c4f9cab506ec6d6e46f2ca94d8b
liang <li_ang_666@163.com>
2024-10-12 15:59:52 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/BossRepair.java
+++ repair/src/main/java/com/liang/repair/impl/BossRepair.java
@@ -12,7 +12,7 @@ public class BossRepair extends ConfigHolder {
 
     public static void main(String[] args) {
         String body = HttpUtil.createPost("http://10.99.138.255:9060/update_boss")
-                .form("hgid", "1807132440")
+                .form("hgid", "2002633907")
                 .timeout(TIMEOUT)
                 .execute()
                 .body();
