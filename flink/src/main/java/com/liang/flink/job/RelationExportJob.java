5472a53452423046018a7faaf2131967a6b76965
liang <li_ang_666@163.com>
2024-10-14 10:36:17 +0800

    1

--- flink/src/main/java/com/liang/flink/job/RelationExportJob.java
+++ flink/src/main/java/com/liang/flink/job/RelationExportJob.java
@@ -52,7 +52,7 @@ create external table if not exists test.relation_edge(
 
 // insert overwrite table test.relation_node_company select /*+ REPARTITION(5) */ distinct * from test.relation_node_company;
 // insert overwrite table test.relation_node_human select /*+ REPARTITION(4) */ distinct * from test.relation_node_human;
-// insert overwrite table test.relation_edge select /*+ REPARTITION(8) */ distinct * from test.relation_edge;
+// insert overwrite table test.relation_edge select /*+ REPARTITION(9) */ distinct * from test.relation_edge;
 @Slf4j
 @LocalConfigFile("relation-export.yml")
 public class RelationExportJob {
