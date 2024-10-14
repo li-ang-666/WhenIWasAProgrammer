d1c0f1bfb6cc4b7cea980066a99af1d7ad53ab86
liang <li_ang_666@163.com>
2024-10-14 15:34:13 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -6,6 +6,5 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        System.out.println("\r".matches("\\s"));
     }
 }
