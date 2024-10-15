2238c292e93a8bd0fe686753f78c1e9b834613d1
liang <li_ang_666@163.com>
2024-10-15 10:50:39 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -1,5 +1,6 @@
 package com.liang.repair.test;
 
+import cn.hutool.core.exceptions.ExceptionUtil;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,6 +9,7 @@ public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
         Exception exception = new Exception("abc");
         RuntimeException runtimeException = new RuntimeException("def", exception);
+        System.out.println("error\n" + ExceptionUtil.stacktraceToString(runtimeException));
         log.error("error", runtimeException);
     }
 }
