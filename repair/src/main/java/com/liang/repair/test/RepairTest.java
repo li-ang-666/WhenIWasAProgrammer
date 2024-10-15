8a62999fc96d6f466603a0ed47851af5165e1404
liang <li_ang_666@163.com>
2024-10-15 15:11:20 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -1,15 +1,15 @@
 package com.liang.repair.test;
 
-import cn.hutool.core.exceptions.ExceptionUtil;
 import com.liang.repair.service.ConfigHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.roaringbitmap.longlong.Roaring64Bitmap;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        Exception exception = new Exception("abc");
-        RuntimeException runtimeException = new RuntimeException("def", exception);
-        System.out.println("error\n" + ExceptionUtil.stacktraceToString(runtimeException));
-        log.error("error", runtimeException);
+        Roaring64Bitmap bitmap = new Roaring64Bitmap();
+        bitmap.addRange(1, 100);
+        System.out.println(bitmap.first());
+        System.out.println(bitmap.last());
     }
 }
