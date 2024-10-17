21ab0e6fa9a0a35f45383a24cfafa494e928bffb
liang <li_ang_666@163.com>
2024-10-17 09:52:08 +0800

    1

--- common/src/main/java/com/liang/common/service/database/factory/DruidFactory.java
+++ common/src/main/java/com/liang/common/service/database/factory/DruidFactory.java
@@ -7,11 +7,41 @@ import com.liang.common.util.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.concurrent.TimeUnit.*;
 
 @Slf4j
 public class DruidFactory implements SinglePoolFactory<DBConfig, DruidDataSource> {
+    private static final Map<String, String> URL_PROP_MAP = new HashMap<>();
+    private static final String URL_PROP_STR;
+    private static final List<String> OTHER_INIT_SQLS = new ArrayList<>();
+
+    static {
+        // map
+        URL_PROP_MAP.put("serverTimezone", "GMT%2B8");
+        URL_PROP_MAP.put("zeroDateTimeBehavior", "convertToNull");
+        URL_PROP_MAP.put("yearIsDateType", "false");
+        URL_PROP_MAP.put("tinyInt1isBit", "false");
+        URL_PROP_MAP.put("useUnicode", "true");
+        URL_PROP_MAP.put("characterEncoding", "UTF-8");
+        URL_PROP_MAP.put("characterSetResults", "UTF-8");
+        URL_PROP_MAP.put("useSSL", "false");
+        URL_PROP_MAP.put("autoReconnect", "true");
+        URL_PROP_MAP.put("maxAllowedPacket", String.valueOf(1024 * 1024 * 1024));
+        URL_PROP_MAP.put("rewriteBatchedStatements", "true");
+        // str
+        URL_PROP_STR = URL_PROP_MAP.entrySet().stream()
+                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                .collect(Collectors.joining("&", "?", ""));
+        // list
+        OTHER_INIT_SQLS.add("set wait_timeout = 3600 * 24 * 7");
+        OTHER_INIT_SQLS.add("set interactive_timeout = 3600 * 24 * 7");
+    }
 
     @Override
     public DruidDataSource createPool(String name) {
@@ -21,28 +51,9 @@ public class DruidFactory implements SinglePoolFactory<DBConfig, DruidDataSource
     @Override
     public DruidDataSource createPool(DBConfig config) {
         try {
-            String url = "jdbc:mysql://" +
-                    config.getHost() + ":" + config.getPort() + "/" + config.getDatabase() +
-                    // 时区
-                    "?serverTimezone=GMT%2B8" +
-                    // datetime 字段处理
-                    "&zeroDateTimeBehavior=convertToNull" +
-                    // year
-                    "&yearIsDateType=false" +
-                    // tinyint(1) 字段处理
-                    "&tinyInt1isBit=false" +
-                    // 编码
-                    "&useUnicode=true" +
-                    "&characterEncoding=UTF-8" +
-                    "&characterSetResults=UTF-8" +
-                    // useSSL
-                    "&useSSL=false" +
-                    // 重连
-                    "&autoReconnect=true" +
-                    // 性能优化
-                    "&maxAllowedPacket=1073741824" + // 1G
-                    "&rewriteBatchedStatements=true";
             DruidDataSource druidDataSource = new DruidDataSource();
+            String url = String.format("jdbc:mysql://%s:%s/%s%s",
+                    config.getHost(), config.getPort(), config.getDatabase(), URL_PROP_STR);
             druidDataSource.setUrl(url);
             druidDataSource.setUsername(config.getUser());
             druidDataSource.setPassword(config.getPassword());
@@ -50,13 +61,13 @@ public class DruidFactory implements SinglePoolFactory<DBConfig, DruidDataSource
             druidDataSource.setInitialSize(1);
             druidDataSource.setMinIdle(1);
             druidDataSource.setMaxActive(128);
-            druidDataSource.setMaxWait((int) TimeUnit.DAYS.toMillis(7));
+            druidDataSource.setMaxWait(MILLISECONDS.convert(7, DAYS));
             druidDataSource.setTestOnBorrow(false);
             druidDataSource.setTestOnReturn(false);
             druidDataSource.setTestWhileIdle(true);
             // 管理minIdle
-            druidDataSource.setTimeBetweenEvictionRunsMillis(TimeUnit.SECONDS.toMillis(30));
-            druidDataSource.setMinEvictableIdleTimeMillis(TimeUnit.SECONDS.toMillis(60));
+            druidDataSource.setTimeBetweenEvictionRunsMillis(MILLISECONDS.convert(30, SECONDS));
+            druidDataSource.setMinEvictableIdleTimeMillis(MILLISECONDS.convert(60, SECONDS));
             // minIdle以内的连接保持活跃
             druidDataSource.setKeepAlive(true);
             druidDataSource.setValidationQuery("select 1");
@@ -68,14 +79,11 @@ public class DruidFactory implements SinglePoolFactory<DBConfig, DruidDataSource
             // 隔离级别
             druidDataSource.setDefaultTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             // 超时
-            druidDataSource.setConnectionInitSqls(Arrays.asList(
-                    "set wait_timeout = 3600 * 24 * 7",
-                    "set interactive_timeout = 3600 * 24 * 7"
-            ));
-            druidDataSource.setConnectTimeout((int) TimeUnit.DAYS.toMillis(7));
-            druidDataSource.setSocketTimeout((int) TimeUnit.DAYS.toMillis(7));
-            druidDataSource.setQueryTimeout((int) TimeUnit.DAYS.toSeconds(7));
-            druidDataSource.setTransactionQueryTimeout((int) TimeUnit.DAYS.toSeconds(7));
+            druidDataSource.setConnectionInitSqls(OTHER_INIT_SQLS);
+            druidDataSource.setConnectTimeout((int) MILLISECONDS.convert(7, DAYS));
+            druidDataSource.setSocketTimeout((int) MILLISECONDS.convert(7, DAYS));
+            druidDataSource.setQueryTimeout((int) SECONDS.convert(7, DAYS));
+            druidDataSource.setTransactionQueryTimeout((int) SECONDS.convert(7, DAYS));
             log.info("DruidFactory createPool success, config: {}", JsonUtils.toString(config));
             return druidDataSource;
         } catch (Exception e) {
