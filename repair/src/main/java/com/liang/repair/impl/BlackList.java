1c8b9d7537ac176f441f6bc1ee4890ad61db955d
liang <li_ang_666@163.com>
2024-10-18 19:45:52 +0800

    1

--- repair/src/main/java/com/liang/repair/impl/BlackList.java
+++ repair/src/main/java/com/liang/repair/impl/BlackList.java
@@ -148,6 +148,7 @@ public class BlackList extends ConfigHolder {
         deleteBossAll("10TNMM602K1GSN032");
         // 删除合作伙伴 ============================================================================
         deletePartner("40TN89C001CR0MVL6");
+        deletePartner("E07CEM9003HR3DQ24");
         deletePartner("E0M969M01Q3RHT5N5");
         deletePartner("L0LBZ9802YY2L1NET");
         deletePartner("F0TSBM600EBH2VDCD");
