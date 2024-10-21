bffaf0332d44381f31da17cc0bbafbf06849c70c
liang <li_ang_666@163.com>
2024-10-21 21:30:00 +0800

    1

--- repair/src/main/java/com/liang/repair/test/RepairTest.java
+++ repair/src/main/java/com/liang/repair/test/RepairTest.java
@@ -1,15 +1,21 @@
 package com.liang.repair.test;
 
+import com.liang.common.service.database.template.JdbcTemplate;
 import com.liang.repair.service.ConfigHolder;
+import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 import lombok.extern.slf4j.Slf4j;
-import org.roaringbitmap.longlong.Roaring64Bitmap;
 
 @Slf4j
 public class RepairTest extends ConfigHolder {
     public static void main(String[] args) throws Exception {
-        Roaring64Bitmap bitmap = new Roaring64Bitmap();
-        bitmap.addRange(1, 100);
-        System.out.println(bitmap.first());
-        System.out.println(bitmap.last());
+        String content = new JdbcTemplate("104.data_bid").queryForObject("select content from company_bid where id = 229774584", rs -> rs.getString(1));
+        System.out.println(htmlToMd(content));
+    }
+
+    private static String htmlToMd(String html) {
+        MutableDataSet options = new MutableDataSet();
+        FlexmarkHtmlConverter converter = FlexmarkHtmlConverter.builder(options).build();
+        return converter.convert(html);
     }
 }
