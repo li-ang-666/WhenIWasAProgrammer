87124c06d6ee97535539afa110622b105afb3848
liang <li_ang_666@163.com>
2024-10-21 22:45:28 +0800

    1

--- flink/src/main/java/com/liang/flink/test/FlinkTest.java
+++ flink/src/main/java/com/liang/flink/test/FlinkTest.java
@@ -2,6 +2,7 @@ package com.liang.flink.test;
 
 import com.liang.common.dto.DorisOneRow;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.Conversions;
 import org.apache.avro.LogicalTypes;
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
@@ -13,19 +14,20 @@ import org.apache.parquet.avro.AvroParquetWriter;
 import org.apache.parquet.hadoop.ParquetWriter;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.UUID;
 
 @Slf4j
 public class FlinkTest {
     public static void main(String[] args) throws Exception {
         Schema schema = SchemaBuilder.record(DorisOneRow.class.getSimpleName()).fields()
-                .name("id").type(LogicalTypes.decimal(32, 12).addToSchema(SchemaBuilder.builder().bytesType())).withDefault(null)
+                .name("id").type(LogicalTypes.decimal(40, 20).addToSchema(SchemaBuilder.builder().bytesType())).withDefault(null)
                 .name("name").type(SchemaBuilder.builder().stringType()).withDefault(null)
                 .endRecord();
         GenericData genericData = new GenericData();
-        genericData.addLogicalTypeConversion(null);
+        genericData.addLogicalTypeConversion(new Conversions.DecimalConversion());
         GenericRecordBuilder record = new GenericRecordBuilder(schema);
-        record.set("id", new BigDecimal("100.001"));
+        record.set("id", new BigDecimal("100.001").setScale(20, RoundingMode.DOWN));
         record.set("name", "liang");
         record.build();
         ParquetWriter<GenericRecord> writer = AvroParquetWriter.<GenericRecord>builder(new Path("obs://hadoop-obs/flink/test/" + UUID.randomUUID()))
